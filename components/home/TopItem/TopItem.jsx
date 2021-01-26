359506652b1971678d6f3b55c357a389c79c3977
昌维 <867597730@qq.com>
2021-01-26 12:59:47 +0800

    update homepage TopItem component, remove avatar image and limit title line count to one line

--- components/home/TopItem/TopItem.jsx
+++ components/home/TopItem/TopItem.jsx
@@ -10,29 +10,20 @@ dayjs.extend(relativeTime)
 export default function TopItem({title, categoryName, categoryColor, tags, authorUrl, authorName, authorAvatarUrl, replyCount, lastPostedAt, link}) {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.left}>
-        <div className={styles.left_avatar}>
-          <a href={authorUrl} target="_blank" rel="noopener noreferer">
-            <img src={authorAvatarUrl} alt=""/>
-          </a>
-        </div>
+      <div className={styles.title}>
+        <MyLink href={link}>{title}</MyLink>
       </div>
-      <div className={styles.right}>
-        <div className={styles.right_title}>
-          <MyLink href={link}>{title}</MyLink>
+      <div className={styles.metadata}>
+        <div className={styles.metadata_category}>
+          <Tag color={categoryColor}>{categoryName}</Tag>
+          {tags.map((tag, index) => <Tag key={index} color={'ccc'}>{tag}</Tag>)}
         </div>
-        <div className={styles.right_metadata}>
-          <div className={styles.right_metadata_category}>
-            <Tag color={categoryColor}>{categoryName}</Tag>
-            {tags.map((tag, index) => <Tag key={index} color={'ccc'}>{tag}</Tag>)}
+        <div className={styles.metadata_statistics}>
+          <div className={styles.metadata_statistics_reply_count}>
+            回复 {replyCount}
           </div>
-          <div className={styles.right_metadata_statistics}>
-            <div className={styles.right_metadata_statistics_reply_count}>
-              回复 {replyCount}
-            </div>
-            <div className={styles.right_metadata_statistics_last_posted_at}>
-              {dayjs(lastPostedAt).locale('zh').fromNow()}
-            </div>
+          <div className={styles.metadata_statistics_last_posted_at}>
+            {dayjs(lastPostedAt).locale('zh').fromNow()}
           </div>
         </div>
       </div>

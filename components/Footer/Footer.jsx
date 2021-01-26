6c27427cb9ad63c2f557f61be93726aac294ae6a
昌维 <867597730@qq.com>
2021-01-26 17:33:06 +0800

    update footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,20 +1,19 @@
 import React from 'react'
-// import Socials from '../socials/Socials'
+import styles from './Footer.module.scss'
+import Socials from '../socials/Socials'
 import i18n from '../../data/footer'
 import MyLink from 'components/MyLink'
-// import { useIntl } from 'react-intl'
 import Container from "components/Container/Container";
+// import { useIntl } from 'react-intl'
 
-const logoImageUrl = '/images/logo.svg'
+const logoImageUrl = 'images/logo.svg'
 
-import styles from './Footer.module.scss'
-import {Row, Col} from "antd";
 
 export default function Footer() {
   
   // const intl = useIntl()
   // const locale = intl.locale
-  const locale = 'en'
+  const locale = 'zh'
   
   const data = i18n[locale]
   
@@ -22,39 +21,37 @@ export default function Footer() {
   
   return (
     <div className={styles.wrapper}>
-      <Container fluid className={styles.container}>
-        <Row gutter={[16, 16]} className={styles.main}>
-          <Col sm={24} md={16}>
-            <Row className={styles.main_left}>
-              {data.map((column, columnIndex) => (
-                <Col key={columnIndex} xs={24} sm={12} md={8} className={styles.main_left_column}>
-                  <div className={styles.main_left_column_title}>
-                    {column.name}
-                  </div>
-                  <ul className={styles.main_left_column_list}>
-                    {column.items.map((item, itemIndex) => (
-                      <li key={itemIndex}>{item.outbound ? <MyLink href={item.link}>{item.name}</MyLink> : <MyLink to={item.link}>{item.name}</MyLink>}</li>
-                    ))}
-                  </ul>
-                </Col>
-              ))}
-            </Row>
-          </Col>
-          
-          <Col sm={24} md={8} className={styles.main_right}>
-            <div className={styles.main_right_logo}>
-              <MyLink to={'/'}>
-                <img src={logoImageUrl} alt="TiDB User Group"/>
-              </MyLink>
-            </div>
+      <Container className={styles.container}>
+        <div className={styles.main}>
+          <div className={styles.main_left}>
+            {data.map((column, columnIndex) => (
+              <div key={columnIndex} className={styles.main_left_column}>
+                <div className={styles.main_left_column_title}>
+                  {column.name}
+                </div>
+                <ul className={styles.main_left_column_list}>
+                  {column.items.map((item, itemIndex) => (
+                    <li key={itemIndex}>{item.outbound ? <MyLink href={item.link}>{item.name}</MyLink> : <MyLink to={item.link}>{item.name}</MyLink>}</li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
+          <div className={styles.main_right}>
             <div className={styles.main_right_socials}>
-              {/*<Socials type="follow" />*/}
+              <Socials type="follow" />
             </div>
-          </Col>
-        </Row>
-        
-        <div className={styles.copyright}>
-          {copyrightNode}
+            <div className={styles.main_right_brand}>
+              <div className={styles.main_right_brand_logo}>
+                <MyLink to={'/'}>
+                  <img src={logoImageUrl} alt="TiDB User Group"/>
+                </MyLink>
+              </div>
+              <div className={styles.main_right_brand_copyright}>
+                {copyrightNode}
+              </div>
+            </div>
+          </div>
         </div>
       </Container>
     </div>

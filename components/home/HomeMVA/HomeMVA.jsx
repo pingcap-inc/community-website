6fecf7be1c51ca55173617ecda968134449098fc
昌维 <867597730@qq.com>
2021-01-28 14:06:12 +0800

    update HomeMVA copywriting

--- components/home/HomeMVA/HomeMVA.jsx
+++ components/home/HomeMVA/HomeMVA.jsx
@@ -9,10 +9,10 @@ export default function HomeMVA({}) {
     <div className={styles.wrapper}>
       <Container className={styles.container}>
         <div className={styles.title}>
-          2020 年度 TiDB MVA
+          2020 年度 TUG MVA
         </div>
         <div className={styles.summary}>
-          TiDB MVA (Most Valuable Advocate) 通过分享优质的技术内容，帮助大家更好地使用 TiDB，是经过认证的技术先锋，享受极高的社区荣誉。
+          TUG MVA (Most Valuable Advocate) 通过分享优质的技术内容，帮助大家更好地使用 TiDB，是经过认证的技术先锋，享受极高的社区荣誉。
         </div>
         <div className={styles.buttons}>
           <Button as={MyLink} href={'/MVA'}>查看更多</Button>

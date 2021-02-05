350fdceb485f9dea2fd83497a9942c10cc4f9622
昌维 <867597730@qq.com>
2021-02-05 18:32:48 +0800

    add banner: annual

--- data/banners.js
+++ data/banners.js
@@ -4,4 +4,9 @@ export default [
     linkUrl: 'https://asktug.com/t/topic/306',
     title: '欢迎成为 TUG 新成员',
   },
+  {
+    imageUrl: '/images/home/banner-annual.jpg',
+    linkUrl: 'https://asktug.com/t/topic/68382',
+    title: 'TUG 社区 2020 年度盘点',
+  },
 ]

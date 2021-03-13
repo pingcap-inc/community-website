1bd457b79be242788f71868115ce16a6f01fb18e
Nick Qi <wei.qi.nick@gmail.com>
2021-03-13 21:09:44 +1300

    CPT-5 feat: introduce styled components (#5)
    * CPT-5 feat: upgrade node dependencies
    
    * CPT-5 feat: install styled-components and its related dependencies
    
    * CPT-5 refactor: introduce `src` folder for a large-scaled application
    
    * CPT-5 feat: support styled-components in NextJS by following the official guide
    
    * CPT-5 feat: implement CoreLayout and apply it to the community page

---
+++ src/styled/index.js
@@ -0,0 +1,14 @@
+import { clearFix } from 'polished';
+import { createGlobalStyle } from 'styled-components';
+
+import resetAntStyles from './antd';
+
+export const createAppGlobalStyle = () => createGlobalStyle`
+  #__next {
+    .clearfix {
+      ${clearFix()};
+    }
+
+    ${resetAntStyles};
+  }
+`;

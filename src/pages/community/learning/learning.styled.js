467d88657e00520db5bc940bf02cde3321c2f89e
Nick Qi <wei.qi.nick@gmail.com>
2021-03-27 11:14:08 +1300

    CPT-13 fix: make the cover image on activity card to be more responsive

--- src/pages/community/learning/learning.styled.js
+++ src/pages/community/learning/learning.styled.js
@@ -1,3 +1,7 @@
 import styled from 'styled-components';
+import { colors, mixins } from '@pingcap/pingcap-ui';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  padding: 5rem 0;
+  background: ${colors.M2};
+`;

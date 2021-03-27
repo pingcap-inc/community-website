8c9b2af4d857e8abe04595e396f6a2935be9e180
Nick Qi <wei.qi.nick@gmail.com>
2021-03-27 16:02:48 +1300

    CPT-13 style: implement stylings for different sizes of buttons

--- src/pages/community/learning/Learning.component.js
+++ src/pages/community/learning/Learning.component.js
@@ -1,6 +1,6 @@
+import Image from 'next/image';
 import React from 'react';
 import { Row, Col } from 'antd';
-import Image from 'next/image';
 
 import * as Styled from './learning.styled';
 import data from './learning.data';
@@ -15,13 +15,20 @@ const Learning = () => {
           <Col xs={24} md={10}>
             <Image alt={title} src="/images/community/learn.svg" width="452" height="247" />
           </Col>
+
           <Col xs={24} md={14}>
             <Styled.Title>{title}</Styled.Title>
+
             <Styled.Desc>
               {desc.map((item, idx) => (
                 <li key={idx}>{item}</li>
               ))}
             </Styled.Desc>
+
+            <Styled.Buttons>
+              <Styled.Button ghost>{btns.pu}</Styled.Button>
+              <Styled.Button>{btns.docs}</Styled.Button>
+            </Styled.Buttons>
           </Col>
         </Row>
       </Styled.Content>

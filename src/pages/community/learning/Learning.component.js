7e293ab93b4fd8aab37abba1f37ce911abcd3309
Nick Qi <wei.qi.nick@gmail.com>
2021-03-27 14:15:28 +1300

    CPT-13 style: add content to the learning module

--- src/pages/community/learning/Learning.component.js
+++ src/pages/community/learning/Learning.component.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import { Row, Col } from 'antd';
+import Image from 'next/image';
 
 import * as Styled from './learning.styled';
+import data from './learning.data';
 
-const Learning = () => (
-  <Styled.Container>
-    <Styled.Content>
-      <Row gutter={[32, 0]}>
-        <Col xs={24} md={10}>
-          Img Col
-        </Col>
-        <Col xs={24} md={14}>
-          Main Col
-        </Col>
-      </Row>
-    </Styled.Content>
-  </Styled.Container>
-);
+const Learning = () => {
+  const { title, desc, btns } = data;
+
+  return (
+    <Styled.Container>
+      <Styled.Content>
+        <Row gutter={[32, 0]}>
+          <Col xs={24} md={10}>
+            <Image alt={title} src="/images/community/learn.svg" width="452" height="247" />
+          </Col>
+          <Col xs={24} md={14}>
+            <Styled.Title>{title}</Styled.Title>
+            <Styled.Desc>
+              {desc.map((item, idx) => (
+                <li key={idx}>{item}</li>
+              ))}
+            </Styled.Desc>
+          </Col>
+        </Row>
+      </Styled.Content>
+    </Styled.Container>
+  );
+};
 
 export default Learning;

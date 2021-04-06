5f0bcf028bb31589e2c2938ffad8655fb2dafce7
Nick Qi <wei.qi.nick@gmail.com>
2021-04-06 16:16:57 +1200

    CPT-63 feat: add tug favicons

--- src/components/head/index.js
+++ src/components/head/index.js
@@ -1,1 +1,8 @@
-export { default as CommunityHead } from './CommunityHead.component';
+import React from 'react';
+
+import * as data from './head.data';
+import Head from './Head.component';
+
+export const CommunityHead = (props) => <Head {...data.community} {...props} />;
+
+export const TugHead = (props) => <Head {...data.tug} {...props} />;

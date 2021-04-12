2fb9b7d3e270b9b18625c084dd7dc7646d557de4
634750802 <634750802@qq.com>
2021-04-12 12:19:17 +0800

    CPT-93 feat: nav link route to none production host if not in production mode

--- src/utils/link.utils.js
+++ src/utils/link.utils.js
@@ -1,5 +1,11 @@
+const hostRegexp = /https?:\/\/(?:tug\.)?tidb\.io\//;
+
 export const handleRedirect = (router, link) => {
   if (!link) return;
+
+  if (process.env.NODE_ENV !== 'production') {
+    link = link.replace(hostRegexp, window.location.origin + '/');
+  }
 
   if (link.startsWith('http')) {
     return window.open(link, '_blank');

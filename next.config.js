ad0ce028ae5c4cd0599e49c233fc98092480e391
Nick Qi <wei.qi.nick@gmail.com>
2021-04-13 15:00:14 +1200

    CPT-89 feat: map '@tidb-community/ui' to its source code for internal consumption

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   pageExtensions: ['page.js'],
 
@@ -7,6 +9,15 @@ module.exports = {
 
   // https://nextjs.org/docs/api-reference/next.config.js/custom-webpack-config
   webpack: (config, options) => {
+    const { alias } = config.resolve;
+
+    config.resolve.alias = {
+      ...alias,
+      // Make sure we will build directly from the source code for internal comsumers,
+      // which gives us an instant reaction if anything updates
+      '@tidb-community/ui': path.resolve('./packages/ui/src'),
+    };
+
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],

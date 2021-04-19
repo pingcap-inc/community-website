7e0de4a96b039f227e80acd4a38db47b535fd2b8
Jagger <634750802@qq.com>
2021-04-19 18:48:44 +0800

    CPT-101 config: use process.env.RUNTIME_ENV instead of process.env.NODE_ENV

--- packages/datasource/src/index.js
+++ packages/datasource/src/index.js
@@ -1,7 +1,7 @@
 import { getData as navGetData } from './nav';
 import { defaultEnvDomains } from './config.domains';
 
-const defaultEnv = process.env.NODE_ENV;
+const defaultEnv = process.env.RUNTIME_ENV || 'production';
 
 export const getData = ({ domain, path, locale, env, envDomainConfig } = {}) => {
   return {
--- packages/datasource/typing/index.d.ts
+++ packages/datasource/typing/index.d.ts
@@ -14,7 +14,7 @@ type GetDataParams = {
   domain: string
   path: string
   locale: Locale
-  env?: Env // default is process.env.NODE_ENV
+  env?: Env // default is process.env.RUNTIME_ENV
   envDomainConfig?: EnvDomainConfig // default is configured in this package
 }
 

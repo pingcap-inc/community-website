ec8011684ed8ebdbf06131921b1039e7038783dd
Nick Qi <wei.qi.nick@gmail.com>
2021-04-24 11:13:43 +1200

    CPT-146 chore: remove asktug script from the source control

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,3 @@
 module.exports = {
   '*.{js,json,css,scss,md}': ['prettier-eslint --write', 'git add'],
-  'packages/{asktug,datasource,ui}/src/**': () => ['npm run build:asktug', 'git add .'],
 };

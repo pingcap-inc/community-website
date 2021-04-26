7697cdf31dbc0ef9db32c5e83b526574a18af0dd
Jagger <634750802@qq.com>
2021-04-26 16:35:01 +0800

    CPT-144 fix: styled components' props passing

--- src/layouts/split/Split.styled.js
+++ src/layouts/split/Split.styled.js
@@ -1,27 +1,25 @@
-import styled, {css} from 'styled-components';
-import PropTypes from 'prop-types'
+import styled, { css } from 'styled-components';
 import { Row as AntRow, Col as AntCol } from 'antd';
 
 export const Container = styled(AntRow)`
-  margin-top: ${props => props.marginTop};
-  margin-bottom: ${props => props.marginBottom};
-  ${props => props.paddingHorizontal && css`
-    padding-left: 24px;
-    padding-right: 24px;
-  `}
+  margin-top: ${(props) => props.$marginTop};
+  margin-bottom: ${(props) => props.$marginBottom};
+  ${(props) =>
+    props.$paddingHorizontal &&
+    css`
+      padding-left: 24px;
+      padding-right: 24px;
+    `}
 `;
 
 export const ItemContainer = styled(AntCol)`
-  ${props => props.marginTop && css`
-    margin-top: 36px;
-  `}
-`
+  ${(props) =>
+    props.$marginTop &&
+    css`
+      margin-top: 36px;
+    `}
+`;
 
 export const ItemContainerWithDivider = styled(ItemContainer)`
-  border-right: 1px solid ${props => props.dividerColor};
-`
-
-ItemContainerWithDivider.propTypes = {
-  dividerColor: PropTypes.string,
-}
-
+  border-right: 1px solid ${(props) => props.$dividerColor};
+`;

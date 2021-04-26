f266ebede9ff9c44b9ef8a61d2aaf9eee6c58671
Jagger <634750802@qq.com>
2021-04-26 16:31:59 +0800

    CPT-144 fix: api url

--- packages/datasource/src/api/org/searchCompany/index.js
+++ packages/datasource/src/api/org/searchCompany/index.js
@@ -1,5 +1,5 @@
 import Axios from 'axios';
 
 export const searchCompany = ({ word }) => {
-  return Axios.post('/api/org/search-company', { word });
+  return Axios.post('/api/orgs/search-company', { word });
 };

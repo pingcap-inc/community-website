f0f0fa322d004f80b9116dd21e6364e46f12fa41
Nick Qi <wei.qi.nick@gmail.com>
2021-04-26 14:13:34 +1200

    CPT-144 feat: launch a mock API server (#62)
    * CPT-144 feat: implement mock API boilerplate
    
    * CPT-144 feat: introduce successResp and errorResp
    
    * CPT-144 feat: add search-company mocks
    
    * CPT-144 feat: add enroll API
    
    * CPT-144 feat: add enroll and upload-incumbency-cert APIs

---
+++ packages/mockApi/routes/orgs/index.js
@@ -0,0 +1,13 @@
+// The API doc could be refer to
+// https://github.com/pingcap/account/blob/master/src/pingcap_sso/api/docs/ORG.md
+//
+// The Postman collection chould be downloaded from
+// https://www.getpostman.com/collections/f6e237c8ce2282dbd6b1
+
+const router = require('express').Router();
+
+router.post('/check-name', require('./checkName'));
+router.post('/search-company', require('./searchCompany'));
+router.use('/enroll', require('./enroll'));
+
+module.exports = router;
---
+++ packages/mockApi/routes/orgs/searchCompany/searchCompany.mock.js
@@ -0,0 +1,21 @@
+const companyList = [
+  {
+    name: '平凯星辰（北京）科技有限公司',
+    credit_code: '91110108339695681X',
+    base: '北京',
+  },
+  {
+    name: '北京平凯星辰科技发展有限公司',
+    credit_code: '911101083363962058',
+    base: '北京',
+  },
+  {
+    name: '河北兴晟农业科技有限公司',
+    credit_code: '91130684MA0A8BQM53',
+    base: '河北',
+  },
+];
+
+module.exports = {
+  companyList,
+};

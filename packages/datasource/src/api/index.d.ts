efc43c4538b5838ce1432f35786766d411b0a2ce
Nick Qi <wei.qi.nick@gmail.com>
2021-05-03 19:51:39 +1200

    CPT-153 fix: replace initApi with the axios client (#75)
    * CPT-153 fix: initialize axios client and dispatch API errors thru the custom event
    
    * CPT-153 fix: make all API methods based on the client instance
    
    * CPT-153 fix: fix the request interceptor

--- packages/datasource/src/api/index.d.ts
+++ packages/datasource/src/api/index.d.ts
@@ -1,25 +1,10 @@
-import { AxiosResponse } from 'axios';
-
 export * as orgs from './orgs';
 export { me } from './me';
-export module Constants {
-  export const SUCCESS: string;
-  export const SUCCESS_NOT_USED: string;
-  export const ERR_PARAMS_WRONG: string;
-}
 
 export type ApiResponse<T, Detail> = {
   detail: Detail;
 } & T;
 
-export interface ApiRequestFunction<Params, ResponseData, ResponseDetails = Constants['SUCCESS']> {
+export interface ApiRequestFunction<Params, ResponseData, ResponseDetails = string> {
   (params: Params): Promise<ApiResponse<ResponseData, ResponseDetails>>;
 }
-
-interface GlobalApiErrorHandler {
-  (response: AxiosResponse): any;
-}
-
-export function initApi(globalErrorHandler: GlobalApiErrorHandler);
-
-export const GLOBAL_ERROR_HANDLED: symbol;
--- src/pages/orgs/[slug]/members/members.utils.js
+++ src/pages/orgs/[slug]/members/members.utils.js
@@ -2,13 +2,14 @@ import * as R from 'ramda';
 
 import { ROLE_NAMES } from './members.constants';
 
-export const getDataSource = ({ membersResp = {}, me }) => {
+export const getDataSource = ({ membersResp = {}, meResp = {} }) => {
   const { data = [] } = membersResp;
+  const meId = meResp.data?.id;
 
   return data.map((item) => ({
     key: item.id,
     ...R.pick(['id', 'name', 'username', 'email'], item),
     role: ROLE_NAMES[item.role],
-    operation: me.id === item.id ? '退出' : '删除',
+    operation: item.id === meId ? '退出' : '删除',
   }));
 };

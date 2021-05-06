532aa0e29fba980ebccd301dcc08955727d4d4f5
Jagger <634750802@qq.com>
2021-05-06 17:35:52 +0800

    CPT-112 feat: audit result page (#80)

--- src/pages/account/organization/new/index.page.js
+++ src/pages/account/organization/new/index.page.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Banner from './banner';
 import Form from './form';
+import Audit from './audit';
 import { CoreLayout, SplitLayout } from 'layouts';
 import { featureToggle } from 'utils';
+import { useRouter } from 'next/router';
 
 export const getServerSideProps = async ({ req }) => {
   // https://vercel.com/docs/environment-variables#system-environment-variables
@@ -26,12 +28,22 @@ export const getServerSideProps = async ({ req }) => {
 };
 
 const CreateOrganization = () => {
+  // temp codes
+  const { query } = useRouter();
+  const [isSubmitted, setSubmitted] = useState(query.submitted === 'true');
+  const status = query.status || 'pending';
+  const rejectReason = query.reason || '未知原因';
+
   return (
     <CoreLayout domain="tug.tidb.io">
-      <SplitLayout dividerColor={'rgba(108, 116, 150, 0.4)'} marginTop="30px" marginBottom="41px">
-        <Banner />
-        <Form />
-      </SplitLayout>
+      {!isSubmitted ? (
+        <SplitLayout dividerColor={'rgba(108, 116, 150, 0.4)'} marginTop="30px" marginBottom="41px">
+          <Banner />
+          <Form onSubmit={() => setSubmitted(true)} />
+        </SplitLayout>
+      ) : (
+        <Audit status={status} rejectReason={rejectReason} />
+      )}
     </CoreLayout>
   );
 };

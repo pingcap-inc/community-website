e18191342273fba77a7cf729cad981781c0b4c6d
634750802 <634750802@qq.com>
2021-05-06 14:18:06 +0800

    CPT-153 feat: map form field to api

--- packages/datasource/src/api/orgs/enroll/index.js
+++ packages/datasource/src/api/orgs/enroll/index.js
@@ -2,7 +2,42 @@ import client from '../../client';
 import { sendCode } from './sendCode';
 import { uploadIncumbencyCert } from './uploadIncumbencyCert';
 
-const enroll = (formContent) => {
+const enroll = ({
+  name,
+  company,
+  industry_type,
+  member_range,
+  job_title,
+  company_base,
+  contact_phone,
+  audit_type,
+  email,
+  email_code,
+  incumbency_cert,
+}) => {
+  const formContent = {
+    name,
+    company,
+    industry_type,
+    member_range,
+    job_title,
+    company_base,
+    contact_phone,
+    audit_type,
+  };
+
+  switch (audit_type) {
+    case 0:
+      formContent.email = email;
+      formContent.email_code = email_code;
+      break;
+    case 1:
+      formContent.incumbency_cert = incumbency_cert;
+      break;
+    default:
+      return Promise.reject('unsupported audit_type');
+  }
+
   return client.post('/api/orgs/enroll', formContent, {
     isDispatchApiErrorDisabled: true,
   });

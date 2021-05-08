979a6cd2ea1a158fe7d82593b909258da8bac173
Nick Qi <wei.qi.nick@gmail.com>
2021-05-09 11:21:18 +1200

    CPT-122 feat: add search box

--- src/pages/orgs/[slug]/members/addModal/AddModal.component.js
+++ src/pages/orgs/[slug]/members/addModal/AddModal.component.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { CloseCircleFilled, SearchOutlined } from '@ant-design/icons';
+
 import * as Styled from './addModal.styled';
 
 const AddModal = (props) => {
-  return <Styled.Modal {...props}>Test</Styled.Modal>;
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const searchboxProps = {
+    value: searchQuery,
+    onChange: (e) => setSearchQuery(e.value),
+    placeholder: '输入用户名搜索',
+    prefix: <SearchOutlined />,
+    suffix: <CloseCircleFilled onClick={(e) => setSearchQuery('')} />,
+  };
+
+  return (
+    <Styled.Modal {...props}>
+      <Styled.Panel>
+        <Styled.Searchbox {...searchboxProps} />
+      </Styled.Panel>
+      <Styled.Panel>Members</Styled.Panel>
+    </Styled.Modal>
+  );
 };
 
 export default AddModal;
--- src/pages/orgs/[slug]/members/addModal/addModal.styled.js
+++ src/pages/orgs/[slug]/members/addModal/addModal.styled.js
@@ -1,5 +1,40 @@
 import styled from 'styled-components';
+import { Input } from 'antd';
+import { colors } from '@tidb-community/ui';
 
 import { Modal as AntModal } from 'antd';
 
-export const Modal = styled(AntModal)``;
+export const Modal = styled(AntModal).attrs({ width: 690 })`
+  .ant-modal {
+    &-body {
+      display: flex;
+      padding: 0;
+    }
+  }
+`;
+
+export const Panel = styled.div`
+  flex: 1;
+  padding: 16px;
+
+  &:first-child {
+    border-right: 1px solid ${colors.T2};
+  }
+`;
+
+export const Searchbox = styled(Input)`
+  .anticon {
+    &-search {
+      color: ${colors.C4};
+    }
+
+    &-close-circle {
+      color: ${colors.C4};
+      cursor: pointer;
+
+      &:hover {
+        color: ${colors.F2};
+      }
+    }
+  }
+`;

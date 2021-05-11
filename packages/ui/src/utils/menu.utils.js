2254bc8b9e640216d42ab86e2e7c7de3434c7e0b
634750802 <634750802@qq.com>
2021-05-11 15:34:16 +0800

    CPT-115 feat: show username

--- packages/ui/src/utils/menu.utils.js
+++ packages/ui/src/utils/menu.utils.js
@@ -1,7 +1,7 @@
 import { Badge, Menu } from 'antd';
 import React from 'react';
 import styled from 'styled-components';
-import * as R from 'ramda'
+import * as R from 'ramda';
 
 const HiddenMenuItem = styled(Menu.Item)`
   display: none !important;
@@ -19,8 +19,8 @@ export const genMenu = ({ items, currentNav, onNavClick }) => {
 
   return items
     .filter(item => !item.hidden) // This is used for getting current nav but not really nav item.
-    .map((item) => {
-      const { badge, title, items, link, browserLink } = item;
+    .flatMap((item) => {
+      const { badge, divider, title, items, link, browserLink } = item;
 
       if (items) {
         const onSubMenuClick = () => {
@@ -36,7 +36,7 @@ export const genMenu = ({ items, currentNav, onNavClick }) => {
           title: link ? <span onClick={onSubMenuClick}>{title}</span> : title,
         };
 
-        return (
+        return [
           <Menu.SubMenu {...subMenuProps}>
             {link && <HiddenMenuItem key={title} onClick={onItemClick(link, browserLink)} />}
             {
@@ -44,23 +44,28 @@ export const genMenu = ({ items, currentNav, onNavClick }) => {
               genMenu({ items, onNavClick })
             }
           </Menu.SubMenu>
-        );
+        ];
       }
 
+      const disabled = !R.is(String, link);
+      const Divider = () => divider ? <Menu.Divider key={`${title}__divider`} /> : undefined;
+
       if (R.is(Number, badge)) {
-        return (
-          <Menu.Item key={title} onClick={onItemClick(link, browserLink)}>
+        return [
+          <Menu.Item key={title} onClick={onItemClick(link, browserLink)} disabled={disabled}>
             <Badge dot={badge > 0}>
               {title}
             </Badge>
-          </Menu.Item>
-        );
+          </Menu.Item>,
+          Divider,
+        ];
       } else {
-        return (
-          <Menu.Item key={title} onClick={onItemClick(link, browserLink)}>
+        return [
+          <Menu.Item key={title} onClick={onItemClick(link, browserLink)} disabled={disabled}>
             {title}
-          </Menu.Item>
-        );
+          </Menu.Item>,
+          Divider,
+        ];
       }
     });
 };

176a0b854b2f6d83094cc03acbd9e0259ba2f457
634750802 <634750802@qq.com>
2021-05-11 10:56:23 +0800

    CPT-122 fix: show api error details

--- src/utils/index.js
+++ src/utils/index.js
@@ -27,3 +27,4 @@ export async function getExcerptByTopicId(id, wordCountLimit = 70) {
 export * as featureToggle from './featureToggle';
 export * as link from './link.utils';
 export * as form from './form.utils';
+export * as errors from './errors.utils';

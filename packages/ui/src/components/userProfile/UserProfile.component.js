b950544b9ce9417256f4676e5176f8b293cdab2d
634750802 <634750802@qq.com>
2021-05-12 17:50:34 +0800

    fix: add missing file

---
+++ packages/ui/src/components/userProfile/UserProfile.component.js
@@ -0,0 +1,59 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Avatar, Badge, Button, Dropdown, Menu } from 'antd';
+import { DownOutlined, UserOutlined } from '@ant-design/icons';
+
+import * as Styled from './userProfile.styled';
+import { menu as menuUtils } from '../../utils';
+import { t } from './userProfile.locale';
+
+const UserProfile = ({
+  avatarUrl,
+  items,
+  onLoginClick,
+  onLogoutClick,
+  locale,
+  currentNav,
+  onNavClick,
+  showBadge = false,
+}) => {
+  const _t = t(locale);
+
+  if (items) {
+    return (
+      <Dropdown
+        arrow
+        placement="bottomRight"
+        overlay={<Menu>{menuUtils.genMenu({ items, currentNav, onNavClick, onLogoutClick, _t })}</Menu>}
+      >
+        <Styled.UserButton type="text" size="small">
+          <Badge dot={showBadge}>
+            <Avatar size={20} src={avatarUrl}>
+              <UserOutlined />
+            </Avatar>
+          </Badge>
+          <DownOutlined />
+        </Styled.UserButton>
+      </Dropdown>
+    );
+  }
+
+  return (
+    <Button type="primary" size="small" icon={<UserOutlined />} onClick={onLoginClick}>
+      {_t('login')}
+    </Button>
+  );
+};
+
+UserProfile.propTypes = {
+  avatarUrl: PropTypes.string,
+  currentNav: PropTypes.string.isRequired,
+  items: PropTypes.array,
+  locale: PropTypes.oneOf(['zh', 'en']),
+  onLoginClick: PropTypes.func,
+  onLogoutClick: PropTypes.func,
+  onNavClick: PropTypes.func.isRequired,
+  showBadge: PropTypes.bool,
+};
+
+export default UserProfile;

a72b3ab518a50fe9689c2537a4cecb0af52f3d2a
Nick Qi <wei.qi.nick@gmail.com>
2021-05-12 22:24:22 +1200

    CPT-163 style: update docs' styles

--- src/pages/docs/layout/layout.styled.js
+++ src/pages/docs/layout/layout.styled.js
@@ -1,6 +1,37 @@
 import styled from 'styled-components';
-import { mixins } from '@tidb-community/ui';
+import { colors, mixins } from '@tidb-community/ui';
 
 export const Container = styled.div`
   ${mixins.responsive()};
+  color: ${colors.F2};
+
+  h1,
+  h2 {
+    color: ${colors.F1};
+  }
+
+  h1 {
+    margin: 4rem 0 2rem;
+    text-align: center;
+  }
+
+  h2 {
+    margin: 2rem 0 1rem;
+  }
+
+  ol {
+    list-style: none;
+  }
+
+  > ol {
+    padding-left: 0;
+  }
+
+  li {
+    margin-bottom: 1rem;
+
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
 `;

3b25c5380ed81b0d2ecb6b532e2d64e445db2b11
634750802 <634750802@qq.com>
2021-05-12 15:40:40 +0800

    CPT-115 fix: encode redirect uri in logout url

--- packages/datasource/src/nav/header/header.data-en.js
+++ packages/datasource/src/nav/header/header.data-en.js
@@ -186,7 +186,7 @@ export const genUserProfileItems = (meData) => {
 
   items.push({
     title: 'Logout',
-    link: `https://accounts.pingcap.com/logout?redirect_to=${window?.location?.href ?? ''}`,
+    link: `https://accounts.pingcap.com/logout?redirect_to=${encodeURI(window?.location?.href ?? '')}`,
     target: '_top',
   });
 

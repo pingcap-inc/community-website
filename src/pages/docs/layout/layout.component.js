e12976c535b933129aa2dda731370b9e9157bbe4
Nick Qi <wei.qi.nick@gmail.com>
2021-05-12 21:37:28 +1200

    CPT-163 feat: add agreement page and the content

---
+++ src/pages/docs/layout/layout.component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import * as Styled from './layout.styled';
+import { CoreLayout } from 'layouts';
+
+const Layout = ({ children }) => {
+  return (
+    <CoreLayout domain="tug.tidb.io" hasMargin>
+      <Styled.Container>{children}</Styled.Container>
+    </CoreLayout>
+  );
+};
+
+export default Layout;

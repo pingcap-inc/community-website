c073438d4a8aaaaa91648844d4bff394847fbfdf
634750802 <634750802@qq.com>
2021-05-13 13:31:52 +0800

    CPT-169 fix: error page style and me data feting

--- src/pages/_error.page.js
+++ src/pages/_error.page.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { createAppGlobalStyle } from '@tidb-community/ui';
+import { api } from '@tidb-community/datasource';
 
 import ErrorPage from 'components/errorPage';
+import { MeContext } from 'context';
 
-const Error = (props) => <ErrorPage {...props} />;
+const GlobalStyle = createAppGlobalStyle();
+
+const Error = (props) => {
+  const [meData, setMeData] = useState(undefined);
+
+  useEffect(() => {
+    api
+      .me()
+      .then(({ data }) => setMeData(data))
+      .catch(() => {});
+  });
+
+  return (
+    <>
+      <GlobalStyle />
+      <MeContext.Provider value={{ meData }}>
+        <ErrorPage {...props} />
+      </MeContext.Provider>
+    </>
+  );
+};
 
 Error.getInitialProps = ({ res, err }) => {
   const statusCode = res ? res.statusCode : err ? err.statusCode : 404;

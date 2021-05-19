ccedd9808b7b22f41ef575dc29b0a7b288eb463c
634750802 <634750802@qq.com>
2021-05-19 19:50:57 +0800

    CPT-106 fix: form reset bug

--- src/pages/account/organization/new/form/fields/VerificationFields.component.js
+++ src/pages/account/organization/new/form/fields/VerificationFields.component.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { Radio, Form as AntForm } from 'formik-antd';
+import React, { useEffect, useRef } from 'react';
 import { useFormikContext } from 'formik';
+import { Radio, Form as AntForm } from 'formik-antd';
 
 import EmailVerificationOption from './EmailVerificationOption.component';
 import IncumbencyCertOption from './IncumbencyCertOption.component';
@@ -8,25 +8,33 @@ import data from '../form.data';
 
 const { verificationType } = data.form;
 
-const VerificationFields = () => {
-  const [type, setType] = useState(verificationType.options[0].value);
-
+// reset form state when field change
+// returns the value of the field
+// TODO: this hook could be reused in other forms
+const useFormikFieldAsOption = (fieldName) => {
   const { resetForm, values } = useFormikContext();
+  const type = values[fieldName];
 
-  const onTypeChange = (e) => {
-    setType(e.target.value);
-    resetForm({ values });
-  };
+  const localState = useRef({});
+
+  useEffect(() => {
+    Object.assign(localState.current, values);
+  }, [localState, values]);
+
+  useEffect(() => {
+    resetForm({ values: localState.current });
+  }, [localState, resetForm, type]);
+
+  return type;
+};
+
+const VerificationFields = () => {
+  const type = useFormikFieldAsOption(verificationType.name);
 
   return (
     <>
       <AntForm.Item name={verificationType.name}>
-        <Radio.Group
-          name={verificationType.name}
-          options={verificationType.options}
-          onChange={onTypeChange}
-          value={type}
-        />
+        <Radio.Group name={verificationType.name} options={verificationType.options} value={type} />
       </AntForm.Item>
       <EmailVerificationOption hidden={type !== 0} />
       <IncumbencyCertOption hidden={type !== 1} />

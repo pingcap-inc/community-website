51732b8a0a3706bb3562d09160900c64b9ba6767
std4453 <me@std4453.com>
2021-05-23 07:29:02 +0800

    CPT-155 feat: introduce shipit (#108)
    * CPT-155 feat: use pm2 cluster mode
    
    According to https://github.com/vercel/next.js/discussions/10675,
    ecosystem.config.js must use the `next` executable as `script` field,
    instead of `npm run start`, otherwise cluster mode won't work.
    
    * CPT-155 feat: use shipit for deployment
    
    * CPT-155 fix: fix a rebase problem with duplicated pm2 configs
    
    * CPT-155 fix: update shipitfile.js to match reload script name
    
    * CPT-155 fix: update shipit config and test on the feature branch
    
    Co-authored-by: Nick Qi <wei.qi.nick@gmail.com>

--- pm2.config.js
+++ pm2.config.js
@@ -1,9 +1,18 @@
+// The configuration is referred to
+// https://pm2.keymetrics.io/docs/tutorials/capistrano-like-deployments
 module.exports = {
   apps: [
     {
       name: 'tug-website-next-server',
+
+      // Provide the relative address otherwise PM2 cannot identify the next command
       script: 'node_modules/.bin/next',
       args: 'start',
+
+      // `cwd` is used for resolving a symlink related issue mentioned below:
+      // https://pm2.keymetrics.io/docs/tutorials/capistrano-like-deployments#the-main-issue
+      // Otherwise, PM2 will readlink first and break the server reload.
+      cwd: process.env.PWD,
       instances: 2,
       exec_mode: 'cluster',
     },
---
+++ shipitfile.js
@@ -0,0 +1,43 @@
+// Learnt from:
+// https://git.vnv.ch/snippets/17#more-documentations-options
+// https://www.digitalocean.com/community/tutorials/how-to-automate-your-node-js-production-deployments-with-shipit-on-centos-7
+module.exports = (shipit) => {
+  require('shipit-deploy')(shipit);
+
+  shipit.initConfig({
+    default: {
+      workspace: '.',
+      deployTo: process.env.DEPLOY_SERVICE_PATH,
+
+      // The node_modules of packages are not sent since they've been bundled in the
+      // main server. The root node_modules is still needed for starting the app.
+      ignores: ['.git', 'packages/**/node_modules'],
+      keepReleases: 5,
+
+      // The workspace dir won't be removed after deploy
+      keepWorkspace: true,
+
+      // Disable the setup of Git repository (fetch, pull, merge, submodules, etc.)
+      // because the current context is already up to date(CI/ CD)
+      shallowClone: false,
+      branch: 'HEAD',
+    },
+
+    production: {
+      servers: {
+        user: process.env.HOST_HK_1_USER,
+        host: process.env.HOST_HK_1_IP,
+      },
+    },
+  });
+
+  shipit.on('published', () => {
+    shipit.start('server:reload');
+  });
+
+  shipit.blTask('server:reload', async () => {
+    await shipit.remote(`npm run server:reload`, {
+      cwd: `${process.env.DEPLOY_SERVICE_PATH}/current/project`,
+    });
+  });
+};

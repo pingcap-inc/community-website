5511912950e9180adafc4f50959614b3e52ee3a3
Nick Qi <wei.qi.nick@gmail.com>
2021-05-23 17:08:34 +1200

    CPT-185 fix: fix CI build failure

--- src/utils/form.utils.js
+++ src/utils/form.utils.js
@@ -1,4 +1,5 @@
 import { api } from '@tidb-community/datasource';
 import { utils } from '@tidb-community/common';
 
-export const wrapFormikSubmitFunction = (func) => utils.wrapFormikSubmitFunction(func, api.events.dispatchApiError);
+export const wrapFormikSubmitFunction = (func) =>
+  utils.form.wrapFormikSubmitFunction(func, api.events.dispatchApiError);

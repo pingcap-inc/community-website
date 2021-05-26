2aec0bb7b4141e18ef970f7c47d69249af06a1e0
Nick Qi <wei.qi.nick@gmail.com>
2021-05-26 22:11:43 +1200

    CPT-229 feat: add user profile related mocks (#123)
    * CPT-229 feat: mock profile API
    
    * CPT-229 feat: randomly generate null values
    
    * CPT-229 feat: add account settings mock
    
    * CPT-229 feat: add set phone mocks
    
    * CPT-229 feat: add set email mocks
    
    * CPT-229 feat: add set password mocks
    
    * CPT-229 feat: add disconnect API mock

---
+++ packages/mockApi/routes/profile.js
@@ -0,0 +1,73 @@
+const _ = require('lodash');
+const dayjs = require('dayjs');
+const faker = require('faker');
+const router = require('express').Router();
+
+const utils = require('../utils');
+
+const { datatype } = faker;
+
+const positionKeys = Object.keys(require('@tidb-community/datasource/lib/form/org/personal-positions.json'));
+
+router.get('', async (req, res) => {
+  await utils.wait();
+
+  // return utils.errorResp({
+  //   code: 401,
+  //   detail: 'Authentication credentials were not provided',
+  // })(req, res);
+
+  const data = utils.generator(
+    {
+      avatar_url: '{{image.avatar}}',
+      username: '{{internet.userName}}',
+      username_last_modified_at: '{{date.past}}',
+      bio: '{{lorem.paragraphs}}',
+      name: '{{name.findName}}',
+      date_of_birth: '{{date.past}}',
+      address: '{{address.streetAddress}}, {{address.city}}, {{address.country}}',
+      company_name: '{{company.companyName}}',
+    },
+    {
+      min: 1,
+      callback: (item) => {
+        const data = {
+          ...item,
+          username_last_modified_at: dayjs(item.username_last_modified_at).format('YYYY-MM-DD HH:mm:ss'),
+          gender: datatype.number({ min: 0, max: 1 }),
+          date_of_birth: dayjs(item.date_of_birth, 'YYYY-MM-DD'),
+          job_title: _.sample(positionKeys),
+        };
+
+        _.sampleSize(
+          [
+            'username_last_modified_at',
+            'bio',
+            'name',
+            'date_of_birth',
+            'address',
+            'company_name',
+            'gender',
+            'job_title',
+          ],
+          3
+        ).forEach((key) => {
+          data[key] = null;
+        });
+
+        return data;
+      },
+    }
+  );
+
+  utils.successResp({
+    data,
+  })(req, res);
+});
+
+router.patch('', async (req, res) => {
+  await utils.wait();
+  utils.successResp()(req, res);
+});
+
+module.exports = router;

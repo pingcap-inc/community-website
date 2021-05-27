ec211cb126990dd05ec1728b40803117511b1978
Nick Qi <wei.qi.nick@gmail.com>
2021-05-27 21:27:31 +1200

    CPT-232 style: construct the UI for the company info page (#124)
    * CPT-232 style: update company page layout
    
    * CPT-232 feat: implement position dropdown

--- src/pages/users/[uid]/company/index.page.js
+++ src/pages/users/[uid]/company/index.page.js
@@ -1,7 +1,58 @@
+import Link from 'next/link';
 import React from 'react';
+import { Button, Col, Row, Form, Input, Select } from 'antd';
+import { getFormData } from '@tidb-community/datasource';
 
+import * as Styled from './company.styled';
 import Layout from '~/pages/users/layout';
+import { CommunityHead } from '~/components/head';
 
-const Company = () => <Layout title="公司信息">Content</Layout>;
+const pageTitle = '公司信息';
+const formData = getFormData();
+const { personalPositions } = formData.org.enums;
+const { Option } = Select;
+
+const Company = () => (
+  <>
+    <CommunityHead title={pageTitle} />
+
+    <Layout title={pageTitle}>
+      <Styled.Alert
+        type="info"
+        showIcon
+        message={
+          <>
+            完成公司信息填写可 +20 积分，完成认证可以获得 +200 经验值，+200 积分
+            <Link href="/account/organization/new">点击完成认证</Link>
+          </>
+        }
+      />
+
+      <Form layout="vertical">
+        <Row gutter={32}>
+          <Col sm={24} md={12}>
+            <Form.Item label="公司名称">
+              <Input placeholder="请输入" />
+            </Form.Item>
+
+            <Form.Item label="职位">
+              <Select placeholder="请选择">
+                {personalPositions.map(({ value, label }) => (
+                  <Option key={value} value={value}>
+                    {label}
+                  </Option>
+                ))}
+              </Select>
+            </Form.Item>
+
+            <Button type="primary">更新信息</Button>
+          </Col>
+
+          <Col sm={24} md={12} />
+        </Row>
+      </Form>
+    </Layout>
+  </>
+);
 
 export default Company;

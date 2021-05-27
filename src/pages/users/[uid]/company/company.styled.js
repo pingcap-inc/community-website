870ad80060ba301b02bb134cb489bcfcea47abf8
Nick Qi <wei.qi.nick@gmail.com>
2021-05-27 21:27:31 +1200

    CPT-232 style: construct the UI for the company info page (#124)
    * CPT-232 style: update company page layout
    
    * CPT-232 feat: implement position dropdown

---
+++ src/pages/users/[uid]/company/company.styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { Alert as AntAlert } from 'antd';
+
+export const Alert = styled(AntAlert)`
+  margin-bottom: 2rem;
+
+  a {
+    margin-left: 0.5rem;
+  }
+`;
--- src/pages/users/[uid]/settings/index.page.js
+++ src/pages/users/[uid]/settings/index.page.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 import Layout from '~/pages/users/layout';
+import { CommunityHead } from '~/components/head';
 
-const Settings = () => <Layout title="账号设置">Content</Layout>;
+const pageTitle = '账号设置';
+
+const Settings = () => (
+  <>
+    <CommunityHead title={pageTitle} />
+
+    <Layout title="账号设置"></Layout>
+  </>
+);
 
 export default Settings;

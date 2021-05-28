5af9ca449157a64c769dc02b5bf474bc4baaf276
Nick Qi <wei.qi.nick@gmail.com>
2021-05-28 21:25:14 +1200

    CPT-192 feat: update stylings for the account settings (#125)

--- src/pages/users/[uid]/settings/index.page.js
+++ src/pages/users/[uid]/settings/index.page.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Button } from 'antd';
 
+import * as Styled from './settings.styled';
+import Box from './box';
 import Layout from '~/pages/users/layout';
 import { CommunityHead } from '~/components/head';
 
@@ -9,7 +12,24 @@ const Settings = () => (
   <>
     <CommunityHead title={pageTitle} />
 
-    <Layout title="账号设置"></Layout>
+    <Layout title="账号设置">
+      <Box title="手机号码" text="130****1234" />
+      <Box title="邮箱" text="li**@gmail.com" />
+      <Box title="密码" text="未设置" />
+
+      <Box>
+        <h2>绑定第三方账号</h2>
+        <Styled.Desc>绑定后可通过第三方应用快速登录，并获得 xxx 积分奖励</Styled.Desc>
+        <Styled.SocialAccounts>
+          <Button type="text" icon={<Styled.GithubIcon />}>
+            绑定 GitHub
+          </Button>
+          <Button type="text" icon={<Styled.WeChatIcon />}>
+            绑定微信
+          </Button>
+        </Styled.SocialAccounts>
+      </Box>
+    </Layout>
   </>
 );
 

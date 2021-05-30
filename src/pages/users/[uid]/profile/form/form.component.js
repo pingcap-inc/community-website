361a8474f69d0555823caed0a59e1280c1c9bb58
Nick Qi <wei.qi.nick@gmail.com>
2021-05-30 16:41:04 +1200

    CPT-223 feat: fill out the form with back-end data

--- src/pages/users/[uid]/profile/form/form.component.js
+++ src/pages/users/[uid]/profile/form/form.component.js
@@ -1,49 +1,64 @@
 import React from 'react';
-import { Button, Col, Row, Form, Input } from 'antd';
+import moment from 'moment';
+import { Button, Col, Row, Form, Input, Select } from 'antd';
 
 import * as Styled from './form.styled';
 
+const { Item } = Form;
+const { Option } = Select;
+
+const dateFormat = 'YYYY/MM/DD';
+
 const FormComponent = ({ data = {} }) => {
+  const { date_of_birth: dateOfBirth } = data;
+
   return (
     <Form layout="vertical">
       <Row gutter={32}>
         <Col sm={24} md={12}>
-          <Form.Item label="用户名">
+          <Item label="用户名">
             <Input placeholder="请输入" defaultValue={data.username} />
-          </Form.Item>
+          </Item>
 
-          <Form.Item label="简介">
+          <Item label="简介">
             <Input.TextArea placeholder="介绍一下你自己吧" showCount maxLength={140} defaultValue={data.bio} />
-          </Form.Item>
+          </Item>
 
-          <Form.Item label="姓名">
-            <Input placeholder="请填写真实姓名" />
-          </Form.Item>
+          <Item label="姓名">
+            <Input placeholder="请填写真实姓名" defaultValue={data.name} />
+          </Item>
 
           <Row gutter={32}>
             <Col sm={24} md={10}>
-              <Form.Item label="性别">
-                <Input placeholder="" />
-              </Form.Item>
+              <Item label="性别">
+                <Select defaultValue={data.gender} placeholder="请选择">
+                  <Option value={0}>男</Option>
+                  <Option value={1}>女</Option>
+                </Select>
+              </Item>
             </Col>
             <Col sm={24} md={14}>
-              <Form.Item label="出生日期">
-                <Styled.DatePicker placeholder="年/月/日" format="YYYY/MM/DD" />
-              </Form.Item>
+              <Item label="出生日期">
+                <Styled.DatePicker
+                  placeholder="年/月/日"
+                  format={dateFormat}
+                  defaultValue={dateOfBirth && moment(dateOfBirth, dateFormat)}
+                />
+              </Item>
             </Col>
           </Row>
 
-          <Form.Item label="地址">
-            <Input placeholder="请填写详细地址" />
-          </Form.Item>
+          <Item label="地址">
+            <Input placeholder="请填写详细地址" defaultValue={data.address} />
+          </Item>
 
           <Button type="primary">更新信息</Button>
         </Col>
 
         <Col sm={24} md={12}>
-          <Form.Item label="头像">
+          <Item label="头像">
             <Styled.Avatar src={data.avatar_url} />
-          </Form.Item>
+          </Item>
         </Col>
       </Row>
     </Form>

421022a5ba20765ea28981de974cdf93b26e51b4
Nick Qi <wei.qi.nick@gmail.com>
2021-05-31 22:26:37 +1200

    CPT-238 refactor: refactor Layout

--- src/pages/my/layout/Layout.component.js
+++ src/pages/my/layout/Layout.component.js
@@ -6,32 +6,28 @@ import Menu from './menu';
 import { CommunityHead } from '~/components/head';
 import { CoreLayout } from '~/layouts';
 
-const Layout = ({ children, title }) => {
-  const recaptchaKey = process.env.NEXT_PUBLIC_RECAPTCHA_KEY;
+const Layout = ({ children, title }) => (
+  <>
+    <CommunityHead title={title}>
+      <script src={`https://www.recaptcha.net/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_KEY}`} />
+    </CommunityHead>
 
-  return (
-    <>
-      <CommunityHead title={title}>
-        <script src={`https://www.recaptcha.net/recaptcha/api.js?render=${recaptchaKey}`} />
-      </CommunityHead>
-
-      <CoreLayout domain="tidb.io" hasMargin>
-        <Styled.Container>
-          <Row gutter={32}>
-            <Col xs={12} sm={8} md={6}>
-              <Menu />
-            </Col>
-            <Col xs={12} sm={16} md={18}>
-              <Styled.Main>
-                <Styled.Title>{title}</Styled.Title>
-                <Styled.Content>{children}</Styled.Content>
-              </Styled.Main>
-            </Col>
-          </Row>
-        </Styled.Container>
-      </CoreLayout>
-    </>
-  );
-};
+    <CoreLayout domain="tidb.io" hasMargin>
+      <Styled.Container>
+        <Row gutter={32}>
+          <Col xs={12} sm={8} md={6}>
+            <Menu />
+          </Col>
+          <Col xs={12} sm={16} md={18}>
+            <Styled.Main>
+              <Styled.Title>{title}</Styled.Title>
+              <Styled.Content>{children}</Styled.Content>
+            </Styled.Main>
+          </Col>
+        </Row>
+      </Styled.Container>
+    </CoreLayout>
+  </>
+);
 
 export default Layout;

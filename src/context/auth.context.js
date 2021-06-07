11b76b0be49ef381bacc31f6b0ca2d33b06e48ae
Nick Qi <wei.qi.nick@gmail.com>
2021-06-07 12:07:44 +1200

    CPT-183 refactor: extract auth related functions into AuthContext

--- src/context/auth.context.js
+++ src/context/auth.context.js
@@ -1,32 +1,18 @@
 import { createContext } from 'react';
 
-// FIXME: It is a temporary fix and the auth issue will be thoroughly handled in CPT-183
-const REG_AUTH_PATH = /https?:\/\/([^/]+)\/(?:account|orgs|my)\//;
 const accountsBaseUrl = process.env.NEXT_PUBLIC_ACCOUNTS_BASE_URL;
 const loginUrl = `${accountsBaseUrl}/login`;
 const logoutUrl = `${accountsBaseUrl}/logout`;
-const homeUrl = 'https://tidb.io/';
 
 const login = function (redirectUrl) {
-  window.open(`${loginUrl}?redirect_to=${encodeURIComponent(redirectUrl ?? window.location.href)}`, '_top');
+  const { location } = window;
+  location.href = `${loginUrl}?redirect_to=${encodeURIComponent(redirectUrl ?? window.location.href)}`;
 };
 
 const logout = function (redirectUrl) {
-  redirectUrl = redirectUrl ?? window.location.href;
-  let url;
-  // do not redirect back to needs-login pages
-  if (REG_AUTH_PATH.test(redirectUrl)) {
-    if (!/^http/.test(homeUrl)) {
-      url = `${window.location.protocol}//${window.location.hostname}${
-        window.location.port ? `:${window.location.port}` : ''
-      }${homeUrl}`;
-    } else {
-      url = homeUrl;
-    }
-  } else {
-    url = redirectUrl;
-  }
-  window.open(`${logoutUrl}?redirect_to=${encodeURIComponent(url)}`, '_top');
+  const { location } = window;
+  redirectUrl = redirectUrl ?? this.isAuthRequired ? location.origin : location.href;
+  location.href = `${logoutUrl}?redirect_to=${encodeURIComponent(redirectUrl)}`;
 };
 
 export const context = {
@@ -35,7 +21,7 @@ export const context = {
 
 context.login = login.bind(context);
 
-context.logout = logout.bind(logout);
+context.logout = logout.bind(context);
 
 context.setIsAuthRequired = (isRequired) => (context.isAuthRequired = isRequired);
 

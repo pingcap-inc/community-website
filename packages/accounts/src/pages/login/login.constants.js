6ce0bb4d222a21e09cce6de61e4e7758286f0cad
634750802 <634750802@qq.com>
2021-06-09 16:21:18 +0800

    chore: update password login title

--- packages/accounts/src/pages/login/login.constants.js
+++ packages/accounts/src/pages/login/login.constants.js
@@ -5,6 +5,6 @@ export const LOGIN_TYPE = {
 
 export const LOGIN_TYPE_NAME = [];
 LOGIN_TYPE_NAME[LOGIN_TYPE.VERIFY_CODE] = '验证码登录';
-LOGIN_TYPE_NAME[LOGIN_TYPE.PASSWORD] = '密码登录';
+LOGIN_TYPE_NAME[LOGIN_TYPE.PASSWORD] = '账号密码登录';
 
 export const LOGIN_TYPE_VALUE_NAME = 'loginType';

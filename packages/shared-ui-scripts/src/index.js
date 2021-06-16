27483796649026dcc70856a47e1a4115707fc836
std4453 <me@std4453.com>
2021-06-16 20:57:31 +0800

    CPT-262 feat: use auth context from parent package

--- packages/shared-ui-scripts/src/index.js
+++ packages/shared-ui-scripts/src/index.js
@@ -1,57 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ReactDOM from 'react-dom';
-
+import useSWR from 'swr';
 import { Header, Footer, UserProfile, createAppGlobalStyle, utils } from '@tidb-community/ui';
 import { getData, api } from '@tidb-community/datasource';
 
-import useSWR from 'swr';
+import { AuthContext } from '../../../src/context';
+
 import * as R from 'ramda';
 import './global.less';
 
-const REG_AUTH_PATH = /https?:\/\/([^/]+)\/(?:account|orgs)\//;
-const loginUrl = 'https://accounts.pingcap.com/login';
-const logoutUrl = 'https://accounts.pingcap.com/logout';
-const homeUrl = 'https://tidb.io/';
-
-const doLogin = (redirectUrl) => {
-  window.open(`${loginUrl}?redirect_to=${encodeURIComponent(redirectUrl ?? window.window.location.href)}`, '_top');
-};
-
-const doLogout = (redirectUrl) => {
-  redirectUrl = redirectUrl ?? window.window.location.href;
-  let url;
-  // do not redirect back to needs-login pages
-  if (REG_AUTH_PATH.test(redirectUrl)) {
-    if (!/^http/.test(homeUrl)) {
-      url = `${window.window.location.protocol}//${window.window.location.hostname}${
-        window.window.location.port ? `:${window.window.location.port}` : ''
-      }${homeUrl}`;
-    } else {
-      url = homeUrl;
-    }
-  } else {
-    url = redirectUrl;
-  }
-  window.open(`${logoutUrl}?redirect_to=${encodeURIComponent(url)}`, '_top');
-};
-
-const fetcher = (path, params) => {
-  // SWR shallowly compares the arguments on every render, and triggers revalidation
-  // if any of them has changed. Thus, if you'd like to pass an object as params to
-  // the API call, you may use JSON.stringify to the object params to a string value.
-  // Read more: https://swr.vercel.app/docs/arguments#passing-objects
-  try {
-    params = JSON.parse(params);
-  } catch (err) {}
-
-  return R.path(path.split('.'), api)(params);
-};
-
 const GlobalStyle = createAppGlobalStyle();
 
-const Page = () => {
-  const locale = 'zh';
+const Page = ({
+  locale = 'zh',
+  title = 'TiDB Community',
+  logoSrc = 'https://tidb.io/images/community/logo.svg',
+  homeUrl = '/',
+  hasMargin = true,
+}) => {
+  const { login, logout } = useContext(AuthContext);
 
+  const fetcher = (path, params) => {
+    // SWR shallowly compares the arguments on every render, and triggers revalidation
+    // if any of them has changed. Thus, if you'd like to pass an object as params to
+    // the API call, you may use JSON.stringify to the object params to a string value.
+    // Read more: https://swr.vercel.app/docs/arguments#passing-objects
+    try {
+      params = JSON.parse(params);
+    } catch (err) {}
+
+    return R.path(path.split('.'), api)(params);
+  };
   const { data: meResp } = useSWR('me', fetcher, {
     revalidateOnFocus: false,
   });
@@ -66,6 +45,8 @@ const Page = () => {
   const { navItems: headerNavItems, userProfileNavItems } = data.header;
   const { navItems: footerNavItems, icons: footerIcons } = data.footer;
 
+  const logo = <img alt={title} src={logoSrc} />;
+
   const onNavClick = ({ link, isSelected }) => {
     if (isSelected) return;
     window.location.href = link;
@@ -73,8 +54,6 @@ const Page = () => {
 
   const currentNav = utils.header.getCurrentNav(headerNavItems, window.location.pathname);
 
-  const title = 'TiDB Community';
-  const logo = <img src="https://tidb.io/images/community/logo.svg" />;
   const headerProps = {
     logo,
     title,
@@ -82,16 +61,17 @@ const Page = () => {
     navItems: headerNavItems,
     currentNav,
     onTitleClick: () => {
-      window.location.href = '/';
+      window.location.href = homeUrl;
     },
   };
+
   const footerProps = {
     logo,
     title,
     onNavClick: (link) => onNavClick({ link }),
     navItems: footerNavItems,
     icons: footerIcons,
-    hasMargin: true,
+    hasMargin,
   };
 
   return (
@@ -102,13 +82,12 @@ const Page = () => {
         userProfileSlot={
           <UserProfile
             onNavClick={onNavClick}
-            onLoginClick={() => doLogin()}
-            onLogoutClick={() => doLogout()}
+            onLoginClick={() => login()}
+            onLogoutClick={() => logout()}
             currentNav={currentNav}
             items={userProfileNavItems}
             avatarUrl={meData?.avatar_url}
             locale={locale}
-            showBadge={meData?.org_invitations.reduce((pre, cur) => pre + (cur.valid ? 1 : 0), 0) > 0}
           />
         }
       />

722675b73b112a206705cdba32b08b4ee4bdf250
Nick Qi <wei.qi.nick@gmail.com>
2021-06-19 20:31:50 +1200

    CPT-280 feat: read json data and remove unused npm scripts

--- packages/i18n/src/genExcels/index.js
+++ packages/i18n/src/genExcels/index.js
@@ -1,1 +1,43 @@
+const fs = require('fs');
+const path = require('path');
 const glob = require('glob');
+
+const rootPath = fs.realpathSync(process.cwd());
+const resolveRoot = (relativePath) => path.resolve(rootPath, relativePath);
+
+const getNamespaces = (filePattern = 'locales/*/*.json') => {
+  const files = glob.sync(filePattern);
+  const re = /\/(((?!\/).)*)\.json$/;
+
+  return new Set(
+    files
+      .map((file) => {
+        const m = re.exec(file);
+        return m && m[1];
+      })
+      .filter(Boolean)
+  );
+};
+
+const genExcel = (namespace, locales) => {
+  locales.forEach((locale) => {
+    console.log({
+      namespace,
+      locale,
+    });
+
+    let json = {};
+    try {
+      json = fs.readFileSync(resolveRoot(`locales/${locale}/${namespace}.json`), 'utf8');
+    } catch (err) {}
+    console.log(json);
+  });
+};
+
+(async () => {
+  const namespaces = getNamespaces();
+
+  namespaces.forEach((ns) => {
+    genExcel(ns, ['zh', 'en']);
+  });
+})();

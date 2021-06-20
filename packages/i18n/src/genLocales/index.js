321443c48984893a2d7e608321a218cbe1ce6d59
Nick Qi <wei.qi.nick@gmail.com>
2021-06-20 22:46:07 +1200

    CPT-280 feat: implement genLocales script

--- packages/i18n/src/genLocales/index.js
+++ packages/i18n/src/genLocales/index.js
@@ -1,19 +1,37 @@
 const ExcelJS = require('exceljs');
+const _ = require('lodash');
+const fs = require('fs');
+const jsonPointer = require('json-pointer');
+const mkdirp = require('mkdirp');
+const path = require('path');
+const rimraf = require('rimraf');
 
 const utils = require('./utils');
 const { resolveRoot } = require('../utils');
 
 const genLocale = async (namespace) => {
   const workbook = new ExcelJS.Workbook();
-  await workbook.xlsx.readFile(resolveRoot(`./excels/${namespace}.xlsx`));
+  await workbook.xlsx.readFile(path.resolve(`./excels/${namespace}.xlsx`));
 
   const sheet = workbook.getWorksheet(namespace);
   const data = utils.prepareLocaleData(sheet);
 
-  console.log(data);
+  for (const locale in data) {
+    const r = {};
+    _.toPairs(data[locale]).forEach(([langKey, value]) => {
+      _.set(r, jsonPointer.parse(langKey), value);
+    });
+
+    const filename = resolveRoot(`./locales/${namespace}/${locale}.json`);
+    mkdirp.sync(resolveRoot(`./locales/${namespace}`));
+    fs.writeFileSync(filename, JSON.stringify(r, null, 2));
+  }
 };
 
 (() => {
+  const localesDir = resolveRoot('./locales');
+  rimraf.sync(localesDir);
+
   const namespaces = utils.getNamespaces();
   namespaces.forEach((namespace) => {
     genLocale(namespace);

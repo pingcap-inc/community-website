3c97c4211be8c9c902f280a217c7c379b86341d9
Nick Qi <wei.qi.nick@gmail.com>
2021-06-20 20:48:35 +1200

    CPT-280 feat: build excel files if locales update

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,12 @@
-module.exports = {
-  '*.{js,jsx,css,json,md,scss,ts,tsx}': ['prettier --write', 'git add'],
+const micromatch = require('micromatch');
+
+module.exports = (allStagedFiles) => {
+  const codeFiles = micromatch(allStagedFiles, ['**/*.{js,jsx,css,json,md,scss,ts,tsx}']);
+  const localeFiles = micromatch(allStagedFiles, ['**/packages/i18n/locales/*/*.json']);
+
+  return [
+    codeFiles.length && `prettier --write ${codeFiles.join(' ')}`,
+    localeFiles.length && 'npm run i18n:genExcels',
+    'git add .',
+  ].filter(Boolean);
 };

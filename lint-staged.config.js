4c378ab4e5ee2e2e64ac9cbb6293a176e10a8797
Nick Qi <wei.qi.nick@gmail.com>
2021-06-23 22:43:02 +1200

    CPT-83 fix: remove auto-generating locale and excel files

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,12 +2,13 @@ const micromatch = require('micromatch');
 
 module.exports = (allStagedFiles) => {
   const codeFiles = micromatch(allStagedFiles, ['**/*.{js,jsx,css,json,md,scss,ts,tsx}']);
-  const excelFiles = micromatch(allStagedFiles, ['**/packages/i18n/excels/*.xlsx']);
-  const localeFiles = micromatch(allStagedFiles, ['**/packages/i18n/locales/*/*.json']);
+  // TODO: will use Feishu for i18n management
+  // const excelFiles = micromatch(allStagedFiles, ['**/packages/i18n/excels/*.xlsx']);
+  // const localeFiles = micromatch(allStagedFiles, ['**/packages/i18n/locales/*/*.json']);
 
   return [
-    excelFiles.length && 'npm run i18n:genLocales',
-    localeFiles.length && 'npm run i18n:genExcels',
+    // excelFiles.length && 'npm run i18n:genLocales',
+    // localeFiles.length && 'npm run i18n:genExcels',
     codeFiles.length && `prettier --write ${codeFiles.join(' ')}`,
     'git add .',
   ].filter(Boolean);

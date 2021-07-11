6adf6f2d43b1cb27abfc38588bafae4b528ac5ea
Nick Qi <wei.qi.nick@gmail.com>
2021-07-11 22:36:45 +1200

    CP-62 perf: optimize css loader

--- packages/asktug/webpack.config.js
+++ packages/asktug/webpack.config.js
@@ -32,12 +32,7 @@ module.exports = {
       },
 
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-
-      {
-        test: /\.scss$/,
+        test: /\.s?css$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
 

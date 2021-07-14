7bf18c248755f3076a7e73406cde835977c67e96
Nick Qi <wei.qi.nick@gmail.com>
2021-07-14 16:37:36 +1200

    CP-63 feat: add mock APIs for contact us features (#189)

--- packages/mockApi/routes/index.js
+++ packages/mockApi/routes/index.js
@@ -6,6 +6,7 @@ const router = require('express').Router();
 router.use('/accounts', require('./accountsSite'));
 
 router.use('/api/account', require('./account'));
+router.use('/api/contact-us', require('./contactUs'));
 router.use('/api/forgot', require('./forgot'));
 router.use('/api/login', require('./login'));
 router.use('/api/me', require('./me'));
@@ -14,5 +15,6 @@ router.use('/api/orgs', require('./orgs'));
 router.use('/api/profile', require('./profile'));
 router.use('/api/signup', require('./signup'));
 router.use('/api/social', require('./social'));
+router.use('/api/tidb-releases', require('./tidbReleases'));
 
 module.exports = router;

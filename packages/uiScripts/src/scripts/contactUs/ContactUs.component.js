4d69f024d8369996c538b20ec56c9dc5796013e6
Nick Qi <wei.qi.nick@gmail.com>
2021-07-17 17:39:06 +1200

    CP-64 style: implement contact-ui icon

--- packages/uiScripts/src/scripts/contactUs/ContactUs.component.js
+++ packages/uiScripts/src/scripts/contactUs/ContactUs.component.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { createAppGlobalStyle, constants } from '@tidb-community/ui';
 
 import * as Styled from './contactUs.styled';
+import Icon from './icon.svg';
 
 const GlobalStyle = createAppGlobalStyle();
 
 const ContactUs = () => (
   <>
     <GlobalStyle />
-    <Styled.Container className={constants.appClassName}>Contact Us</Styled.Container>
+    <Styled.Container className={constants.appClassName}>
+      <Icon />
+    </Styled.Container>
   </>
 );
 export default ContactUs;
--- packages/uiScripts/src/scripts/contactUs/contactUs.styled.js
+++ packages/uiScripts/src/scripts/contactUs/contactUs.styled.js
@@ -2,7 +2,11 @@ import styled from 'styled-components';
 import { colors, mixins } from '@tidb-community/ui';
 
 export const Container = styled.div`
+  ${mixins.size('42px')};
+  ${mixins.flexCenter()};
+  border-radius: 50%;
+  background: ${colors.B1};
   position: fixed;
-  right: 30px;
-  bottom: 30px;
+  right: 32px;
+  bottom: 32px;
 `;

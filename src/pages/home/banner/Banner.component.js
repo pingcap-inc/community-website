74d2d200bb4dbf08f9557c0f408b30147851930f
Nick Qi <wei.qi.nick@gmail.com>
2021-08-15 21:06:06 +1200

    CP-109 style: add stylings for the left banner panel

--- src/pages/home/banner/Banner.component.js
+++ src/pages/home/banner/Banner.component.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Row } from 'antd';
 import { useTranslation } from 'next-i18next';
 
 import * as Styled from './banner.styled';
@@ -8,7 +9,19 @@ const Banner = () => {
 
   const lang = t('banner', { returnObjects: true });
 
-  return <Styled.Container>{lang.title}</Styled.Container>;
+  return (
+    <Styled.Container>
+      <Styled.Content>
+        <Row gutter={32} justify="space-between" align="middle">
+          <Styled.LeftPanel>
+            <Styled.Logo />
+            <Styled.Intro>{lang.intro}</Styled.Intro>
+          </Styled.LeftPanel>
+          <Styled.RightPanel>Image</Styled.RightPanel>
+        </Row>
+      </Styled.Content>
+    </Styled.Container>
+  );
 };
 
 export default Banner;
--- src/pages/home/banner/banner.styled.js
+++ src/pages/home/banner/banner.styled.js
@@ -1,11 +1,40 @@
 import styled from 'styled-components';
-import { colors } from '@tidb-community/ui';
+import { colors, mixins } from '@tidb-community/ui';
+import { Col } from 'antd';
 
+import LogoSvg from './logo.svg';
 import { Section } from '~/pages/home/index.styled';
 
 export const Container = styled(Section)`
   && {
     color: ${colors.M1};
     background: ${colors.B4};
+    padding: 5rem 0 7.5rem;
   }
+`;
+
+export const Content = styled.div`
+  ${mixins.responsive()};
+  width: 100%;
+`;
+
+export const LeftPanel = styled(Col).attrs({
+  md: 10,
+  lg: 8,
+})``;
+
+export const RightPanel = styled(Col).attrs({
+  md: 14,
+  lg: 12,
+})``;
+
+export const Logo = styled(LogoSvg)`
+  max-width: 350px;
+  margin-bottom: 0.5rem;
+`;
+
+export const Intro = styled.p`
+  ${mixins.reset()};
+  color: ${colors.M1};
+  font-size: 20px;
 `;

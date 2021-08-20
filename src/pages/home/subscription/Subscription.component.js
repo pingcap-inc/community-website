5b03b2c1812c06f23d44c7f9d9f503e0c6b05a81
Xiyan Shao <xiyan128@gmail.com>
2021-08-20 17:37:41 +0800

    CP-120 feat: implemented UI for the subscription section

--- src/pages/home/subscription/Subscription.component.js
+++ src/pages/home/subscription/Subscription.component.js
@@ -2,13 +2,56 @@ import React from 'react';
 import { useTranslation } from 'next-i18next';
 
 import * as Styled from './subscription.styled';
+import { Row, Col, Grid } from 'antd';
+import { SloganBox } from './subscription.styled';
+
+const { useBreakpoint } = Grid;
 
 const Subscription = () => {
   const { t } = useTranslation('page-home');
 
   const lang = t('subscription', { returnObjects: true });
+  const bp = useBreakpoint();
 
-  return <Styled.Container>{lang.title}</Styled.Container>;
+  return (
+    <Styled.SubscriptionSection>
+      <Styled.Container isMobile={!bp.lg}>
+        <Row>
+          <Col xs={24} lg={16}>
+            <Row justify={!bp.lg && 'center'}>
+              <SloganBox>
+                <Styled.Slogan>
+                  {lang.slogan}, {lang.see}
+                  <Styled.Underline href={lang.links.orgArch.url}> {lang.links.orgArch.label} </Styled.Underline>{' '}
+                  {lang.links.connective}
+                  <Styled.Underline href={lang.links.contributorList.url}>
+                    {lang.links.contributorList.label}
+                  </Styled.Underline>
+                </Styled.Slogan>
+              </SloganBox>
+            </Row>
+            <Row justify={!bp.lg && 'center'}>
+              <Styled.JoinButton type="primary" isMobile={!bp.lg}>
+                {' '}
+                {lang.joinButton.label}
+              </Styled.JoinButton>
+            </Row>
+          </Col>
+          <Col xs={24} lg={8} justify>
+            <Styled.Slogan>{lang.subscribe}</Styled.Slogan>
+            <Styled.EmailInput
+              placeholder={lang.emailInput.placeHolder}
+              allowClear
+              enterButton={lang.emailInput.submit}
+              size="large"
+              onSearch={() => {}}
+            />
+            <Styled.TermCaption>{lang.termsDesc}</Styled.TermCaption>
+          </Col>
+        </Row>
+      </Styled.Container>
+    </Styled.SubscriptionSection>
+  );
 };
 
 export default Subscription;
--- src/pages/home/subscription/subscription.styled.js
+++ src/pages/home/subscription/subscription.styled.js
@@ -1,10 +1,75 @@
 import styled from 'styled-components';
-import { colors } from '@tidb-community/ui';
+import { colors, mixins } from '@tidb-community/ui';
 
 import { Section } from '~/pages/home/index.styled';
+import { Button, Input, Row } from 'antd';
 
-export const Container = styled(Section)`
-  && {
-    background: ${colors.M2};
+export const Container = styled.div`
+  ${mixins.reset()}
+  ${mixins.centerBlock()}
+  ${mixins.responsive()}
+
+  ${(props) =>
+    props.isMobile &&
+    `
+    text-align: center;
+    ${mixins.flexCenter()}
+  `}
+`;
+
+export const SloganBox = styled(Row)`
+  max-width: 634px;
+  padding-right: 8px;
+  padding-left: 8px;
+`;
+
+export const SubscriptionSection = styled(Section)`
+  ${mixins.flexCenter()}
+  background-image: url("images/tugHome/subscription-background.svg");
+  background-size: cover;
+  padding-top: 62px;
+  padding-bottom: 62px;
+`;
+
+export const Slogan = styled.div`
+  color: ${colors.M1};
+  font-size: 20px;
+`;
+export const TermCaption = styled.div`
+  margin-top: 36px;
+  color: ${colors.M1};
+  font-size: 14px;
+`;
+
+export const Underline = styled(Slogan)`
+  display: inline;
+  text-decoration: underline;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export const JoinButton = styled(Button)`
+  ${mixins.flexCenter()}
+  ${(props) => (props.isMobile ? `margin-bottom: 50px; margin-top: 28px` : `margin-top: 46px;`)}
+`;
+
+export const EmailInput = styled(Input.Search)`
+  height: 45px;
+  margin-top: 33px;
+  border-radius: 6px;
+  max-width: 350px;
+
+  span {
+    font-size: 16px;
+  }
+
+  span.ant-input-affix-wrapper {
+    height: 45px;
+  }
+
+  button.ant-btn {
+    height: 45px;
   }
 `;

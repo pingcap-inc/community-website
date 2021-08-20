c13c0ba0844f61399fb97b27140c2c4dd9efd0e1
Nick Qi <wei.qi.nick@gmail.com>
2021-08-20 14:39:40 +1200

    CP-109 fix: add the missing feature flag to the new homepage

--- src/pages/contact-us/utils/index.js
+++ src/pages/contact-us/utils/index.js
@@ -1,14 +1,15 @@
 import { getI18nProps } from '~/utils/i18n.utils';
 
 export const getServerSideProps = async (ctx) => {
-  const i18nProps = await getI18nProps(['common', 'page-contact-us'])(ctx);
-
   const isEnabled = process.env.NEXT_PUBLIC_FT_CONTACT_US;
+
   if (!isEnabled) {
     return {
       notFound: true,
     };
   }
+
+  const i18nProps = await getI18nProps(['common', 'page-contact-us'])(ctx);
 
   return {
     props: {

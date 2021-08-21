3105222dbf667592e57bd772c39d9b748e9cddb2
Nick Qi <wei.qi.nick@gmail.com>
2021-08-21 15:57:07 +1200

    CP-109 feat: implement UI for the banner and carousel (#214)
    * CP-109 style: add stylings for the left banner panel
    
    * CP-109 style: stylings for the github star button
    
    * CP-109 style: adjust star number position
    
    * CP-109 feat: banner stylings with the placeholder image
    
    * CP-109 style: place the nav bar to its position
    
    * CP-109 style: update nav bar stylings
    
    * CP-109 fix: fix the warning of "expected server HTML.."
    
    * CP-109 style: adjust nav bar stylings for the mobile screens
    
    * CP-109 style: make content to be center-aligned for small screens

--- src/pages/home/banner/Banner.component.js
+++ src/pages/home/banner/Banner.component.js
@@ -1,14 +1,108 @@
-import React from 'react';
+import Image from 'next/image';
+import React, { useState, useRef } from 'react';
+import { Col, Grid, Row } from 'antd';
+import { GithubOutlined } from '@ant-design/icons';
 import { useTranslation } from 'next-i18next';
+import { useMount } from 'ahooks';
 
 import * as Styled from './banner.styled';
+import ActivityIcon from './activity.svg';
+import ArticleIcon from './article.svg';
+import AsktugIcon from './asktug.svg';
+import DocIcon from './doc.svg';
+
+const { useBreakpoint } = Grid;
+
+const navItems = [
+  {
+    icon: DocIcon,
+    langKey: 'doc',
+  },
+  {
+    icon: AsktugIcon,
+    langKey: 'asktug',
+  },
+  {
+    icon: ArticleIcon,
+    langKey: 'article',
+  },
+  {
+    icon: ActivityIcon,
+    langKey: 'activity',
+  },
+];
 
 const Banner = () => {
+  const bp = useBreakpoint();
+  const [mounted, setMounted] = useState(false);
+  const tooltipContainerRef = useRef(null);
   const { t } = useTranslation('page-home');
 
-  const lang = t('banner', { returnObjects: true });
+  // Solving the warning of "Expected server HTML to contain a matching <tag>"
+  // because of AntD Tooltip.
+  // More details: https://github.com/vercel/next.js/discussions/17443
+  useMount(() => {
+    setMounted(true);
+  });
 
-  return <Styled.Container>{lang.title}</Styled.Container>;
+  const lang = t('banner', { returnObjects: true });
+  const { navs: navsLang } = lang;
+  const isSmallScreen = !bp.md;
+
+  const tooltipProps = {
+    title: '37,916',
+    placement: 'right',
+    autoAdjustOverflow: false,
+    getPopupContainer: () => tooltipContainerRef?.current,
+  };
+
+  return (
+    <Styled.Container>
+      <Styled.Content isSmallScreen={isSmallScreen}>
+        <Row gutter={[32, 64]} justify="space-between" align="middle">
+          <Styled.LeftPanel>
+            <Styled.Logo />
+            <Styled.Intro>{lang.intro}</Styled.Intro>
+            <Row gutter={32} justify="space-between" align="middle">
+              <Col flex="none">
+                <Styled.TryButton>{lang.tryButton}</Styled.TryButton>
+              </Col>
+              <Col flex="auto">
+                <Styled.StarButton>
+                  <GithubOutlined />
+                  Star
+                  {mounted && (
+                    <Styled.StarButtonTooltip {...tooltipProps}>
+                      <Styled.TooltipContainer ref={tooltipContainerRef} />
+                    </Styled.StarButtonTooltip>
+                  )}
+                </Styled.StarButton>
+              </Col>
+            </Row>
+          </Styled.LeftPanel>
+
+          <Styled.RightPanel>
+            <Styled.Carousel isSmallScreen={isSmallScreen}>
+              {[...new Array(4).keys()].map((key) => (
+                <div key={key}>
+                  <Image src="/images/home/banner-carousel.png" height="234" width="652" />
+                </div>
+              ))}
+            </Styled.Carousel>
+          </Styled.RightPanel>
+        </Row>
+
+        <Styled.Navs isSmallScreen={isSmallScreen}>
+          {navItems.map(({ icon: Icon, langKey }, idx) => (
+            <Styled.NavItem key={idx}>
+              <Icon />
+              <span>{navsLang[langKey]}</span>
+            </Styled.NavItem>
+          ))}
+        </Styled.Navs>
+      </Styled.Content>
+    </Styled.Container>
+  );
 };
 
 export default Banner;
--- src/pages/home/banner/banner.styled.js
+++ src/pages/home/banner/banner.styled.js
@@ -1,11 +1,265 @@
-import styled from 'styled-components';
-import { colors } from '@tidb-community/ui';
+import * as polished from 'polished';
+import styled, { css } from 'styled-components';
+import { Button, Carousel as AntCarousel, Col, Row, Tooltip } from 'antd';
+import { colors, mixins } from '@tidb-community/ui';
 
+import LogoSvg from './logo.svg';
 import { Section } from '~/pages/home/index.styled';
+
+const githubBlack = '#24292e';
+const githubLightGrey = '#f0f0f0';
 
 export const Container = styled(Section)`
   && {
     color: ${colors.M1};
     background: ${colors.B4};
+    padding: 0;
   }
+`;
+
+export const Content = styled.div`
+  ${mixins.responsive()};
+  width: 100%;
+  position: relative;
+  padding: 5rem 0 7.5rem;
+
+  ${(props) =>
+    props.isSmallScreen &&
+    css`
+      padding: 3rem 0 5.5rem;
+
+      ${LeftPanel} {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        > * {
+          display: block;
+        }
+
+        p {
+          text-align: center;
+        }
+
+        .ant-btn-primary {
+          margin-bottom: 2rem;
+        }
+      }
+
+      ${Intro} {
+        width: 80%;
+      }
+    `}
+`;
+
+export const LeftPanel = styled(Col).attrs({
+  xs: {
+    order: 2,
+  },
+  sm: {
+    order: 2,
+  },
+  md: {
+    order: 1,
+    span: 10,
+  },
+  lg: {
+    order: 1,
+    span: 8,
+  },
+})``;
+
+export const RightPanel = styled(Col).attrs({
+  xs: {
+    order: 1,
+  },
+  sm: {
+    order: 1,
+  },
+  md: {
+    order: 2,
+    span: 14,
+  },
+  lg: {
+    order: 2,
+    span: 12,
+  },
+})``;
+
+export const Logo = styled(LogoSvg)`
+  max-width: 350px;
+  margin-bottom: 0.5rem;
+`;
+
+export const Intro = styled.p`
+  ${mixins.reset()};
+  color: ${colors.M1};
+  font-size: 20px;
+  margin-bottom: 2rem;
+`;
+
+export const TryButton = styled(Button).attrs({
+  type: 'primary',
+  size: 'large',
+})`
+  && {
+    border-radius: 6px;
+    padding: 0 2rem;
+
+    span {
+      font-size: 18px;
+    }
+  }
+`;
+
+export const StarButton = styled.span`
+  position: relative;
+  display: inline-block;
+  border-radius: 4px;
+  background: ${githubLightGrey};
+  font-weight: bold;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif;
+  cursor: pointer;
+
+  .anticon {
+    font-size: 16px;
+    margin-right: 5px;
+  }
+
+  &,
+  .ant-tooltip-inner {
+    ${mixins.verticalLineMiddle('26px')};
+    font-size: 14px;
+    color: ${githubBlack};
+    min-height: auto;
+    padding: 0 8px;
+  }
+`;
+
+export const TooltipContainer = styled.div`
+  position: absolute;
+  top: 50%;
+  right: 6px;
+`;
+
+export const StarButtonTooltip = styled(Tooltip).attrs({
+  color: githubLightGrey,
+  visible: true,
+})``;
+
+export const NavItem = styled(Col).attrs({
+  span: 6,
+})`
+  ${mixins.flexCenter()};
+  height: 40px;
+  border-right: 1px solid ${colors.T2};
+
+  &:last-child {
+    border-right: none;
+  }
+
+  svg {
+    height: 40px;
+    margin-right: 40px;
+  }
+
+  span {
+    font-size: 18px;
+    color: ${colors.F1};
+  }
+`;
+
+export const Navs = styled(Row).attrs({
+  justify: 'center',
+  align: 'middle',
+})`
+  background: ${colors.M1};
+  position: absolute;
+  bottom: -45px;
+  left: 0;
+  right: 0;
+  padding: 25px 0;
+  margin: 0 16px;
+  border-radius: 10px;
+  border: 1px solid ${colors.T2};
+  box-shadow: 0 2px 6px ${polished.rgba('#000', 0.08)};
+
+  ${(props) =>
+    props.isSmallScreen &&
+    css`
+      padding: 12px 0;
+
+      ${NavItem} {
+        height: 66px;
+        flex-direction: column;
+        border-right: none;
+
+        svg {
+          margin-right: 0;
+          margin-bottom: 8px;
+        }
+
+        span {
+          font-size: 14px;
+        }
+      }
+    `};
+`;
+
+export const Carousel = styled(AntCarousel).attrs((props) => ({
+  dotPosition: props.isSmallScreen ? 'bottom' : 'right',
+}))`
+  .slick-slide {
+    padding-right: 2.5rem;
+  }
+
+  .slick-dots {
+    li {
+      margin: 8px 3px;
+
+      &,
+      button {
+        ${mixins.size('6px', '40px')}
+        background: ${polished.rgba(colors.M1, 0.2)};
+        border-radius: 3px;
+      }
+
+      &.slick-active {
+        &,
+        button {
+          ${mixins.size('6px', '50px')}
+          background: ${polished.rgba(colors.M1, 0.6)};
+        }
+      }
+    }
+  }
+
+  ${(props) =>
+    props.isSmallScreen &&
+    css`
+      .slick-slide {
+        padding-right: 0;
+      }
+
+      .slick-dots {
+        margin: 0 auto;
+        bottom: -32px;
+
+        li {
+          margin: 3px 8px;
+
+          &,
+          button {
+            ${mixins.size('40px', '6px')}
+          }
+
+          &.slick-active {
+            &,
+            button {
+              ${mixins.size('50px', '6px')}
+            }
+          }
+        }
+      }
+    `};
 `;

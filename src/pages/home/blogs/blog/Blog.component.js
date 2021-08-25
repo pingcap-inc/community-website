528f38c8c262b155fc52e97e025bbafc93b2c4b4
Nick Qi <wei.qi.nick@gmail.com>
2021-08-25 20:48:53 +1200

    CP-112 style: update blog item with the source site icon

--- src/pages/home/blogs/blog/Blog.component.js
+++ src/pages/home/blogs/blog/Blog.component.js
@@ -2,11 +2,13 @@ import React from 'react';
 import dayjs from 'dayjs';
 
 import * as Styled from './blog.styled';
+import AsktugIcon from './asktug.svg';
 
 const Blog = ({ title, link, user, createdTime, onClick }) => (
   <Styled.Container onClick={onClick(link)}>
     <Styled.TitleRow>
       <h3>{title}</h3>
+      <AsktugIcon />
     </Styled.TitleRow>
 
     <Styled.InformationRow>

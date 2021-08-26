8cf7fb5b56204364e9f76e643895851669b4507b
Xiyan Shao <xiyan128@gmail.com>
2021-08-26 19:03:57 +0800

    CP-114 feat: added basic learning section UIs

--- src/pages/home/learning/Learning.component.js
+++ src/pages/home/learning/Learning.component.js
@@ -1,14 +1,87 @@
 import React from 'react';
-import { useTranslation } from 'next-i18next';
+import { Trans, useTranslation } from 'next-i18next';
 
 import * as Styled from './learning.styled';
+import TwoColumnsLayout from '../twoColumsLayout';
+import { ModuleTitle } from '../index.styled';
+import { Col } from 'antd';
+import { useIsSmallScreen } from '../../../hooks';
 
 const Learning = () => {
   const { t } = useTranslation('page-home');
 
   const lang = t('learning', { returnObjects: true });
 
-  return <Styled.Container>{lang.title}</Styled.Container>;
+  const { isSmallScreen } = useIsSmallScreen();
+
+  return (
+    <Styled.Container>
+      <TwoColumnsLayout
+        title={lang.title}
+        leftPanel={
+          <>
+            <Styled.Logo src={'/images/home/learning-pingcap-education.svg'} />
+            <Styled.Text>
+              <Trans
+                t={t}
+                i18nKey={'learning.desc'}
+                components={[<Styled.Link href={lang.educationLink} />, <Styled.Link href={lang.certificateLink} />]}
+              />
+            </Styled.Text>
+            <Styled.VideoHeader>
+              {' '}
+              {lang.videosTitle}
+              <Styled.FreeLabel>{lang.freeLabel}</Styled.FreeLabel>
+            </Styled.VideoHeader>
+            <Styled.VideosRow wrap={false}>
+              <Col xs={24} md={18}>
+                <Styled.VideoBoxWrapper isSmallScreen={isSmallScreen}>
+                  <Styled.VideoBox isSmallScreen={isSmallScreen}>
+                    <Styled.VideoPlayButton>
+                      <Styled.VideoPlayIcon color={'white'} height={'48px'} />
+                    </Styled.VideoPlayButton>
+                    {!isSmallScreen && (
+                      <Styled.VideoCaption isSmallScreen={isSmallScreen}>{lang.majorVideoDesc}</Styled.VideoCaption>
+                    )}
+                  </Styled.VideoBox>
+                </Styled.VideoBoxWrapper>
+              </Col>
+              <Col xs={0} md={6}>
+                {[1, 2, 3].map((video) => (
+                  <Styled.VideoBoxWrapperSmall>
+                    <Styled.VideoBox isSmallScreen={isSmallScreen}>
+                      <Styled.VideoPlayButton>
+                        <Styled.VideoPlayIconSmall color={'white'} height={'48px'} />
+                      </Styled.VideoPlayButton>
+                    </Styled.VideoBox>
+                  </Styled.VideoBoxWrapperSmall>
+                ))}
+              </Col>
+            </Styled.VideosRow>
+            {isSmallScreen && <Styled.Text>{lang.majorVideoDesc}</Styled.Text>}
+
+            <Styled.More isSmallScreen={isSmallScreen}>更多课程</Styled.More>
+          </>
+        }
+        rightPanel={
+          <>
+            {lang.linkSections.map((section) => (
+              <>
+                <ModuleTitle>{section.title}</ModuleTitle>
+                <Styled.LinksRow gutter={16}>
+                  {section.links.map((link) => (
+                    <Styled.LinkWrapper xs={24} md={12} lg={8}>
+                      <Styled.Link href={link.link}> {link.label} </Styled.Link>
+                    </Styled.LinkWrapper>
+                  ))}
+                </Styled.LinksRow>
+              </>
+            ))}
+          </>
+        }
+      />
+    </Styled.Container>
+  );
 };
 
 export default Learning;
--- src/pages/home/learning/learning.styled.js
+++ src/pages/home/learning/learning.styled.js
@@ -1,5 +1,132 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { Section } from '~/pages/home/index.styled';
-
+import { colors, mixins } from '@tidb-community/ui';
+import { Col, Row } from 'antd';
+import { PlayCircleFilled } from '@ant-design/icons';
+import { Link as RawLink } from '~/components';
 export const Container = styled(Section)``;
+
+export const Text = styled.div`
+  font-size: 16px;
+  font-weight: lighter;
+`;
+
+export const Link = styled.a`
+  font-weight: lighter;
+  text-decoration: underline;
+
+  &:hover {
+    color: ${colors.B1};
+    text-decoration: underline;
+  }
+`;
+
+export const Logo = styled.img`
+  margin-bottom: 2rem;
+`;
+
+export const VideoHeader = styled.div`
+  font-size: 20px;
+  font-weight: bold !important;
+  margin-top: 2.5rem;
+`;
+
+export const FreeLabel = styled.span`
+  display: inline-block;
+  margin-left: 1rem;
+  font-weight: 300;
+  color: ${colors.T1};
+`;
+
+export const LinksRow = styled(Row)`
+  margin-bottom: 1rem;
+`;
+export const LinkWrapper = styled(Col)`
+  padding-bottom: 1rem;
+`;
+
+export const VideoBox = styled.div`
+  position: relative;
+  background-size: auto 100%;
+  padding-bottom: 62.8%;
+  width: 100%;
+  background-image: url('/images/home/subscription-background.svg');
+  border-color: #fff;
+  ${(props) =>
+    !props.isSmallScreen &&
+    css`
+      box-shadow: inset 0 0 0 8px #fff;
+    `}
+`;
+
+export const VideoCaption = styled.div`
+  padding: 0.4rem 0.6rem 0.3rem 0.4rem;
+  position: absolute;
+  background-color: rgba(0, 0, 0, 0.46);
+  z-index: 2;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  color: white;
+  font-size: 15px;
+  font-weight: lighter;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  border: 8px solid #fff;
+  border-top-width: 0;
+  box-sizing: border-box;
+`;
+
+export const VideoPlayButton = styled.div`
+  ${mixins.flexCenter};
+  ${mixins.flexVerticalCenter()};
+  position: absolute;
+  z-index: 3;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+`;
+
+export const VideoPlayIcon = styled(PlayCircleFilled)`
+  font-size: 5rem;
+  color: white;
+`;
+
+export const VideoPlayIconSmall = styled(PlayCircleFilled)`
+  font-size: 2rem;
+  color: white;
+`;
+
+export const VideoBoxWrapper = styled.div`
+  box-sizing: border-box;
+  // adjust border for alignment
+  ${(props) =>
+    !props.isSmallScreen &&
+    css`
+      transform: translateX(-8px);
+    `}
+`;
+
+export const VideoBoxWrapperSmall = styled.div`
+  max-width: 100%;
+  height: 33.3%;
+`;
+
+export const VideosRow = styled(Row)`
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+`;
+
+export const More = styled(RawLink)`
+  display: block;
+  margin-top: 1rem;
+  ${(props) =>
+    props.isSmallScreen &&
+    css`
+      margin-bottom: 3rem;
+    `}
+`;

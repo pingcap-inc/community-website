b63d76087fea833d3b63fed847e6d22db929d18a
昌维 <867597730@qq.com>
2021-09-05 16:40:59 +0800

    CP-152 feat: add login redirection logic

--- src/pages/tug/apply/ApplyMain.page.js
+++ src/pages/tug/apply/ApplyMain.page.js
@@ -13,9 +13,6 @@ import Label from '~/components/Label';
 
 const ApplyMainPage = () => {
   const title = 'TiDB User Group 会员申请';
-  const description =
-    '加入 TUG，让数据库、大数据从业者找到自己的圈子；' +
-    '探索技术问题，随时随地交流成长，解决问题；发表技术见解，收获前沿知识，提升个人影响力。';
 
   const postImageUrl = '/images/people/apply/post.svg';
 
@@ -68,214 +65,200 @@ const ApplyMainPage = () => {
   };
 
   return (
-    <>
-      <TugHead title={title} description={description} />
+    <CoreLayout domain="tidb.io">
+      <Container className={styles.container}>
+        <div className={styles.start}>
+          <h1 className={styles.start_title}>{title}</h1>
 
-      <CoreLayout>
-        <Container className={styles.container}>
-          <div className={styles.start}>
-            <h1 className={styles.start_title}>{title}</h1>
+          <div className={styles.start_post}>
+            <img src={postImageUrl} alt={title} />
+          </div>
 
-            <div className={styles.start_post}>
-              <img src={postImageUrl} alt={title} />
-            </div>
+          <div className={styles.divider_row} />
 
-            <div className={styles.divider_row} />
+          <h2 className={styles.start_question}>{question}</h2>
 
-            <h2 className={styles.start_question}>{question}</h2>
+          <div className={styles.start_answer}>{answer}</div>
 
-            <div className={styles.start_answer}>{answer}</div>
+          <div className={styles.divider_row} />
 
-            <div className={styles.divider_row} />
-
-            <h2 className={styles.start_why_join_title}>{whyJoinTitle}</h2>
-            <div className={styles.start_why_join_list}>
-              {whyJoinItems.map((value) => (
-                <div className={styles.start_why_join_list_item}>
-                  <div className={styles.start_why_join_list_item_icon}>
-                    <img src={value.iconUrl} alt={value.text} />
-                  </div>
-                  <div className={styles.start_why_join_list_item_text}>{value.text}</div>
+          <h2 className={styles.start_why_join_title}>{whyJoinTitle}</h2>
+          <div className={styles.start_why_join_list}>
+            {whyJoinItems.map((value) => (
+              <div className={styles.start_why_join_list_item}>
+                <div className={styles.start_why_join_list_item_icon}>
+                  <img src={value.iconUrl} alt={value.text} />
                 </div>
-              ))}
-            </div>
+                <div className={styles.start_why_join_list_item_text}>{value.text}</div>
+              </div>
+            ))}
           </div>
+        </div>
 
-          <div className={styles.end}>
-            <div className={styles.end_form}>
-              <h2 className={styles.end_form_title}>申请信息</h2>
-              <form onSubmit={handleSubmit(onSubmit)}>
-                <Label required className={styles.end_form_label}>
-                  真实姓名
-                </Label>
-                <Controller
-                  name="real_name"
-                  defaultValue=""
-                  control={control}
-                  render={({ field }) => <Input {...field} />}
-                />
+        <div className={styles.end}>
+          <div className={styles.end_form}>
+            <h2 className={styles.end_form_title}>申请信息</h2>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <Label required className={styles.end_form_label}>
+                真实姓名
+              </Label>
+              <Controller
+                name="real_name"
+                defaultValue=""
+                control={control}
+                render={({ field }) => <Input {...field} />}
+              />
 
-                <Label required className={styles.end_form_label}>
-                  手机号码
-                </Label>
-                <Controller
-                  name="phone"
-                  defaultValue=""
-                  control={control}
-                  render={({ field }) => <Input {...field} />}
-                />
+              <Label required className={styles.end_form_label}>
+                手机号码
+              </Label>
+              <Controller name="phone" defaultValue="" control={control} render={({ field }) => <Input {...field} />} />
 
-                <Label required className={styles.end_form_label}>
-                  邮箱
-                </Label>
-                <Controller
-                  name="email"
-                  defaultValue=""
-                  control={control}
-                  render={({ field }) => <Input {...field} />}
-                />
+              <Label required className={styles.end_form_label}>
+                邮箱
+              </Label>
+              <Controller name="email" defaultValue="" control={control} render={({ field }) => <Input {...field} />} />
 
-                <Label required className={styles.end_form_label}>
-                  公司
-                </Label>
-                <Controller
-                  name="company_name"
-                  defaultValue=""
-                  control={control}
-                  render={({ field }) => <Input {...field} />}
-                />
+              <Label required className={styles.end_form_label}>
+                公司
+              </Label>
+              <Controller
+                name="company_name"
+                defaultValue=""
+                control={control}
+                render={({ field }) => <Input {...field} />}
+              />
 
-                <Label required className={styles.end_form_label}>
-                  职位
-                </Label>
-                <Controller
-                  name="job_title"
-                  defaultValue=""
-                  control={control}
-                  render={({ field }) => (
-                    <Select {...field} style={{ width: '100%' }}>
-                      {Object.keys(jobTitle).map((value) => (
-                        <Select.Option key={value} value={value}>
-                          {jobTitle[value]}
-                        </Select.Option>
-                      ))}
-                    </Select>
-                  )}
-                />
+              <Label required className={styles.end_form_label}>
+                职位
+              </Label>
+              <Controller
+                name="job_title"
+                defaultValue=""
+                control={control}
+                render={({ field }) => (
+                  <Select {...field} style={{ width: '100%' }}>
+                    {Object.keys(jobTitle).map((value) => (
+                      <Select.Option key={value} value={value}>
+                        {jobTitle[value]}
+                      </Select.Option>
+                    ))}
+                  </Select>
+                )}
+              />
 
-                <Label required className={styles.end_form_label}>
-                  你的公司对 TiDB 正处于
-                </Label>
-                <Controller
-                  name="stage_of_comany_use_of_tidb"
-                  defaultValue=""
-                  control={control}
-                  render={({ field }) => (
-                    <Select {...field} style={{ width: '100%' }}>
-                      {optionStageOfComanyUseOfTidb.map((value) => (
-                        <Select.Option key={value} value={value}>
+              <Label required className={styles.end_form_label}>
+                你的公司对 TiDB 正处于
+              </Label>
+              <Controller
+                name="stage_of_comany_use_of_tidb"
+                defaultValue=""
+                control={control}
+                render={({ field }) => (
+                  <Select {...field} style={{ width: '100%' }}>
+                    {optionStageOfComanyUseOfTidb.map((value) => (
+                      <Select.Option key={value} value={value}>
+                        {value}
+                      </Select.Option>
+                    ))}
+                  </Select>
+                )}
+              />
+
+              <Label required className={styles.end_form_label}>
+                你加入 TUG 的原因
+              </Label>
+              <Controller
+                name="reason_for_application"
+                defaultValue=""
+                control={control}
+                render={({ field }) => <Input {...field} />}
+              />
+
+              <Label required className={styles.end_form_label}>
+                请选择你乐于分享的
+              </Label>
+              {/*<Radio.Group onChange={this.onChange} value={value}>*/}
+              <Controller
+                name="preferred_way_of_sharing"
+                defaultValue=""
+                control={control}
+                render={({ field }) => (
+                  <Radio.Group {...field}>
+                    <Space direction="vertical">
+                      {optionPreferredWayOfSharing.map((value) => (
+                        <Radio key={value} value={value}>
                           {value}
-                        </Select.Option>
+                        </Radio>
                       ))}
-                    </Select>
-                  )}
-                />
+                    </Space>
+                  </Radio.Group>
+                )}
+              />
 
-                <Label required className={styles.end_form_label}>
-                  你加入 TUG 的原因
-                </Label>
-                <Controller
-                  name="reason_for_application"
-                  defaultValue=""
-                  control={control}
-                  render={({ field }) => <Input {...field} />}
-                />
+              <Label required className={styles.end_form_label}>
+                你希望担任 TUG 什么⻆⾊
+              </Label>
+              <Controller
+                name="roles_want_to_play"
+                defaultValue=""
+                control={control}
+                render={({ field }) => (
+                  <Radio.Group {...field}>
+                    <Space direction="vertical">
+                      {optionRolesWantToPlay.map((value) => (
+                        <Radio key={value} value={value}>
+                          {value}
+                        </Radio>
+                      ))}
+                    </Space>
+                  </Radio.Group>
+                )}
+              />
 
-                <Label required className={styles.end_form_label}>
-                  请选择你乐于分享的
-                </Label>
-                {/*<Radio.Group onChange={this.onChange} value={value}>*/}
-                <Controller
-                  name="preferred_way_of_sharing"
-                  defaultValue=""
-                  control={control}
-                  render={({ field }) => (
-                    <Radio.Group {...field}>
-                      <Space direction="vertical">
-                        {optionPreferredWayOfSharing.map((value) => (
-                          <Radio key={value} value={value}>
-                            {value}
-                          </Radio>
-                        ))}
-                      </Space>
-                    </Radio.Group>
-                  )}
-                />
+              <Label required className={styles.end_form_label}>
+                微信 id（确保可以搜索到，以便于⼩助⼿可以邀请进群）
+              </Label>
+              <Controller
+                name="wechat_id"
+                defaultValue=""
+                control={control}
+                render={({ field }) => <Input {...field} />}
+              />
 
-                <Label required className={styles.end_form_label}>
-                  你希望担任 TUG 什么⻆⾊
-                </Label>
-                <Controller
-                  name="roles_want_to_play"
-                  defaultValue=""
-                  control={control}
-                  render={({ field }) => (
-                    <Radio.Group {...field}>
-                      <Space direction="vertical">
-                        {optionRolesWantToPlay.map((value) => (
-                          <Radio key={value} value={value}>
-                            {value}
-                          </Radio>
-                        ))}
-                      </Space>
-                    </Radio.Group>
-                  )}
-                />
+              <Label className={styles.end_form_label}>三四句话介绍⼀下⾃⼰（⽤于⼊群介绍）</Label>
+              <Controller
+                name="bio"
+                defaultValue=""
+                control={control}
+                render={({ field }) => <Input.TextArea {...field} rows={3} />}
+              />
 
-                <Label required className={styles.end_form_label}>
-                  微信 id（确保可以搜索到，以便于⼩助⼿可以邀请进群）
-                </Label>
-                <Controller
-                  name="wechat_id"
-                  defaultValue=""
-                  control={control}
-                  render={({ field }) => <Input {...field} />}
-                />
+              <Label className={styles.end_form_label}>在哪得知 TUG</Label>
+              <Controller
+                name="channel"
+                defaultValue=""
+                control={control}
+                render={({ field }) => <Input {...field} />}
+              />
 
-                <Label className={styles.end_form_label}>三四句话介绍⼀下⾃⼰（⽤于⼊群介绍）</Label>
-                <Controller
-                  name="bio"
-                  defaultValue=""
-                  control={control}
-                  render={({ field }) => <Input.TextArea {...field} rows={3} />}
-                />
+              <Label className={styles.end_form_label}>推荐⼈</Label>
+              <Controller
+                name="referrer"
+                defaultValue=""
+                control={control}
+                render={({ field }) => <Input {...field} />}
+              />
 
-                <Label className={styles.end_form_label}>在哪得知 TUG</Label>
-                <Controller
-                  name="channel"
-                  defaultValue=""
-                  control={control}
-                  render={({ field }) => <Input {...field} />}
-                />
-
-                <Label className={styles.end_form_label}>推荐⼈</Label>
-                <Controller
-                  name="referrer"
-                  defaultValue=""
-                  control={control}
-                  render={({ field }) => <Input {...field} />}
-                />
-
-                <Button block htmlType="submit" className={styles.end_form_submit_button}>
-                  提交申请
-                </Button>
-                <div className={styles.end_form_submit_extra}>提交即表明你已阅读并同意《服务协议》《隐私协议》</div>
-              </form>
-            </div>
+              <Button block htmlType="submit" className={styles.end_form_submit_button}>
+                提交申请
+              </Button>
+              <div className={styles.end_form_submit_extra}>提交即表明你已阅读并同意《服务协议》《隐私协议》</div>
+            </form>
           </div>
-        </Container>
-      </CoreLayout>
-    </>
+        </div>
+      </Container>
+    </CoreLayout>
   );
 };
 
--- src/pages/tug/apply/index.page.js
+++ src/pages/tug/apply/index.page.js
@@ -3,20 +3,34 @@ import React, { useContext } from 'react';
 import ApplyMainPage from './ApplyMain.page';
 import Error from '../../_error.page';
 import { AuthContext } from '~/context';
+import { CommunityHead } from '~/components';
 
-const Apply = () => {
+const PageContent = () => {
   const { login, isAnonymous } = useContext(AuthContext);
+
   const isTugMember = false;
   if (isAnonymous) {
-    if (isTugMember) {
-      return <Error statusCode={409} errorMsg={'已经提交过申请并且正在审核中，或者已经加入 TUG 了'} />;
-    } else {
-      return <ApplyMainPage />;
-    }
-  } else {
     login();
     return null;
   }
+
+  if (isTugMember) {
+    return <Error statusCode={409} errorMsg={'已经提交过申请并且正在审核中，或者已经加入 TUG 了'} />;
+  } else {
+    return <ApplyMainPage />;
+  }
 };
 
-export default Apply;
+const title = 'TiDB User Group 会员申请';
+const description =
+  '加入 TUG，让数据库、大数据从业者找到自己的圈子；' +
+  '探索技术问题，随时随地交流成长，解决问题；发表技术见解，收获前沿知识，提升个人影响力。';
+
+const Page = () => (
+  <>
+    <CommunityHead title={title} description={description} />
+    <PageContent />
+  </>
+);
+
+export default Page;

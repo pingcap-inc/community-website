6e18085a106ac62d595a0d450fff4c4f2eca8883
Xiyan Shao <xiyan128@gmail.com>
2021-09-06 13:58:18 +0800

    CP-154 refactor: added comment to possibly obscure shell script

--- env_dump.sh
+++ env_dump.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
+# Script to dump shell environment variables (loaded by CircleCI) to local
+# configuration file to be used by Next.js
+
 template_env_file=".env.template"
 local_env_file=".env.local"
 
+# read placeholder environment variables from .env.template
 while read -r line || [ -n "$line" ]; do
+    # ignore the trailing '='
     name="${line:0:$((${#line}-1))}"
+    # output to .env.local
+    # note that '!' indicates indirection
     echo "${name}=\"${!name}\"" >> "${local_env_file}"
 done < "${template_env_file}"

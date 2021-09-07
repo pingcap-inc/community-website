d1008a778bf4f01fbfe0fac9eb0fead68f9232d5
Nick Qi <wei.qi.nick@gmail.com>
2021-09-07 19:36:51 +1200

    CP-154 fix: fix a filename typo

--- env_dump.sh
+++ env_dump.sh
@@ -3,7 +3,7 @@
 # Script to dump shell environment variables (loaded by CircleCI) to local
 # configuration file to be used by Next.js
 
-template_env_file=".env.circleci.template"
+template_env_file=".env.circle-ci.template"
 local_env_file=".env.local"
 
 # read placeholder environment variables from .env.template

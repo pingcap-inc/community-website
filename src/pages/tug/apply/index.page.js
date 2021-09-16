c41bc2be3a1a947984ff6f1919e8993be89d91a6
昌维 <867597730@qq.com>
2021-09-16 18:42:26 +0800

    CP-152 feat: remove check isTugMember logic

--- src/pages/tug/apply/index.page.js
+++ src/pages/tug/apply/index.page.js
@@ -2,21 +2,16 @@ import React, { useContext } from 'react';
 
 import Apply from './Apply.component';
 import { title, description } from './Apply.data';
-import Error from '~/pages/_error.page';
+// import Error from '~/pages/_error.page';
 import { AuthContext } from '~/context';
 import { CommunityHead } from '~/components';
 
 const PageContent = () => {
   const { isAnonymous, login } = useContext(AuthContext);
 
-  const isTugMember = false;
   if (isAnonymous) {
     login();
     return null;
-  }
-
-  if (isTugMember) {
-    return <Error statusCode={409} errorMsg={'已经提交过申请并且正在审核中，或者已经加入 TUG 了'} />;
   }
 
   return <Apply />;

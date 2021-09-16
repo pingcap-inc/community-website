dbdf9912e3ef0fae855025cc2821d92abc03545d
昌维 <867597730@qq.com>
2021-09-16 21:41:34 +0800

    CP-152 feat: add some error case to mockapi

--- packages/mockApi/routes/tug/applications.js
+++ packages/mockApi/routes/tug/applications.js
@@ -1,19 +1,23 @@
-// eslint-disable-next-line no-unused-vars
 const { errorResp, successResp } = require('../../utils');
 
 module.exports = (req, res) => {
   const { real_name } = req.body;
-
-  // eslint-disable-next-line no-console
-  console.log(req.body);
-  console.log('real_name', real_name);
 
   if (real_name === undefined || real_name === '[empty]') {
     return errorResp({
       detail: 'something wrong',
       errors: {
         real_name: ['this field is required'],
+        email: ['invalid email address'],
       },
+    })(req, res);
+  } else if (real_name === '[401]') {
+    return errorResp({
+      detail: 'Authentication credentials were not provided',
+    })(req, res);
+  } else if (real_name === '[409]') {
+    return errorResp({
+      detail: '用户已经提交过申请，或者已经加入 TUG 了',
     })(req, res);
   } else {
     successResp({

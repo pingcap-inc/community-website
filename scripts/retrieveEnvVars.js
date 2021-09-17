41aa50f92b1619cf115007efcfb92efb54ad80a4
Nick Qi <wei.qi.nick@gmail.com>
2021-09-17 21:50:25 +1200

    CP-198 fix: fix CI variables for the preview builds (#262)
    * CP-198 fix: fix the eslint error
    
    * CP-198 feat: move recaptcha key to the CircleCI
    
    * CP-198 feat: test the CI for the main branch
    
    * CP-198 feat: add `.env.preview' and try to replace the shell script with a node script
    
    * CP-198 feat: generate `.env.local` for different envs
    
    * CP-198 fix: update CI with the new env retrieve script

---
+++ scripts/retrieveEnvVars.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+
+const { env } = process;
+const isPreview = env.CI_STAGE === 'preview';
+
+const resolveRoot = (relativePath) => path.resolve(path.join(__dirname, '..'), relativePath);
+const localFile = resolveRoot('.env.local');
+const previewFile = resolveRoot('.env.preview');
+const templeFile = resolveRoot('.env.circle-ci.template');
+
+(() => {
+  const envValues = {};
+  const data = fs.readFileSync(templeFile, 'utf8');
+  const names = data.split(/=?\n/).filter(Boolean);
+
+  if (isPreview) {
+    const re = /(.*?)=(.*)/;
+    const data = fs.readFileSync(previewFile, 'utf8');
+    const lines = data
+      .split(/\n/)
+      .filter(Boolean)
+      .map((line) => line.replace(/\s*=\s*/, '='));
+
+    lines.forEach((line) => {
+      const [, name, value] = re.exec(line);
+      envValues[name] = value;
+    });
+  }
+
+  names.forEach((name) => {
+    if (isPreview && !envValues[name]) {
+      envValues[name] = env[`PREVIEW_${name}`] || env[name];
+    } else {
+      return (envValues[name] = env[name]);
+    }
+  });
+
+  if (fs.existsSync(localFile)) {
+    fs.renameSync(localFile, `${localFile}.bk`);
+  }
+
+  fs.writeFileSync(
+    localFile,
+    Object.entries(envValues)
+      .map(([name, value]) => value && `${name}=${value}`)
+      .filter(Boolean)
+      .join('\n')
+  );
+})();

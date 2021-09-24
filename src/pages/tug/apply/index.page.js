ffc1689bbc0ba4d27ae9e2290202499bc419e9a6
Nick Qi <wei.qi.nick@gmail.com>
2021-09-24 20:59:57 +1200

    CP-216 fix: fix head.titleSuffix in the page title because of i18n initialization

--- src/pages/tug/apply/index.page.js
+++ src/pages/tug/apply/index.page.js
@@ -1,9 +1,20 @@
 import React, { useContext } from 'react';
 
 import Apply from './Apply.component';
-import { title, description } from './apply.data';
 import { AuthContext } from '~/context';
 import { CommunityHead } from '~/components';
+import { getI18nProps } from '~/utils/i18n.utils';
+import { title, description } from './apply.data';
+
+export const getStaticProps = async (ctx) => {
+  const i18nProps = await getI18nProps(['common'])(ctx);
+
+  return {
+    props: {
+      ...i18nProps,
+    },
+  };
+};
 
 const PageContent = () => {
   const { isAnonymous, login } = useContext(AuthContext);

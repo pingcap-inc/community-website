e21b01ead2c1a05d7e8c31669241481385fb0836
Nick Qi <wei.qi.nick@gmail.com>
2021-09-26 21:54:03 +1300

    feat: use SQLite for the local development

--- packages/strapi/config/database.js
+++ packages/strapi/config/database.js
@@ -1,20 +1,37 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'mysql',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', 'root'),
-        password: env('DATABASE_PASSWORD', ''),
-        charset: 'utf8mb4',
-      },
-      options: {
-        charset: 'utf8mb4',
-      },
+module.exports = ({ env }) => {
+  const isSQLite = env('STRAPI_DB_CLIENT', 'mysql') === 'sqlite';
+
+  const defaultConnection = {
+    connector: 'bookshelf',
+    settings: {
+      client: 'mysql',
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', 'strapi'),
+      username: env('DATABASE_USERNAME', 'root'),
+      password: env('DATABASE_PASSWORD', ''),
+      charset: 'utf8mb4',
     },
-  },
-});
+    options: {
+      charset: 'utf8mb4',
+    },
+  };
+
+  if (isSQLite) {
+    defaultConnection.settings = {
+      client: 'sqlite',
+      filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+    };
+
+    defaultConnection.options = {
+      useNullAsDefault: true,
+    };
+  }
+
+  return {
+    defaultConnection: 'default',
+    connections: {
+      default: defaultConnection,
+    },
+  };
+};

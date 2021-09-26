68ab7cc414b8b2167a319a567bf25ec1a06f21fb
wd0517 <me@wangdi.ink>
2021-09-26 10:40:18 +0800

    feat: merge production collection types to repo; replace sqlite3 with mysql

--- packages/strapi/config/database.js
+++ packages/strapi/config/database.js
@@ -4,11 +4,16 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'mysql',
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'root'),
+        password: env('DATABASE_PASSWORD', ''),
+        charset: 'utf8mb4',
       },
       options: {
-        useNullAsDefault: true,
+        charset: 'utf8mb4',
       },
     },
   },

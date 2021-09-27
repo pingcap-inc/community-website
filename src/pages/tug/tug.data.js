0c26848763561914a8a8db6cfd26f2ed88caf919
cw1997 <changwei1006@gmail.com>
2021-09-27 21:13:44 +0800

    CP-224 refactor: migrate the TUG member data file

--- src/pages/tug/tug.data.js
+++ src/pages/tug/tug.data.js
@@ -1,7 +1,3 @@
-import mva2019 from './mva2019';
-import mva2020 from './mva2020';
-import mva2021 from './mva2021';
-
 const data = {
   tmc: [
     {
@@ -177,10 +173,6 @@ const data = {
       role: 'TUG 顾问团成员',
     },
   ],
-
-  mva2019,
-  mva2020,
-  mva2021,
 };
 
 export default data;

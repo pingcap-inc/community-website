5db3a8d7be275b7e80b54f2de3957bbcd77281ab
Nick Qi <wei.qi.nick@gmail.com>
2021-09-27 22:58:40 +1300

    CP-223 fix: fix the date filter logic

--- src/pages/activities/index.page.js
+++ src/pages/activities/index.page.js
@@ -1,3 +1,4 @@
+import * as R from 'ramda';
 import React from 'react';
 import useSWR from 'swr';
 import { api } from '@tidb-community/datasource';
@@ -49,15 +50,37 @@ const Page = () => {
   const { t } = useTranslation('page-activities');
   const { filters, current, pageSize } = useSelector((state) => state.activities);
 
+  const dataParam = (() => {
+    let year;
+    const { date } = filters;
+
+    if (!date || date === R.head(DATES)) {
+      return;
+    }
+
+    if (date === R.last(DATES)) {
+      year = parseInt(R.compose(R.head, R.takeLast(2))(DATES), 10);
+      return {
+        date_lt: year,
+      };
+    }
+
+    year = parseInt(date, 10);
+    return {
+      date_gte: year,
+      date_lte: year + 1,
+    };
+  })();
+
   const params = {
     _limit: pageSize,
     _publicationState: isProd ? undefined : 'preview',
     _sort: 'date:DESC',
     _start: (current - 1) * pageSize,
-    category: filters.category === CATEGORIES[0] ? undefined : filters.category,
-    type: filters.type === TYPES[0] ? undefined : filters.type,
-    date: filters.date === DATES[0] ? undefined : filters.date,
-    location: filters.location === LOCATIONS[0] ? undefined : filters.location,
+    category: filters.category === R.head(CATEGORIES) ? undefined : filters.category,
+    type: filters.type === R.head(TYPES) ? undefined : filters.type,
+    location: filters.location === R.head(LOCATIONS) ? undefined : filters.location,
+    ...dataParam,
   };
   const { data: activities = [] } = useSWR(['tidbio-activitiespage-activities', JSON.stringify(params)], fetcher);
   const { data: total } = useSWR(['tidbio-activitiespage-activities/count', JSON.stringify(params)], fetcher);

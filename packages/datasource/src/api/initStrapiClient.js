7d051e772e7fdd5da5bd999d49792dc1586d539f
Nick Qi <wei.qi.nick@gmail.com>
2021-09-28 23:00:48 +1300

    CP-222 fix: fix a destructuring issue

--- packages/datasource/src/api/initStrapiClient.js
+++ packages/datasource/src/api/initStrapiClient.js
@@ -1,17 +1,15 @@
 import axios from 'axios';
 
 const initStrapiClient = async ({ baseUrl, email, password } = {}) => {
-  const { env } = process;
-
   const client = axios.create({
-    baseURL: baseUrl || env.NEXT_PUBLIC_STRAPI_BASE_URL,
+    baseURL: baseUrl ?? process.env.NEXT_PUBLIC_STRAPI_BASE_URL,
   });
 
   client.interceptors.response.use(({ data }) => data);
 
   const authResp = await client.post('/admin/login', {
-    email: email || env.NEXT_PUBLIC_STRAPI_EMAIL,
-    password: password || env.NEXT_PUBLIC_STRAPI_PASSWORD,
+    email: email ?? process.env.NEXT_PUBLIC_STRAPI_EMAIL,
+    password: password ?? process.env.NEXT_PUBLIC_STRAPI_PASSWORD,
   });
 
   client.interceptors.request.use((config) => {

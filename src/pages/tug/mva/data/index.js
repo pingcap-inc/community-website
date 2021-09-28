76c00dd1275a1251359e7d7e82572f005f3f4e80
Chang Wei <changwei@pingcap.com>
2021-09-28 15:10:02 +0800

    CP-224: add people to tug (#279)
    * CP-224 feat: add tug member to tug page data
    
    * CP-224 refactor: migrate the TUG member data file
    
    * CP-224 refactor: migrate the MVA member data file
    
    * CP-224 refactor: update HomeMVA component, use antd.button component instead of custom button component
    
    * CP-224 fix: fix title bug on tug page
    
    * CP-224: update tug data

---
+++ src/pages/tug/mva/data/index.js
@@ -0,0 +1,11 @@
+import mva2019 from './mva2019';
+import mva2020 from './mva2020';
+import mva2021 from './mva2021';
+
+const mvaData = {
+  mva2019,
+  mva2020,
+  mva2021,
+};
+
+export default mvaData;

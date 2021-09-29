b6c4024c2d0697cb68c2d37547eb285eac3063dc
Nick Qi <wei.qi.nick@gmail.com>
2021-09-29 21:34:49 +1300

    CP-223 style: align logos image to the right on desktop

--- src/pages/activities/cooperation/cooperation.styled.js
+++ src/pages/activities/cooperation/cooperation.styled.js
@@ -1,14 +1,29 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import LogoSvg from './logo.svg';
+import { ActionButton, Container, TwoColumns as RawTwoColumns } from '~/pages/home/subscription/subscription.styled';
 
-export { Container, TwoColumns, ActionButton } from '~/pages/home/subscription/subscription.styled';
+export { ActionButton, Container };
+
+export const TwoColumns = styled(RawTwoColumns)`
+  ${(props) =>
+    props.isSmallScreen &&
+    css`
+      ${RightPanel} {
+        text-align: center;
+      }
+    `}
+`;
 
 export const LeftPanel = styled.div`
   p {
     font-size: 18px;
     margin-bottom: 0;
   }
+`;
+
+export const RightPanel = styled.div`
+  text-align: right;
 `;
 
 export const Logo = styled(LogoSvg)`

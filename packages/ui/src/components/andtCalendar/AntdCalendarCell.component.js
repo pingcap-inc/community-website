a1ac920589f8507052d4440e6a6e9aaada20a1b4
Chang Wei <m11002815@gapps.ntust.edu.tw>
2021-10-09 22:21:56 -0700

    CP-162 feat: update antdCalendar story data

--- packages/ui/src/components/andtCalendar/AntdCalendarCell.component.js
+++ packages/ui/src/components/andtCalendar/AntdCalendarCell.component.js
@@ -4,6 +4,8 @@ import moment from 'moment';
 
 import * as Styled from './AntdCalendarCell.styled';
 import AntdCalendarCellCard from './AntdCalendarCellCard.component';
+import { EventExtension } from './AntdCalendarCell.styled';
+import { getColorByType } from './utils';
 
 const AntdCalendarCell = ({ date, data }) => {
   return (
@@ -15,13 +17,31 @@ const AntdCalendarCell = ({ date, data }) => {
           .filter((value) => {
             const startDate = moment(value.startDate);
             const endDate = moment(value.endDate);
-            return date.isAfter(startDate) && date.isBefore(endDate);
+            return date.isSame(startDate) || date.isSame(endDate) || (date.isAfter(startDate) && date.isBefore(endDate));
           })
-          .map((value, key) => (
-            <Popover content={<AntdCalendarCellCard data={value} />} title="" trigger="click">
-              <Styled.Event key={key}>{value.title}</Styled.Event>
-            </Popover>
-          ))}
+          .map((value, key) => {
+            // const width = (7 - date.day()) *
+            const startDate = moment(value.startDate);
+            // const endDate = moment(value.endDate);
+            const isStartDate = date.format('YYYYMMDD') === startDate.format('YYYYMMDD');
+            const backgroundColor = `${getColorByType(value.type)}1E`
+            const width = `${getColorByType(value.type)}1E`
+            return (
+              <Popover key={key} content={<AntdCalendarCellCard data={value} />} title="" trigger="click">
+                {/*{isStartDate ? */}
+                {/*  <Styled.Event key={key}>{value.title}</Styled.Event>*/}
+                {/*  :*/}
+                {/*  <Styled.EventExtension key={key}/>*/}
+                {/*}*/}
+                <Styled.Event style={{backgroundColor}}>
+                  <Styled.EventInner>
+                    {isStartDate ? value.title : ''}
+                  </Styled.EventInner>
+                </Styled.Event>
+                {/*<Styled.Event key={key}>{value.title}</Styled.Event>*/}
+              </Popover>
+            )
+          })}
       </Styled.List>
     </Styled.Wrapper>
   );
--- packages/ui/src/components/andtCalendar/AntdCalendarCell.styled.js
+++ packages/ui/src/components/andtCalendar/AntdCalendarCell.styled.js
@@ -7,6 +7,8 @@ export const Wrapper = styled.div`
 
   font-weight: 400;
   font-size: 16px;
+  
+  //overflow: visible;
 `;
 
 export const Line = styled.div`
@@ -21,14 +23,33 @@ export const Day = styled.div`
 
 export const List = styled.div`
   box-sizing: border-box;
-  height: 60px;
+  height: 120px;
+  //overflow: hidden;
 `;
 
 export const Event = styled.div`
-  div {
-    padding: 0;
-  }
-
-  background-color: lightblue;
+  //background-color: lightblue; 
   //border: 1px solid darkgray;
+  margin: 2px 0;
+  height: 20px;
+`;
+
+export const EventInner = styled.div`
+  z-index: 99999;
+  position: absolute;
+  font-size: 16px;
+  //overflow:hidden; //超出的文本隐藏
+  //text-overflow:ellipsis; //溢出用省略号显示
+  white-space:nowrap; //溢出不换行
+`;
+
+export const EventExtension = styled.div`
+  //border: 1px solid darkgray;
+  overflow: visible;
+  margin: 2px 0;
+  height: 20px;
+  font-size: 16px;
+  //overflow:hidden; //超出的文本隐藏
+  //text-overflow:ellipsis; //溢出用省略号显示
+  white-space:nowrap; //溢出不换行
 `;

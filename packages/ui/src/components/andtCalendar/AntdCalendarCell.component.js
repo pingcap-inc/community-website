49c29bb3d5374f5f0001a770a9486c25e9ca8156
Chang Wei <m11002815@gapps.ntust.edu.tw>
2021-10-10 21:03:44 -0700

    CP-162 style: reformat code, remove unused code

--- packages/ui/src/components/andtCalendar/AntdCalendarCell.component.js
+++ packages/ui/src/components/andtCalendar/AntdCalendarCell.component.js
@@ -4,7 +4,6 @@ import moment from 'moment';
 
 import * as Styled from './AntdCalendarCell.styled';
 import AntdCalendarCellCard from './AntdCalendarCellCard.component';
-import { EventExtension } from './AntdCalendarCell.styled';
 import { getColorByType } from './utils';
 
 const AntdCalendarCell = ({ date, data }) => {
@@ -17,30 +16,21 @@ const AntdCalendarCell = ({ date, data }) => {
           .filter((value) => {
             const startDate = moment(value.startDate);
             const endDate = moment(value.endDate);
-            return date.isSame(startDate) || date.isSame(endDate) || (date.isAfter(startDate) && date.isBefore(endDate));
+            return (
+              date.isSame(startDate) || date.isSame(endDate) || (date.isAfter(startDate) && date.isBefore(endDate))
+            );
           })
           .map((value, key) => {
-            // const width = (7 - date.day()) *
             const startDate = moment(value.startDate);
-            // const endDate = moment(value.endDate);
             const isStartDate = date.format('YYYYMMDD') === startDate.format('YYYYMMDD');
-            const backgroundColor = `${getColorByType(value.type)}1E`
-            const width = `${getColorByType(value.type)}1E`
+            const backgroundColor = `${getColorByType(value.type)}1E`;
             return (
               <Popover key={key} content={<AntdCalendarCellCard data={value} />} title="" trigger="click">
-                {/*{isStartDate ? */}
-                {/*  <Styled.Event key={key}>{value.title}</Styled.Event>*/}
-                {/*  :*/}
-                {/*  <Styled.EventExtension key={key}/>*/}
-                {/*}*/}
-                <Styled.Event style={{backgroundColor}}>
-                  <Styled.EventInner>
-                    {isStartDate ? value.title : ''}
-                  </Styled.EventInner>
+                <Styled.Event style={{ backgroundColor }}>
+                  <Styled.EventInner>{isStartDate ? value.title : ''}</Styled.EventInner>
                 </Styled.Event>
-                {/*<Styled.Event key={key}>{value.title}</Styled.Event>*/}
               </Popover>
-            )
+            );
           })}
       </Styled.List>
     </Styled.Wrapper>

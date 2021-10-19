2d63433fdaae6e962757818d45ffcc9230d2cc07
Chang Wei <m11002815@gapps.ntust.edu.tw>
2021-10-19 01:57:22 -0700

    CP-162: update style of about page

--- src/pages/events/about/About.component.js
+++ src/pages/events/about/About.component.js
@@ -1,22 +1,27 @@
 import Image from 'next/image';
 import React, { useContext, useRef } from 'react';
-import { Button, Col, Row } from 'antd';
-import { useTranslation } from 'next-i18next';
+import { Button, Row } from 'antd';
+import { Trans, useTranslation } from 'next-i18next';
 import { useSize } from 'ahooks';
 import { common as commonUtils, link as linkUtils } from '~/utils';
 
 import { MyFullCalendar } from '@tidb-community/ui';
 
 import * as Styled from './about.styled';
+import { Link } from '~/components';
 import { useIsSmallScreen } from '~/hooks';
 import { PageDataContext } from '~/context';
 import { useRouter } from 'next/router';
+import { LeftPanel, RightPanel } from './about.styled';
 
 const About = () => {
   const cardRef = useRef();
   const cardSize = useSize(cardRef);
   const { isSmallScreen } = useIsSmallScreen();
   const { t } = useTranslation('page-events');
+
+  const lang = t('about', { returnObjects: true });
+  const { paragraph2: paragraph2Lang } = lang;
 
   const calendar = t('calendar', { returnObjects: true });
   const about = t('about', { returnObjects: true });
@@ -43,45 +48,58 @@ const About = () => {
   return (
     <Styled.Container isSmallScreen={isSmallScreen}>
       <Styled.Content>
-        <Styled.TopSection>
-          <Styled.Title>{calendar.title}</Styled.Title>
-          <Styled.CalendarCard>
-            <MyFullCalendar
-              data={
-                events?.map((val, idx) => {
-                  const image = commonUtils.getStrapiImgProps(val.image);
-                  return {
-                    key: idx,
-                    ...val,
-                    image,
-                  };
-                }) ?? []
-              }
-            />
-          </Styled.CalendarCard>
-        </Styled.TopSection>
-
-        <Styled.TopSection>
-          <Styled.Title>{about.title}</Styled.Title>
-          <Row gutter={[32, 32]}>
-            {card.map((v, i) => (
-              <Col key={i} sm={24} md={12}>
-                <Styled.Card ref={cardRef}>
-                  <Styled.CardImg $isVertical={isVerticalCard}>
-                    <Image alt={v.title} src={v.image} layout="fill" objectFit="cover" />
-                  </Styled.CardImg>
-                  <Styled.CardInfo $isVertical={isVerticalCard}>
-                    <h3>{v.title}</h3>
-                    <p>{getDesc(v.desc)}</p>
-                    <Button type="primary" size="small" onClick={handleCardButtonClick(v.link)}>
-                      {v.button}
-                    </Button>
-                  </Styled.CardInfo>
-                </Styled.Card>
-              </Col>
-            ))}
-          </Row>
-        </Styled.TopSection>
+        <Row gutter={[64, 64]}>
+          <LeftPanel>
+            <Styled.TopSection>
+              <Styled.Title>{about.title}</Styled.Title>
+              <Styled.Desc>
+                <p>{lang.paragraph1}</p>
+                <p>
+                  <Trans
+                    t={t}
+                    i18nKey={'about.paragraph2.text'}
+                    components={[<Link href={paragraph2Lang.link1} />, <Link href={paragraph2Lang.link2} />]}
+                  />
+                </p>
+              </Styled.Desc>
+              <Row gutter={[32, 32]}>
+                {card.map((v, i) => (
+                  <Styled.Card ref={cardRef}>
+                    <Styled.CardImg $isVertical={isVerticalCard}>
+                      <Image alt={v.title} src={v.image} layout="fill" objectFit="cover" />
+                    </Styled.CardImg>
+                    <Styled.CardInfo $isVertical={isVerticalCard}>
+                      <h3>{v.title}</h3>
+                      <p>{getDesc(v.desc)}</p>
+                      <Button type="primary" size="small" onClick={handleCardButtonClick(v.link)}>
+                        {v.button}
+                      </Button>
+                    </Styled.CardInfo>
+                  </Styled.Card>
+                ))}
+              </Row>
+            </Styled.TopSection>
+          </LeftPanel>
+          <RightPanel>
+            <Styled.TopSection>
+              <Styled.Title>{calendar.title}</Styled.Title>
+              <Styled.CalendarCard>
+                <MyFullCalendar
+                  data={
+                    events?.map((val, idx) => {
+                      const image = commonUtils.getStrapiImgProps(val.image);
+                      return {
+                        key: idx,
+                        ...val,
+                        image,
+                      };
+                    }) ?? []
+                  }
+                />
+              </Styled.CalendarCard>
+            </Styled.TopSection>
+          </RightPanel>
+        </Row>
       </Styled.Content>
     </Styled.Container>
   );

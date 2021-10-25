a8c8b8c71d42432aa2eddec85a6784d560bcf6b9
Chang Wei <m11002815@gapps.ntust.edu.tw>
2021-10-25 01:18:56 -0700

    CP-162: use less instead of scss for avoid the css loader problems happened

--- packages/ui/src/components/myFullCalendar/MyFullCalendar.component.js
+++ packages/ui/src/components/myFullCalendar/MyFullCalendar.component.js
@@ -6,7 +6,7 @@ import allLocales from '@fullcalendar/core/locales-all';
 import FullCalendarCellCard from './FullCalendarCellCard.component';
 import { getColorByType, processData } from './utils';
 
-import styles from './styles.module.scss';
+import styles from './styles.module.less';
 
 const MyFullCalendar = ({ data }) => {
   const renderEventContent = (eventInfo) => (

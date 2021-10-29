6fb76681c756c2247b448f66aeb9fd410fbc8a4b
Jagger <634750802@qq.com>
2021-10-29 17:36:20 +0800

    CP-267 feat: blog info component   (#293)
    * feat(CP-267): first impl for blog info component
    
    * feat: refine props
    
    * chore: use community theme color

--- packages/ui/src/components/index.js
+++ packages/ui/src/components/index.js
@@ -11,5 +11,6 @@ export { default as RemoteAutoComplete } from './remoteAutoComplete';
 export { default as Upload } from './upload';
 export { default as UserProfile } from './userProfile';
 export { default as ViewMoreButton } from './viewMoreButton';
+export { default as BlogInfo } from './blogInfo';
 export { default as withActionMenu } from './actionMenu';
 export { default as withVerifyCode } from './verifyCodeInput';

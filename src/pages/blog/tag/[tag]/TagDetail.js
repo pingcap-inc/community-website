e8aa09373f7cf7d16046d1e74053b49e107b27b1
Chang Wei <m11002815@gapps.ntust.edu.tw>
2021-11-03 06:07:56 -0700

    CP-250: add TagDetail page

---
+++ src/pages/blog/tag/[tag]/TagDetail.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as Styled from './index.styled';
+
+const TagDetail = () => {
+  return <Styled.Container>TagDetail</Styled.Container>;
+};
+
+export default TagDetail;

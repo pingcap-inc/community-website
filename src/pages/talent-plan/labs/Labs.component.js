00d3b58d30789d1c5a91f1908c6251e6d7d3ad85
Xiyan Shao <xiyan128@gmail.com>
2021-11-03 22:16:25 -0700

    CP-277 feat: labs section

--- src/pages/talent-plan/labs/Labs.component.js
+++ src/pages/talent-plan/labs/Labs.component.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Col, Row } from 'antd';
+import { Col, Image, Row } from 'antd';
 import { GithubOutlined } from '@ant-design/icons';
 import { useRouter } from 'next/router';
 
 import * as Styled from './labs.styled';
+import { Styled as CommonStyled } from '@tidb-community/ui';
 import { common as commonUtils } from '~/utils';
 import { useIsSmallScreen } from '~/hooks';
 import { useTranslation } from 'next-i18next';
+import { LabCardImgWrapper } from './labs.styled';
 
 const Labs = () => {
   const router = useRouter();
@@ -16,7 +18,29 @@ const Labs = () => {
 
   const lang = t('labs', { returnObjects: true });
 
-  return <Styled.Container>{lang.intro}</Styled.Container>;
+  return (
+    <Styled.Container>
+      <Styled.Content>
+        <CommonStyled.Title>{lang.title}</CommonStyled.Title>
+        <Row gutter={16}>
+          {lang.orgs.map((org, index) => (
+            <Col lg={8} md={12} xs={24} key={org.name}>
+              <Styled.LabCard>
+                <Row gutter={4}>
+                  <Styled.LabCardImgWrapper span={12}>
+                    <Image src={org.img}> </Image>
+                  </Styled.LabCardImgWrapper>
+                  <Col span={12}>
+                    <Styled.LabCardTitle> {org.name} </Styled.LabCardTitle>
+                  </Col>
+                </Row>
+              </Styled.LabCard>
+            </Col>
+          ))}
+        </Row>
+      </Styled.Content>
+    </Styled.Container>
+  );
 };
 
 export default Labs;

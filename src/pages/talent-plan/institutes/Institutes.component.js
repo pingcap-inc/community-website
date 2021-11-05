e05cf40a3e93cf16d8c30998f1a43992c91104eb
Xiyan Shao <xiyan128@gmail.com>
2021-11-05 00:17:36 -0700

    CP-277 feat: institutes section basic layout

--- src/pages/talent-plan/institutes/Institutes.component.js
+++ src/pages/talent-plan/institutes/Institutes.component.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Col, Row } from 'antd';
-import { GithubOutlined } from '@ant-design/icons';
 import { useRouter } from 'next/router';
 
 import * as Styled from './institutes.styled';
-import { common as commonUtils } from '~/utils';
 import { useIsSmallScreen } from '~/hooks';
 import { useTranslation } from 'next-i18next';
+import { Styled as CommonStyled } from '@tidb-community/ui';
+import { Col, Row } from 'antd';
 
 const Institutes = () => {
   const router = useRouter();
@@ -16,7 +15,36 @@ const Institutes = () => {
 
   const lang = t('institutes', { returnObjects: true });
 
-  return <Styled.Container>{lang.intro}</Styled.Container>;
+  return (
+    <Styled.Container>
+      <Styled.Content>
+        <CommonStyled.Title>{lang.title}</CommonStyled.Title>
+        <Row gutter={16}>
+          <Col span={8}>
+            <Styled.ListCard>
+              {lang.names.map((name) => (
+                <Styled.ListItem key={name}> {name} </Styled.ListItem>
+              ))}
+            </Styled.ListCard>
+          </Col>
+          <Col span={8}>
+            <Styled.ListCard>
+              {lang.names.map((name) => (
+                <Styled.ListItem key={name}> {name} </Styled.ListItem>
+              ))}
+            </Styled.ListCard>
+          </Col>
+          <Col span={8}>
+            <Styled.ListCard>
+              {lang.names.map((name) => (
+                <Styled.ListItem key={name}> {name} </Styled.ListItem>
+              ))}
+            </Styled.ListCard>
+          </Col>
+        </Row>
+      </Styled.Content>
+    </Styled.Container>
+  );
 };
 
 export default Institutes;
--- src/pages/talent-plan/institutes/institutes.styled.js
+++ src/pages/talent-plan/institutes/institutes.styled.js
@@ -1,10 +1,37 @@
 import styled from 'styled-components';
 import { colors, Styled } from '@tidb-community/ui';
+import { getImage } from '~/pages/talent-plan/talent-plan.utils';
 
 export const Container = styled(Styled.Section)`
   && {
     color: ${colors.M1};
-    background: ${colors.B4};
     padding: 0;
+  }
+`;
+
+export const Content = styled(Styled.Content)`
+  position: relative;
+  padding: 5rem 1rem 7.5rem;
+`;
+
+export const ListCard = styled.div`
+  box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14),
+    0px 2px 1px -1px rgba(0, 0, 0, 0.12);
+  border-radius: 4px;
+  background-color: ${colors.M2};
+  padding: 32px;
+`;
+
+export const ListItem = styled.div`
+  color: ${colors.F1};
+  line-height: 2.5;
+
+  &:before {
+    background-size: 24px 24px;
+    height: 24px;
+    width: 24px;
+    display: inline-block;
+    content: '  ';
+    background-image: url('${getImage('institutes-item-icon.svg')}');
   }
 `;

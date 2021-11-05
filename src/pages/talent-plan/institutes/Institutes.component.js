5dc2c3f946ff9223c3d5633e1b2d4a1589cb3e3c
Xiyan Shao <xiyan128@gmail.com>
2021-11-05 14:20:01 -0700

    CP-277 feat: completed institutes

--- src/pages/talent-plan/institutes/Institutes.component.js
+++ src/pages/talent-plan/institutes/Institutes.component.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useRouter } from 'next/router';
 
 import * as Styled from './institutes.styled';
 import { useIsSmallScreen } from '~/hooks';
@@ -8,40 +7,47 @@ import { Styled as CommonStyled } from '@tidb-community/ui';
 import { Col, Row } from 'antd';
 
 const Institutes = () => {
-  const router = useRouter();
-
-  const { isSmallScreen, breakpoint } = useIsSmallScreen();
+  const { isSmallScreen } = useIsSmallScreen();
   const { t } = useTranslation('page-talent-plan');
 
   const lang = t('institutes', { returnObjects: true });
+
+  const MAX_LIST_ITEMS = 10;
+  // split list into chunks of 15
+  const chunks = [];
+  for (let i = 0; i < lang.names.length; i += MAX_LIST_ITEMS) {
+    chunks.push(lang.names.slice(i, i + MAX_LIST_ITEMS));
+  }
+
+  const PAGE_SIZE = isSmallScreen ? 1 : 3;
+  const pages = [];
+  for (let i = 0; i < chunks.length; i += PAGE_SIZE) {
+    pages.push(chunks.slice(i, i + PAGE_SIZE));
+  }
 
   return (
     <Styled.Container>
       <Styled.Content>
         <CommonStyled.Title>{lang.title}</CommonStyled.Title>
-        <Row gutter={16}>
-          <Col span={8}>
-            <Styled.ListCard>
-              {lang.names.map((name) => (
-                <Styled.ListItem key={name}> {name} </Styled.ListItem>
-              ))}
-            </Styled.ListCard>
-          </Col>
-          <Col span={8}>
-            <Styled.ListCard>
-              {lang.names.map((name) => (
-                <Styled.ListItem key={name}> {name} </Styled.ListItem>
-              ))}
-            </Styled.ListCard>
-          </Col>
-          <Col span={8}>
-            <Styled.ListCard>
-              {lang.names.map((name) => (
-                <Styled.ListItem key={name}> {name} </Styled.ListItem>
-              ))}
-            </Styled.ListCard>
-          </Col>
-        </Row>
+        <Styled.Carousel>
+          {pages.map((page) => (
+            <Styled.CarouselContent>
+              {
+                <Row gutter={16}>
+                  {page.map((chunk) => (
+                    <Col key={chunk[0]} span={isSmallScreen ? 24 : 8}>
+                      <Styled.ListCard>
+                        {chunk.map((name) => (
+                          <Styled.ListItem key={name}> {name} </Styled.ListItem>
+                        ))}
+                      </Styled.ListCard>
+                    </Col>
+                  ))}
+                </Row>
+              }
+            </Styled.CarouselContent>
+          ))}
+        </Styled.Carousel>
       </Styled.Content>
     </Styled.Container>
   );
--- src/pages/talent-plan/institutes/institutes.styled.js
+++ src/pages/talent-plan/institutes/institutes.styled.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
-import { colors, Styled } from '@tidb-community/ui';
+import { colors, mixins, Styled } from '@tidb-community/ui';
+import * as polished from 'polished';
 import { getImage } from '~/pages/talent-plan/talent-plan.utils';
+import { Carousel as AntdCarousel } from 'antd';
 
 export const Container = styled(Styled.Section)`
   && {
@@ -19,14 +21,18 @@ export const ListCard = styled.div`
     0px 2px 1px -1px rgba(0, 0, 0, 0.12);
   border-radius: 4px;
   background-color: ${colors.M2};
-  padding: 32px;
+  padding: 48px;
+  height: 100%;
 `;
 
 export const ListItem = styled.div`
+  font-size: 16px;
   color: ${colors.F1};
   line-height: 2.5;
+  vertical-align: middle;
 
   &:before {
+    vertical-align: middle;
     background-size: 24px 24px;
     height: 24px;
     width: 24px;
@@ -34,4 +40,31 @@ export const ListItem = styled.div`
     content: '  ';
     background-image: url('${getImage('institutes-item-icon.svg')}');
   }
+`;
+
+export const Carousel = styled(AntdCarousel).attrs({ autoplay: true, autoplaySpeed: 4000 })`
+  .slick-dots {
+    li {
+      &,
+      button {
+        ${mixins.size('8px', '8px')}
+        background: ${polished.rgba(colors.F2, 0.3)};
+        border-radius: 4px;
+      }
+
+      &.slick-active {
+        &,
+        button {
+          ${mixins.size('8px', '8px')}
+          background: ${polished.rgba(colors.F2, 0.5)};
+        }
+      }
+    }
+  }
+`;
+
+export const CarouselContent = styled.div`
+  padding-bottom: 3rem;
+  padding-right: 0.2rem;
+  padding-left: 0.2rem;
 `;

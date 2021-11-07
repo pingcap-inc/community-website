07880b9b218965dea890161170c20bfe6a8ca1dd
Xiyan Shao <xiyan128@gmail.com>
2021-11-06 23:27:01 -0700

    CP-277 feat: completed team section

--- src/pages/talent-plan/team/Team.component.js
+++ src/pages/talent-plan/team/Team.component.js
@@ -1,22 +1,38 @@
 import React from 'react';
-import { Col, Row } from 'antd';
-import { GithubOutlined } from '@ant-design/icons';
-import { useRouter } from 'next/router';
-
+import { Styled as CommonStyled } from '@tidb-community/ui';
 import * as Styled from './team.styled';
-import { common as commonUtils } from '~/utils';
-import { useIsSmallScreen } from '~/hooks';
 import { useTranslation } from 'next-i18next';
+import { Row } from 'antd';
+import { getImage } from '~/pages/talent-plan/talent-plan.utils';
 
 const Team = () => {
-  const router = useRouter();
-
-  const { isSmallScreen, breakpoint } = useIsSmallScreen();
   const { t } = useTranslation('page-talent-plan');
 
   const lang = t('team', { returnObjects: true });
 
-  return <Styled.Container>{lang.intro}</Styled.Container>;
+  return (
+    <Styled.Container>
+      <Styled.Content>
+        <CommonStyled.Title> {lang.title} </CommonStyled.Title>
+        <Row gutter={16}>
+          {lang.members.map((member) => (
+            <Styled.CardWrapper lg={6} xs={12} key={member.nameEN}>
+              <Styled.MemberCard>
+                <Styled.MemberCardAvatar size={48} src={getImage(`team-${member.nameZH}.png`)} />
+                <Styled.MemberCardContent>
+                  <Styled.MemberCardHeader>{member.nameZH}</Styled.MemberCardHeader>
+                  <Styled.MemberCardLine>{member.nameEN}</Styled.MemberCardLine>
+                  <Styled.MemberCardLine>{member.org}</Styled.MemberCardLine>
+                  <Styled.MemberCardLine>{member.role}</Styled.MemberCardLine>
+                  <Styled.MemberCardLine>{member.github}</Styled.MemberCardLine>
+                </Styled.MemberCardContent>
+              </Styled.MemberCard>
+            </Styled.CardWrapper>
+          ))}
+        </Row>
+      </Styled.Content>
+    </Styled.Container>
+  );
 };
 
 export default Team;
--- src/pages/talent-plan/team/team.styled.js
+++ src/pages/talent-plan/team/team.styled.js
@@ -1,10 +1,50 @@
 import styled from 'styled-components';
-import { colors, Styled } from '@tidb-community/ui';
+import { colors, mixins, Styled } from '@tidb-community/ui';
+import { Avatar, Col } from 'antd';
 
 export const Container = styled(Styled.Section)`
   && {
     color: ${colors.M1};
-    background: ${colors.B4};
     padding: 0;
   }
+`;
+
+export const CardWrapper = styled(Col)`
+  padding-bottom: 1rem;
+`;
+
+export const MemberCard = styled.div`
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.2), 0 1px 1px 0 rgba(0, 0, 0, 0.14), 0 2px 1px -1px rgba(0, 0, 0, 0.12);
+  border-radius: 4px;
+  background-color: ${colors.M2};
+  padding: 1rem;
+  height: 100%;
+  display: flex;
+`;
+
+export const MemberCardAvatar = styled(Avatar)`
+  min-width: 48px;
+`;
+
+export const MemberCardContent = styled.div`
+  margin-left: 1rem;
+  color: ${colors.F1};
+`;
+
+export const MemberCardHeader = styled.div`
+  font-weight: bold;
+`;
+
+export const MemberCardLine = styled.div`
+  color: ${colors.F2};
+  font-size: 14px;
+  font-weight: normal;
+  word-wrap: break-word;
+  text-overflow: ellipsis;
+  ${mixins.lineClamp(1)};
+`;
+
+export const Content = styled(Styled.Content)`
+  position: relative;
+  padding: 5rem 1rem 7.5rem;
 `;

647850033a3acc19cb4a253cf1ddcb5aaf7de328
Chang Wei <m11002815@gapps.ntust.edu.tw>
2021-11-10 00:00:09 -0800

    CP-282: add posts mock api

--- packages/mockApi/routes/blog/index.js
+++ packages/mockApi/routes/blog/index.js
@@ -2,5 +2,7 @@ const router = require('express').Router();
 
 router.get('/categories', require('./categories/categories'));
 router.get('/categories/:id', require('./categories/[id]'));
+router.get('/posts', require('./posts/posts'));
+router.get('/posts/:id', require('./posts/[id]'));
 
 module.exports = router;

659e211fdfac482fa1efc9a64bdb8108169e21b5
Chang Wei <m11002815@gapps.ntust.edu.tw>
2021-11-10 00:18:42 -0800

    CP-282: fix blog mock api router

--- packages/mockApi/routes/blog/index.js
+++ packages/mockApi/routes/blog/index.js
@@ -2,7 +2,12 @@ const router = require('express').Router();
 
 router.get('/categories', require('./categories/categories'));
 router.get('/categories/:id', require('./categories/[id]'));
+router.post('/categories/:id', require('./categories/create-categories'));
+router.put('/categories/:id', require('./categories/update-categories'));
+
 router.get('/posts', require('./posts/posts'));
 router.get('/posts/:id', require('./posts/[id]'));
+router.post('/posts/:id', require('./posts/create-posts'));
+router.put('/posts/:id', require('./posts/update-posts'));
 
 module.exports = router;

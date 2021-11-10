7ba0823f8644fd1838fe257c1670e09e95671798
Chang Wei <m11002815@gapps.ntust.edu.tw>
2021-11-10 00:31:38 -0800

    CP-282: fix blog mock api router, add delete posts and categories mock api

--- packages/mockApi/routes/blog/index.js
+++ packages/mockApi/routes/blog/index.js
@@ -2,12 +2,14 @@ const router = require('express').Router();
 
 router.get('/categories', require('./categories/categories'));
 router.get('/categories/:id', require('./categories/[id]'));
-router.post('/categories/:id', require('./categories/create-categories'));
+router.post('/categories', require('./categories/create-categories'));
 router.put('/categories/:id', require('./categories/update-categories'));
+router.delete('/categories/:id', require('./categories/delete-categories'));
 
 router.get('/posts', require('./posts/posts'));
 router.get('/posts/:id', require('./posts/[id]'));
-router.post('/posts/:id', require('./posts/create-posts'));
+router.post('/posts', require('./posts/create-posts'));
 router.put('/posts/:id', require('./posts/update-posts'));
+router.delete('/posts/:id', require('./posts/delete-posts'));
 
 module.exports = router;

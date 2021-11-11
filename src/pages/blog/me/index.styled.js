e676d9d7640b414c838878148a48524c543add4e
Chang Wei <m11002815@gapps.ntust.edu.tw>
2021-11-10 20:42:58 -0800

    CP-255: add Tab component, show number on tab

---
+++ src/pages/blog/me/index.styled.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+import { colors, Styled } from '@tidb-community/ui';
+import { Breadcrumb as AntdBreadcrumb } from 'antd';
+
+const { Content } = Styled;
+
+export { Content };
+
+export const Container = styled.div`
+  padding: 2rem 1rem;
+`;
+
+export const Breadcrumb = styled(AntdBreadcrumb)``;
+
+export const Tab = styled.div`
+  display: flex;
+  padding: 1rem 0;
+`;
+
+export const List = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export const TabItem = styled.div`
+  cursor: pointer;
+  margin: 0 0.5rem;
+  color: ${(props) => (props.selected ? colors.T7 : '')};
+  border-bottom: ${(props) => (props.selected ? `4px solid ${colors.T7}` : 'none')};
+`;
+
+export const Item = styled.div`
+  margin: 0.5rem 0;
+`;

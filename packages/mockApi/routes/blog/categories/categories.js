df3e60f5d3e83a4cc996e3e8b0d4f6e028e93144
Chang Wei <m11002815@gapps.ntust.edu.tw>
2021-11-15 06:36:11 -0800

    CP-249: fix mock api scheme

--- packages/mockApi/routes/blog/categories/categories.js
+++ packages/mockApi/routes/blog/categories/categories.js
@@ -1,28 +1,79 @@
 const { successResp } = require('../../../utils');
-const { categories } = require('./categories.mock');
-const { LOCALHOST } = require('../../../constants');
+// const { categories } = require('./categories.mock');
+// const { LOCALHOST } = require('../../../constants');
 
 module.exports = (req, res) => {
-  const { page, size } = req.query;
-  const _page = page ? Number.parseInt(page) : 1;
-  const _size = size ? Number.parseInt(size) : 20;
+  // const { page, size } = req.query;
+  // const _page = page ? Number.parseInt(page) : 1;
+  // const _size = size ? Number.parseInt(size) : 20;
   const data = {
-    _embedded: {
-      categories,
-    },
-    _links: {
-      self: {
-        href: `${LOCALHOST}/blog/api/categories?page=${_page}&size=${_size}`,
+    content: [
+      {
+        creatorId: 1,
+        createdAt: '2021-10-28T19:38:49.000+00:00',
+        modifierId: 1,
+        lastModifiedAt: '2021-10-28T19:38:49.000+00:00',
+        deletedAt: null,
+        id: 1,
+        name: '用户实践',
+        slug: 'user-practice',
+        weight: 10,
+        description: '用户实践',
       },
-      profile: {
-        href: `${LOCALHOST}/blog/api/profile/categories`,
+      {
+        creatorId: 1,
+        createdAt: '2021-10-28T19:38:49.000+00:00',
+        modifierId: 1,
+        lastModifiedAt: '2021-10-28T19:38:49.000+00:00',
+        deletedAt: null,
+        id: 2,
+        name: '原理解读',
+        slug: 'principle-interpretation',
+        weight: 10,
+        description: '原理解读',
       },
-    },
+      {
+        creatorId: 1,
+        createdAt: '2021-10-28T19:38:49.000+00:00',
+        modifierId: 1,
+        lastModifiedAt: '2021-10-28T19:38:49.000+00:00',
+        deletedAt: null,
+        id: 3,
+        name: '测评',
+        slug: 'evaluation',
+        weight: 10,
+        description: '测试与评价分析',
+      },
+      {
+        creatorId: 1,
+        createdAt: '2021-10-28T19:38:49.000+00:00',
+        modifierId: 1,
+        lastModifiedAt: '2021-10-28T19:38:49.000+00:00',
+        deletedAt: null,
+        id: 4,
+        name: '运维实践',
+        slug: 'operation-and-maintenance-practice',
+        weight: 10,
+        description: '运维实践',
+      },
+      {
+        creatorId: 1,
+        createdAt: '2021-10-28T19:38:49.000+00:00',
+        modifierId: 1,
+        lastModifiedAt: '2021-10-28T19:38:49.000+00:00',
+        deletedAt: null,
+        id: 5,
+        name: 'TUG 技术探讨',
+        slug: 'tug-technology-discussion',
+        weight: 10,
+        description: 'TUG 技术探讨',
+      },
+    ],
     page: {
-      size: _size,
-      totalElements: categories.length,
-      totalPages: Math.floor(categories.length / _size),
-      number: _page,
+      size: 20,
+      totalElements: 5,
+      totalPages: 1,
+      number: 1,
     },
   };
   return successResp(data)(req, res);
--- packages/mockApi/routes/blog/index.js
+++ packages/mockApi/routes/blog/index.js
@@ -6,7 +6,8 @@ router.post('/categories', require('./categories/create-categories'));
 router.put('/categories/:id', require('./categories/update-categories'));
 router.delete('/categories/:id', require('./categories/delete-categories'));
 
-router.get('/posts', require('./posts/posts'));
+router.get('/posts/latest', require('./posts/posts'));
+router.get('/posts/recommend', require('./posts/posts'));
 router.get('/posts/:id', require('./posts/[id]'));
 router.post('/posts', require('./posts/create-posts'));
 router.put('/posts/:id', require('./posts/update-posts'));

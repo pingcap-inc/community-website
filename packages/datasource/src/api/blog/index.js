b40e50aee302dd2a0b9cfa4c0737b3f6866c9006
Chang Wei <m11002815@gapps.ntust.edu.tw>
2021-11-16 13:51:42 +0800

    CP-249: fix mockapi mock data field name

--- packages/datasource/src/api/blog/index.js
+++ packages/datasource/src/api/blog/index.js
@@ -5,9 +5,9 @@ const BLOG_API_URL_PREFIX = 'blog/api';
 export * as posts from './posts';
 export * as common from './common';
 
-export const getLatest = () => client.get(`${BLOG_API_URL_PREFIX}/latest`);
+export const getLatest = () => client.get(`${BLOG_API_URL_PREFIX}/posts/latest`);
 
-export const getRecommend = () => client.get(`${BLOG_API_URL_PREFIX}/recommend`);
+export const getRecommend = () => client.get(`${BLOG_API_URL_PREFIX}/posts/recommend`);
 
 export const getTags = () => client.get(`${BLOG_API_URL_PREFIX}/tags`);
 

ae562f46722847e3679bf4e99f04859b56a2ba56
Chang Wei <m11002815@gapps.ntust.edu.tw>
2021-11-16 07:09:11 -0800

    CP-249: fix active style on OrderBySwitch component

---
+++ src/pages/blog/home/BlogHomepage/index.js
@@ -0,0 +1,53 @@
+import * as React from 'react';
+
+import * as styled from './index.styled';
+
+import { CommunityHead } from '~/components';
+import { PageDataContext } from '~/context';
+
+import BlogLayout from '../../BlogLayout.component';
+import CategoryList from '../CategoryList';
+import CategoryListMobile from '../CategoryListMobile';
+import SearchOnMobile from '../SearchOnMobile';
+import OrderBySwitch from '../OrderBySwitch';
+import BlogList from '../../BlogList';
+import HotTagList from '../../HotTagList';
+import WriteBlogButton from '../../WriteBlogButton';
+
+const orderBy = [
+  { name: '推荐排序', url: '/blog' },
+  { name: '时间排序', url: '/blog/latest' },
+];
+
+export default function BlogHomepage({ categories, blogs, hotTags }) {
+  return (
+    <PageDataContext.Provider value={{}}>
+      <CommunityHead
+        title="博客 - 首页"
+        // description
+        // keyword
+      />
+      <BlogLayout>
+        <styled.Content>
+          <styled.Container>
+            <styled.Start>
+              <CategoryList categories={categories} />
+            </styled.Start>
+            <styled.Center>
+              <CategoryListMobile categories={categories} />
+              <SearchOnMobile />
+              <OrderBySwitch items={orderBy} />
+              <BlogList blogs={blogs} />
+            </styled.Center>
+            <styled.End>
+              <styled.WriteBlog>
+                <WriteBlogButton />
+              </styled.WriteBlog>
+              <HotTagList hotTags={hotTags} />
+            </styled.End>
+          </styled.Container>
+        </styled.Content>
+      </BlogLayout>
+    </PageDataContext.Provider>
+  );
+}

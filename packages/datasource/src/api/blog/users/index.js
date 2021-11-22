4da3b86da4500a7139bc9dcabea74757b58e84fb
Chang Wei <m11002815@gapps.ntust.edu.tw>
2021-11-21 19:50:01 -0800

    CP-249: update users api parameters

--- packages/datasource/src/api/blog/users/index.js
+++ packages/datasource/src/api/blog/users/index.js
@@ -1,6 +1,6 @@
 import blogClient from '../../blogClient';
 
-export const getComments = (id) => blogClient.get(`/api/${id}/comments`);
-export const getPosts = (id) => blogClient.get(`/api/${id}/posts`);
-export const getLikes = (id) => blogClient.get(`/api/${id}/likes`);
-export const getFavorites = (id) => blogClient.get(`/api/${id}/favorites`);
+export const getComments = (userId) => blogClient.get(`/api/${userId}/comments`);
+export const getPosts = (userId) => blogClient.get(`/api/${userId}/posts`);
+export const getLikes = (userId) => blogClient.get(`/api/${userId}/likes`);
+export const getFavorites = (userId) => blogClient.get(`/api/${userId}/favorites`);

d58f7dd5b03d4f43cbaf1d5734920aae8a56170e
Jagger <634750802@qq.com>
2021-11-24 03:33:17 +0800

    fix: users api

--- packages/datasource/src/api/blog/users/index.js
+++ packages/datasource/src/api/blog/users/index.js
@@ -1,6 +1,6 @@
 import blogClient from '../../blogClient';
 
-export const getComments = (userId, params) => blogClient.get(`/api/${userId}/comments`, { params });
-export const getPosts = (userId, params) => blogClient.get(`/api/${userId}/posts`, { params });
-export const getLikes = (userId, params) => blogClient.get(`/api/${userId}/likes`, { params });
-export const getFavorites = (userId, params) => blogClient.get(`/api/${userId}/favorites`, { params });
+export const getComments = (userId, params) => blogClient.get(`/api/users/${userId}/comments`, { params });
+export const getPosts = (userId, params) => blogClient.get(`/api/users/${userId}/posts`, { params });
+export const getLikes = (userId, params) => blogClient.get(`/api/users/${userId}/likes`, { params });
+export const getFavorites = (userId, params) => blogClient.get(`/api/users/${userId}/favorites`, { params });

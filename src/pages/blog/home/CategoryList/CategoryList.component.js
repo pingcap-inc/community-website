607c7c9d808cafcfc839f93e8e786f05af186ef3
Jagger <634750802@qq.com>
2021-11-24 02:09:17 +0800

    fix: user posts link

--- src/pages/blog/home/CategoryList/CategoryList.component.js
+++ src/pages/blog/home/CategoryList/CategoryList.component.js
@@ -3,8 +3,11 @@ import * as Styled from './index.styled';
 import Link from 'next/link';
 import { Divider } from 'antd';
 import { useRouter } from 'next/router';
+import { usePrincipal } from '../../blog.hooks';
 
 const CategoryList = ({ categories: { content } }) => {
+  const { isLogin, id } = usePrincipal();
+
   return (
     <Styled.Container>
       <Styled.List>
@@ -13,7 +16,7 @@ const CategoryList = ({ categories: { content } }) => {
         ))}
       </Styled.List>
       <Divider />
-      <FixedLink url={'/blog'}>我的博客</FixedLink>
+      {isLogin ? <FixedLink url={`/blog/user/${id}/posts`}>我的博客</FixedLink> : undefined}
       <FixedLink url={'/blog'}>博客发布指南</FixedLink>
       <FixedLink url={'/blog'}>隐私协议</FixedLink>
       <FixedLink url={'/blog'}>联系我们</FixedLink>

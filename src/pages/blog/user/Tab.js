3884ca3b059ac1f28ec6a683c75648028a10b113
Jagger <634750802@qq.com>
2021-11-24 11:18:49 +0800

    feat: user details api

--- src/pages/blog/user/Tab.js
+++ src/pages/blog/user/Tab.js
@@ -2,18 +2,14 @@ import React from 'react';
 import * as Styled from './Tab.styled';
 import Link from 'next/link';
 
-const Tab = ({ id, selectedKey }) => {
-  const numPosts = 7;
-  const numComments = 13;
-  const numLike = 65;
-  const numFav = 17;
+const Tab = ({ id, selectedKey, posts = 0, likes = 0, comments = 0, favorites = 0 }) => {
   return (
     <Styled.Tab>
       <Styled.Tab>
-        <TabItem id={id} selectedKey={selectedKey} name={`文章 ${numPosts}`} urlKey="posts" />
-        <TabItem id={id} selectedKey={selectedKey} name={`评论 ${numComments}`} urlKey="comments" />
-        <TabItem id={id} selectedKey={selectedKey} name={`赞 ${numLike}`} urlKey="like" />
-        <TabItem id={id} selectedKey={selectedKey} name={`收藏 ${numFav}`} urlKey="favorites" />
+        <TabItem id={id} selectedKey={selectedKey} name={`文章 ${posts}`} urlKey="posts" />
+        <TabItem id={id} selectedKey={selectedKey} name={`评论 ${comments}`} urlKey="comments" />
+        <TabItem id={id} selectedKey={selectedKey} name={`赞 ${likes}`} urlKey="like" />
+        <TabItem id={id} selectedKey={selectedKey} name={`收藏 ${favorites}`} urlKey="favorites" />
       </Styled.Tab>
     </Styled.Tab>
   );

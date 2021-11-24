c5d705009c379af641b3c6662f40d2687e0d303a
Jagger <634750802@qq.com>
2021-11-24 15:10:50 +0800

    fix: user detail page

--- src/pages/blog/user/[id]/posts.page.js
+++ src/pages/blog/user/[id]/posts.page.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import * as Styled from './index.styled';
-import { Breadcrumb } from 'antd';
-import Link from 'next/link';
 
 import { getI18nProps } from '~/utils/i18n.utils';
-import { CommunityHead } from '~/components';
-import { PageDataContext } from '~/context';
-
-import BlogLayout from '../../BlogLayout.component';
-import Tab from '../Tab';
 import { api } from '@tidb-community/datasource';
 import BlogList from '../../BlogList';
+import UserDetailsLayout from './Layout.component';
 
 export const getServerSideProps = async (ctx) => {
   const i18nProps = await getI18nProps(['common'])(ctx);
@@ -29,32 +22,11 @@ export const getServerSideProps = async (ctx) => {
   };
 };
 
-const Posts = ({ id, blogs, user: { posts, likes, comments, favorites } }) => {
+const Posts = ({ id, blogs, user }) => {
   return (
-    <PageDataContext.Provider value={{}}>
-      <CommunityHead
-        title="博客"
-        // description
-        // keyword
-      />
-
-      <BlogLayout>
-        <Styled.Content>
-          <Styled.Container>
-            <Styled.Breadcrumb>
-              <Breadcrumb.Item>
-                <Link href="/blog">博客</Link>
-              </Breadcrumb.Item>
-              <Breadcrumb.Item>用户</Breadcrumb.Item>
-            </Styled.Breadcrumb>
-
-            <Tab id={id} selectedKey="posts" posts={posts} likes={likes} favorites={favorites} comments={comments} />
-
-            <BlogList blogs={blogs} />
-          </Styled.Container>
-        </Styled.Content>
-      </BlogLayout>
-    </PageDataContext.Provider>
+    <UserDetailsLayout userDetails={user} item="博客" itemKey="posts">
+      <BlogList blogs={blogs} />
+    </UserDetailsLayout>
   );
 };
 

52dd9d1656b173d11e98e795959945932d3efc9f
Chang Wei <changwei@pingcap.com>
2021-11-25 16:03:58 +0800

    CP-249: add seo info (#322)
    * CP-249: add i18n data and seo info on post detail page, remove mistake i18n context on tag page
    
    * CP-249: remove mistake i18n context on category page, latest page, blog home page
    
    * CP-249: add seo info on edit page

--- src/pages/blog/index.page.js
+++ src/pages/blog/index.page.js
@@ -7,7 +7,7 @@ import { getI18nProps } from '~/utils/i18n.utils';
 import BlogHomepage from './home/BlogHomepage';
 
 export const getServerSideProps = async (ctx) => {
-  const i18nProps = await getI18nProps(['common', 'page-events'])(ctx);
+  const i18nProps = await getI18nProps(['common'])(ctx);
 
   const categories = await api.blog.getCategories();
   const blogs = await api.blog.getRecommend();

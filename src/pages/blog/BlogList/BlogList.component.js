bab9b99e1b8b58d7a793e4f47fe032aced9a2d30
Chang Wei <m11002815@gapps.ntust.edu.tw>
2021-12-02 03:22:41 -0800

    CP-249: use antd List to refactor BlogList

--- src/pages/blog/BlogList/BlogList.component.js
+++ src/pages/blog/BlogList/BlogList.component.js
@@ -2,7 +2,7 @@ import React from 'react';
 import * as Styled from './index.styled';
 import { BlogInfo } from '@tidb-community/ui';
 import { useRouter } from 'next/router';
-import { Pagination, Skeleton } from 'antd';
+import { List, Pagination, Skeleton } from 'antd';
 import { useRouterPage } from '~/utils/pagination.utils';
 
 const BlogList = ({
@@ -23,25 +23,29 @@ const BlogList = ({
   return (
     <Styled.Container>
       <Styled.List>
-        {content.map((value) => {
-          const onClick = () => router.push(`/blog/${value.id}`);
-          const onClickAuthor = () => router.push(`/blog/user/${value.author.id}`);
-          const onClickCategory = () => router.push(`/blog/category/${value.category.slug}`);
-          const onClickTag = (tag) => router.push(`/blog/tag/${tag.slug}`);
-          return (
-            <Styled.Item key={value.id}>
-              <BlogInfo
-                {...value}
-                usernameExtends={usernameExtends}
-                bottomExtends={bottomExtends}
-                onClick={onClick}
-                onClickAuthor={onClickAuthor}
-                onClickCategory={onClickCategory}
-                onClickTag={onClickTag}
-              />
-            </Styled.Item>
-          );
-        })}
+        <List
+          pagination={{ current: number, total: totalElements, onChange: onPageChange }}
+          dataSource={content}
+          renderItem={(value) => {
+            const onClick = () => router.push(`/blog/${value.id}`);
+            const onClickAuthor = () => router.push(`/blog/user/${value.author.id}`);
+            const onClickCategory = () => router.push(`/blog/category/${value.category.slug}`);
+            const onClickTag = (tag) => router.push(`/blog/tag/${tag.slug}`);
+            return (
+              <Styled.Item key={value.id}>
+                <BlogInfo
+                  {...value}
+                  usernameExtends={usernameExtends}
+                  bottomExtends={bottomExtends}
+                  onClick={onClick}
+                  onClickAuthor={onClickAuthor}
+                  onClickCategory={onClickCategory}
+                  onClickTag={onClickTag}
+                />
+              </Styled.Item>
+            );
+          }}
+        />
       </Styled.List>
       <Styled.Pagination>
         <Pagination current={number} total={totalElements} onChange={onPageChange} />

14c8169e3fcd79eb25c2ced76749a7893a776214
Chang Wei <m11002815@gapps.ntust.edu.tw>
2021-12-02 02:54:33 -0800

    CP-249: fix color of order switch

--- src/pages/blog/home/OrderBySwitch/index.styled.js
+++ src/pages/blog/home/OrderBySwitch/index.styled.js
@@ -16,8 +16,8 @@ export const List = styled.div`
 export const Item = styled.div`
   cursor: pointer;
   margin: 0.5rem;
-  color: ${(props) => (props.selected ? colors.T7 : '')};
-  border-bottom: ${(props) => (props.selected ? `4px solid ${colors.T7}` : 'none')};
+  color: ${(props) => (props.selected ? colors.B1 : '')};
+  border-bottom: ${(props) => (props.selected ? `4px solid ${colors.B1}` : 'none')};
 `;
 
 export const AllTag = styled.div`

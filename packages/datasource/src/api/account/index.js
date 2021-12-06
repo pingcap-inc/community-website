e9526e95107194626e3f0514b5b638a9b21f6d2c
Jagger <634750802@qq.com>
2021-12-06 13:25:29 +0800

    fix: remove params

--- packages/datasource/src/api/account/index.js
+++ packages/datasource/src/api/account/index.js
@@ -2,34 +2,34 @@ import client from '../client';
 
 export const settings = () => client.get('/api/account/settings');
 
-export const sendPhoneCode = ({ phone, re_token_v3 }) =>
+export const sendPhoneCode = ({ phone }) =>
   client.post('/api/account/set-phone/send-code', {
     phone,
   });
 
-export const setPhone = ({ phone, code, re_token_v3 }) =>
+export const setPhone = ({ phone, code }) =>
   client.post('/api/account/set-phone', {
     phone,
     code,
   });
 
-export const sendEmailCode = ({ email, re_token_v3 }) =>
+export const sendEmailCode = ({ email }) =>
   client.post('/api/account/set-email/send-code', {
     email,
   });
 
-export const setEmail = ({ email, code, re_token_v3 }) =>
+export const setEmail = ({ email, code }) =>
   client.post('/api/account/set-email', {
     email,
     code,
   });
 
-export const setPassword = ({ new_password, re_token_v3 }) =>
+export const setPassword = ({ new_password }) =>
   client.post('/api/account/set-password', {
     new_password,
   });
 
-export const resetPassword = ({ old_password, new_password, re_token_v3 }) =>
+export const resetPassword = ({ old_password, new_password }) =>
   client.post('/api/account/reset-password', {
     old_password,
     new_password,

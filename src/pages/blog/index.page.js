9185a97bcdb4cddb92904d5e2c2a2a8e63caac63
Chang Wei <m11002815@gapps.ntust.edu.tw>
2021-12-06 01:14:35 -0800

    CP-249: fix pagination on blog home page

--- src/pages/blog/index.page.js
+++ src/pages/blog/index.page.js
@@ -5,13 +5,14 @@ import { api } from '@tidb-community/datasource';
 import { getI18nProps } from '~/utils/i18n.utils';
 
 import BlogHomepage from './home/BlogHomepage';
+import { getPageQuery } from '../../utils/pagination.utils';
 
 export const getServerSideProps = async (ctx) => {
   const i18nProps = await getI18nProps(['common'])(ctx);
 
-  // const { page, size } = getPageQuery(ctx.query);
+  const { page, size } = getPageQuery(ctx.query);
   const { getCategories, getRecommend, getHotTags } = api.blog;
-  const [categories, blogs, hotTags] = await Promise.all([getCategories(), getRecommend(), getHotTags()]);
+  const [categories, blogs, hotTags] = await Promise.all([getCategories(), getRecommend({ page, size }), getHotTags()]);
   return {
     props: {
       ...i18nProps,

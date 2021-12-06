d92266a02c0ec4f102ae35bf9e7dce07a17a38b7
Chang Wei <m11002815@gapps.ntust.edu.tw>
2021-12-06 01:18:50 -0800

    CP-249: fix pagination on blog latest page

--- src/pages/blog/latest/index.page.js
+++ src/pages/blog/latest/index.page.js
@@ -4,13 +4,14 @@ import { api } from '@tidb-community/datasource';
 
 import { getI18nProps } from '~/utils/i18n.utils';
 import BlogHomepage from '../home/BlogHomepage';
+import { getPageQuery } from '../../../utils/pagination.utils';
 
 export const getServerSideProps = async (ctx) => {
   const i18nProps = await getI18nProps(['common'])(ctx);
 
-  // const { page, size } = getPageQuery(ctx.query);
+  const { page, size } = getPageQuery(ctx.query);
   const { getCategories, getLatest, getHotTags } = api.blog;
-  const [categories, blogs, hotTags] = await Promise.all([getCategories(), getLatest(), getHotTags()]);
+  const [categories, blogs, hotTags] = await Promise.all([getCategories(), getLatest({ page, size }), getHotTags()]);
 
   return {
     props: {

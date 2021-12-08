38f53ad97ba732b0cc0ac739af1e5984c44e121e
Chang Wei <m11002815@gapps.ntust.edu.tw>
2021-12-07 23:20:14 -0800

    CP-249: recover link color to B1

--- packages/ui/src/antd/link.js
+++ packages/ui/src/antd/link.js
@@ -2,14 +2,9 @@ import { css } from 'styled-components';
 
 import * as colors from '../colors';
 
-const linkColor = '#73061c';
-
 export default css`
-  a {
-    color: ${linkColor};
-    &:hover {
-      color: ${colors.B1};
-      text-decoration: underline;
-    }
+  a,
+  a:hover {
+    color: ${colors.B1};
   }
 `;

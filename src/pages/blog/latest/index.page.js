e194e8704cbb88472f4e5d58dc1de8e61377f026
Chang Wei <m11002815@gapps.ntust.edu.tw>
2021-12-08 03:54:09 -0800

    CP-249: fix scroll on all latest blogs page

--- src/pages/blog/latest/index.page.js
+++ src/pages/blog/latest/index.page.js
@@ -6,11 +6,12 @@ import { getI18nProps } from '~/utils/i18n.utils';
 import BlogHomepage from '../home/BlogHomepage';
 import { getPageQuery } from '../../../utils/pagination.utils';
 
+const { getCategories, getLatest, getHotTags } = api.blog;
+
 export const getServerSideProps = async (ctx) => {
   const i18nProps = await getI18nProps(['common'])(ctx);
 
   const { page, size } = getPageQuery(ctx.query);
-  const { getCategories, getLatest, getHotTags } = api.blog;
   const [categories, blogs, hotTags] = await Promise.all([getCategories(), getLatest({ page, size }), getHotTags()]);
 
   return {
@@ -24,5 +25,5 @@ export const getServerSideProps = async (ctx) => {
 };
 
 export default function BlogHomePage(props) {
-  return <BlogHomepage {...props} />;
+  return <BlogHomepage {...props} blogApi={getLatest} />;
 }

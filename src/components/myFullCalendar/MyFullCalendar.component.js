1d6816201e2146168851dc27b33819bcaab277ba
Chang Wei <m11002815@gapps.ntust.edu.tw>
2021-12-09 07:24:19 -0800

    CP-249: update events calendar text color to #fff when hover

--- src/components/myFullCalendar/MyFullCalendar.component.js
+++ src/components/myFullCalendar/MyFullCalendar.component.js
@@ -23,12 +23,12 @@ const MyFullCalendar = ({ data }) => {
   const eventMouseEnter = ({ event }) => {
     const { extendedProps } = event;
     event.setProp('backgroundColor', getColorByType(extendedProps.category));
-    event.setProp('color', '#ffffff');
+    event.setProp('textColor', '#ffffff');
   };
   const eventMouseLeave = ({ event }) => {
     const { extendedProps } = event;
     event.setProp('backgroundColor', getColorByType(extendedProps.category) + transparency);
-    event.setProp('color', '#2c2c2c');
+    event.setProp('textColor', '#2c2c2c');
   };
 
   const renderDayCellContent = ({ date }) => date.getDate();

7e68302739068b3e6fdd73b2ad6bfd86fde486a3
Chang Wei <m11002815@gapps.ntust.edu.tw>
2021-12-10 00:32:42 -0800

    CP-249: set status alert background to red

--- src/pages/blog/[id]/components/StatusAlert/StatusAlert.component.js
+++ src/pages/blog/[id]/components/StatusAlert/StatusAlert.component.js
@@ -3,15 +3,15 @@ import { Alert } from 'antd';
 
 const StatusAlert = ({ blogInfo }) => {
   if (blogInfo.status === 'DRAFT') {
-    return <Alert message="该文章目前为草稿状态，请提交审核以发布。" />;
+    return <Alert type="error" message="该文章目前为草稿状态，请提交审核以发布。" />;
   }
 
   if (blogInfo.status === 'PENDING') {
-    return <Alert message="该文章正在审核中，其他用户暂时不可见。" />;
+    return <Alert type="error" message="该文章正在审核中，其他用户暂时不可见。" />;
   }
 
   if (blogInfo.status === 'REJECTED') {
-    return <Alert type="warning" message="该文章被审核人员拒绝发布，请修改后重新提交审核。" />;
+    return <Alert type="error" message="该文章被审核人员拒绝发布，请修改后重新提交审核。" />;
   }
 
   if (blogInfo.status === 'ARCHIVED') {

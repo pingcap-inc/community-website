6553dfb8886f2424d5618b488f66310780469c13
Chang Wei <m11002815@gapps.ntust.edu.tw>
2021-12-17 03:33:53 -0800

    CP-249: fix hot tag items, add passHref prop to the next/link element

--- src/pages/blog/HotTagList/index.styled.js
+++ src/pages/blog/HotTagList/index.styled.js
@@ -7,7 +7,7 @@ export const Container = styled.div`
   ${mixins.showOnDesktop()}
   ${borderRadius}
   margin-top: 1rem;
-  a {
+  .ant-card-head a {
     color: ${colors.B1};
   }
 `;
@@ -21,8 +21,9 @@ export const List = styled.div`
   flex-wrap: wrap;
 `;
 
-export const Item = styled.div`
+export const Item = styled.a`
   ${borderRadius};
+  display: block;
   cursor: pointer;
   margin: 0.5rem;
   color: ${(props) => (props.selected ? colors.B1 : '')};

ebcc640379220265813ba24426f157ab9ac1027e
Chang Wei <m11002815@gapps.ntust.edu.tw>
2021-12-20 00:59:07 -0800

    CP-249: migrate post route url from [id] to [slug] on BlogInfo component

--- src/pages/blog/components/blogInfo/BlogInfo.component.js
+++ src/pages/blog/components/blogInfo/BlogInfo.component.js
@@ -9,6 +9,7 @@ import { HeartOutlined, MessageOutlined, PushpinOutlined } from '@ant-design/ico
 
 const BlogInfo = ({
   id,
+  slug,
   usernameExtends = undefined,
   publishedAt,
   title,
@@ -56,7 +57,7 @@ const BlogInfo = ({
         </Styled.Author>
         <Styled.Title>
           {!recommended && <PushpinOutlined style={{ marginRight: 8, fontSize: 20 }} />}
-          <Link href={`/blog/${id}`}>{title === '' ? ' [未填写文章标题] ' : title}</Link>
+          <Link href={`/blog/${slug}`}>{title === '' ? ' [未填写文章标题] ' : title}</Link>
           {titleExtends}
         </Styled.Title>
         <Styled.Meta>

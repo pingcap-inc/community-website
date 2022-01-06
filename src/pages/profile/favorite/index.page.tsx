1bf79a8da225ed0e3d905d7ee33e114103938fbd
Chang Wei <m11002815@gapps.ntust.edu.tw>
2022-01-06 08:46:54 -0800

    CP-307 feat: update favorite page's ListItem

--- src/pages/profile/favorite/index.page.tsx
+++ src/pages/profile/favorite/index.page.tsx
@@ -5,9 +5,8 @@ import { getI18nProps } from '~/utils/i18n.utils';
 import Tab, { EUgcType } from '../_components/Tab';
 import ProfileLayout from '../_components/ProfileLayout';
 import { GetServerSideProps } from 'next';
-import { Pagination, Select } from 'antd';
+import { Pagination } from 'antd';
 import ListItem from '../_components/ListItem';
-import dayjs from 'dayjs';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   // @ts-ignore
@@ -27,58 +26,22 @@ export default function ProfileAnswerPage() {
   const onChange = () => {
     //  TODO: handle page change
   };
-  const date = dayjs(new Date()).format();
   return (
     <ProfileLayout>
       <CommonStyled.Action>
         <Tab selected={EUgcType.favorite} nums={{ answer: 3, question: 4, post: 5, favorite: 6 }} />
-        <Select defaultValue={''}>
-          <Select.Option value={''}>回答状态</Select.Option>
-          <Select.Option value={'1'}>回答状态</Select.Option>
-          <Select.Option value={'2'}>回答状态</Select.Option>
-        </Select>
       </CommonStyled.Action>
       <CommonStyled.List>
-        <ListItem
-          url={'#'}
-          title={'ansbile升级集群V3到4.0.14问题'}
-          summary={
-            '这个场景就比较痛苦了，官方后续只会支持tiup 的迭代。。Evict 的策略 是通过 PD 来设定的，目前你出现的问题，基本上都是环境问题了，可能无法解决 :rofl: 这个场景就比较痛苦了，官方后续只会支持tiup 的迭代。。Evict 的策略 是通过 PD 来设定的，目前你出现的问题，基本上都是环境问题了，可能无法解决 :rofl:'
-          }
-          metadataEnd={date}
-        />
-        <ListItem
-          url={'#'}
-          title={'ansbile升级集群V3到4.0.14问题'}
-          summary={
-            '这个场景就比较痛苦了，官方后续只会支持tiup 的迭代。。Evict 的策略 是通过 PD 来设定的，目前你出现的问题，基本上都是环境问题了，可能无法解决 :rofl: 这个场景就比较痛苦了，官方后续只会支持tiup 的迭代。。Evict 的策略 是通过 PD 来设定的，目前你出现的问题，基本上都是环境问题了，可能无法解决 :rofl:'
-          }
-          metadataEnd={date}
-        />
-        <ListItem
-          url={'#'}
-          title={'ansbile升级集群V3到4.0.14问题'}
-          summary={
-            '这个场景就比较痛苦了，官方后续只会支持tiup 的迭代。。Evict 的策略 是通过 PD 来设定的，目前你出现的问题，基本上都是环境问题了，可能无法解决 :rofl: 这个场景就比较痛苦了，官方后续只会支持tiup 的迭代。。Evict 的策略 是通过 PD 来设定的，目前你出现的问题，基本上都是环境问题了，可能无法解决 :rofl:'
-          }
-          metadataEnd={date}
-        />
-        <ListItem
-          url={'#'}
-          title={'ansbile升级集群V3到4.0.14问题'}
-          summary={
-            '这个场景就比较痛苦了，官方后续只会支持tiup 的迭代。。Evict 的策略 是通过 PD 来设定的，目前你出现的问题，基本上都是环境问题了，可能无法解决 :rofl: 这个场景就比较痛苦了，官方后续只会支持tiup 的迭代。。Evict 的策略 是通过 PD 来设定的，目前你出现的问题，基本上都是环境问题了，可能无法解决 :rofl:'
-          }
-          metadataEnd={date}
-        />
-        <ListItem
-          url={'#'}
-          title={'ansbile升级集群V3到4.0.14问题'}
-          summary={
-            '这个场景就比较痛苦了，官方后续只会支持tiup 的迭代。。Evict 的策略 是通过 PD 来设定的，目前你出现的问题，基本上都是环境问题了，可能无法解决 :rofl: 这个场景就比较痛苦了，官方后续只会支持tiup 的迭代。。Evict 的策略 是通过 PD 来设定的，目前你出现的问题，基本上都是环境问题了，可能无法解决 :rofl:'
-          }
-          metadataEnd={date}
-        />
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((value) => (
+          <ListItem
+            key={value}
+            url={'#'}
+            title={'ansbile升级集群V3到4.0.14问题'}
+            summary={
+              '这个场景就比较痛苦了，官方后续只会支持tiup 的迭代。。Evict 的策略 是通过 PD 来设定的，目前你出现的问题，基本上都是环境问题了，可能无法解决 :rofl: 这个场景就比较痛苦了，官方后续只会支持tiup 的迭代。。Evict 的策略 是通过 PD 来设定的，目前你出现的问题，基本上都是环境问题了，可能无法解决 :rofl:'
+            }
+          />
+        ))}
       </CommonStyled.List>
       <CommonStyled.Pagination>
         <Pagination showQuickJumper defaultCurrent={2} total={500} onChange={onChange} />

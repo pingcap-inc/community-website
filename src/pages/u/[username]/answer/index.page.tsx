7d40e3699c98d7ddeebe39ee247eb54339c1bd9c
Chang Wei <m11002815@gapps.ntust.edu.tw>
2022-01-07 07:22:42 -0800

    CP-307 feat: fetch data by username

--- src/pages/u/[username]/answer/index.page.tsx
+++ src/pages/u/[username]/answer/index.page.tsx
@@ -9,17 +9,22 @@ import { Pagination, Select } from 'antd';
 import ListItem from '../_components/ListItem';
 import { getBadgesById, getUserProfileById, IRawBadges } from '../api';
 import { getRelativeDatetime } from '~/utils/datetime.utils';
+import { ParsedUrlQuery } from 'querystring';
 
 interface IProps {
   badges: IRawBadges[];
 }
+interface IQuery extends ParsedUrlQuery {
+  username: string;
+}
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
+export const getServerSideProps: GetServerSideProps<IProps, IQuery> = async (ctx) => {
+  const { username } = ctx.params;
   const [i18nProps, badges, profile] = await Promise.all([
     // @ts-ignore
     getI18nProps(['common'])(ctx),
-    getBadgesById('cw1997'),
-    getUserProfileById('cw1997'),
+    getBadgesById(username),
+    getUserProfileById(username),
   ]);
   return { props: { ...i18nProps, badges, profile } };
 };

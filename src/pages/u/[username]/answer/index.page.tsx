5d5c0a4499c756e1ce864b0f47f011774b1500a1
Chang Wei <m11002815@gapps.ntust.edu.tw>
2022-01-18 00:01:17 -0800

    CP-307: fix loading status

--- src/pages/u/[username]/answer/index.page.tsx
+++ src/pages/u/[username]/answer/index.page.tsx
@@ -6,7 +6,7 @@ import { getPageQuery } from '~/utils/pagination.utils';
 import Tab, { EUgcType } from '../_components/Tab';
 import ProfileLayout from '../_components/ProfileLayout';
 import { GetServerSideProps } from 'next';
-import { List, Select, Skeleton } from 'antd';
+import { List, Select, Spin } from 'antd';
 import ListItem from '../_components/ListItem';
 import {
   getAnswersByUsername,
@@ -85,11 +85,7 @@ export default function ProfileAnswerPage(props: IProps) {
           dataLength={data.length}
           next={loadMoreData}
           hasMore={hasMore}
-          loader={
-            <div style={{ marginTop: '16px' }}>
-              <Skeleton avatar paragraph={{ rows: 1 }} active />
-            </div>
-          }
+          loader={<Spin />}
           // endMessage={<Divider plain>没有更多数据了</Divider>}
         >
           <List

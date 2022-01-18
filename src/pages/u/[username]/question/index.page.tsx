f927e4abd751ec5c062f9b27096fba9c8e665a31
Chang Wei <m11002815@gapps.ntust.edu.tw>
2022-01-18 00:41:01 -0800

    CP-307: connect question api

--- src/pages/u/[username]/question/index.page.tsx
+++ src/pages/u/[username]/question/index.page.tsx
@@ -5,7 +5,7 @@ import { getI18nProps } from '~/utils/i18n.utils';
 import Tab, { EUgcType } from '../_components/Tab';
 import ProfileLayout from '../_components/ProfileLayout';
 import { GetServerSideProps } from 'next';
-import { Pagination, Select, Space } from 'antd';
+import { List, Select, Skeleton, Space } from 'antd';
 import ListItem from '../_components/ListItem';
 import { EyeOutlined, MessageOutlined } from '@ant-design/icons';
 import {
@@ -19,11 +19,16 @@ import {
 } from '../api';
 import { getRelativeDatetime } from '~/utils/datetime.utils';
 import { ParsedUrlQuery } from 'querystring';
+import { getPageQuery } from '~/utils/pagination.utils';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
 
 interface IProps {
   badges: IRawBadges[];
   profile: IProfile;
   questions: IQuestions[];
+  username: string;
 }
 interface IQuery extends ParsedUrlQuery {
   username: string;
@@ -32,23 +37,28 @@ interface IQuery extends ParsedUrlQuery {
 }
 
 export const getServerSideProps: GetServerSideProps<IProps, IQuery> = async (ctx) => {
-  const { username, page, size } = ctx.params;
-  const actualPage: number = page !== undefined ? Number(page) ?? 0 : 0;
-  const actualSize: number = size !== undefined ? Number(size) ?? 30 : 30;
+  const { username } = ctx.params;
+  const pageInfo = getPageQuery(ctx.query);
   const [i18nProps, badges, profile, questions] = await Promise.all([
     // @ts-ignore
     getI18nProps(['common'])(ctx),
     getBadgesByUsername(username),
     getUserProfileByUsername(username),
-    getQuestionsByUsername(username, actualPage, actualSize),
+    getQuestionsByUsername(username, pageInfo.page, pageInfo.size),
   ]);
-  return { props: { ...i18nProps, badges, profile, questions } };
+  return { props: { ...i18nProps, badges, profile, questions, username } };
 };
 
 export default function ProfileAnswerPage(props: IProps) {
-  const { badges, profile, questions } = props;
-  const onChange = () => {
-    //  TODO: handle page change
+  const { badges, profile, questions, username } = props;
+  const router = useRouter();
+  const pageInfo = getPageQuery(router.query);
+  const [page, setPage] = useState(pageInfo.page);
+  const [data, setData] = useState(questions);
+  const loadMoreData = async () => {
+    const newData = await getQuestionsByUsername(username, page, pageInfo.size);
+    setData((data) => [...data, ...newData]);
+    setPage((page) => page + 1);
   };
   return (
     <ProfileLayout badges={badges} profile={profile}>
@@ -61,29 +71,60 @@ export default function ProfileAnswerPage(props: IProps) {
         </Select>
       </CommonStyled.Action>
       <CommonStyled.List>
-        {questions.map((value) => (
-          <ListItem
-            key={value.id}
-            url={getPostUrl(value.id, 1)}
-            title={value.title}
-            summary={''}
-            metadataStart={
-              <Space size={24}>
-                <div>
-                  <MessageOutlined /> {value.reply_count}
-                </div>
-                <div>
-                  <EyeOutlined /> {value.views}
-                </div>
-              </Space>
-            }
-            metadataEnd={getRelativeDatetime(value.created_at)}
+        {/*{questions.map((value) => (*/}
+        {/*  <ListItem*/}
+        {/*    key={value.id}*/}
+        {/*    url={getPostUrl(value.id, 1)}*/}
+        {/*    title={value.title}*/}
+        {/*    summary={''}*/}
+        {/*    metadataStart={*/}
+        {/*      <Space size={24}>*/}
+        {/*        <div>*/}
+        {/*          <MessageOutlined /> {value.reply_count}*/}
+        {/*        </div>*/}
+        {/*        <div>*/}
+        {/*          <EyeOutlined /> {value.views}*/}
+        {/*        </div>*/}
+        {/*      </Space>*/}
+        {/*    }*/}
+        {/*    metadataEnd={getRelativeDatetime(value.created_at)}*/}
+        {/*  />*/}
+        {/*))}*/}
+        <InfiniteScroll
+          dataLength={data.length}
+          next={loadMoreData}
+          hasMore={data.length !== 0}
+          loader={
+            <div style={{ marginTop: '16px' }}>
+              <Skeleton avatar paragraph={{ rows: 1 }} active />
+            </div>
+          }
+        >
+          <List
+            dataSource={data}
+            locale={{ emptyText: '暂无数据' }}
+            renderItem={(value) => (
+              <ListItem
+                key={value.id}
+                url={getPostUrl(value.id, 1)}
+                title={value.title}
+                summary={''}
+                metadataStart={
+                  <Space size={24}>
+                    <div>
+                      <MessageOutlined /> {value.reply_count}
+                    </div>
+                    <div>
+                      <EyeOutlined /> {value.views}
+                    </div>
+                  </Space>
+                }
+                metadataEnd={getRelativeDatetime(value.created_at)}
+              />
+            )}
           />
-        ))}
+        </InfiniteScroll>
       </CommonStyled.List>
-      <CommonStyled.Pagination>
-        <Pagination showQuickJumper defaultCurrent={2} total={500} onChange={onChange} />
-      </CommonStyled.Pagination>
     </ProfileLayout>
   );
 }

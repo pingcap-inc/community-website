584b56bcfa213e2d129f0c26a28a9e7f0c2941d8
Chang Wei <m11002815@gapps.ntust.edu.tw>
2022-01-19 06:05:12 -0800

    CP-307: export api.blog.users.username module

--- packages/datasource/src/api/blog/users/index.js
+++ packages/datasource/src/api/blog/users/index.js
@@ -1,5 +1,8 @@
 import blogClient from '../../blogClient';
 
+import * as username from './username';
+export { username };
+
 export const getComments = (userId, params) => blogClient.get(`/api/users/${userId}/comments`, { params });
 export const getPosts = (userId, params) => blogClient.get(`/api/users/${userId}/posts`, { params });
 export const getLikes = (userId, params) => blogClient.get(`/api/users/${userId}/likes`, { params });

0e5cdf853506d503ac7d035ef18c917910f46b4d
Chang Wei <changwei@pingcap.com>
2022-01-24 06:21:21 -0800

    CP-307: fix tab style, add scroll

--- src/pages/u/[username]/_components/Tab/index.styled.ts
+++ src/pages/u/[username]/_components/Tab/index.styled.ts
@@ -3,12 +3,15 @@ import { colors } from '@tidb-community/ui';
 
 export const Tab = styled.div`
   display: flex;
+  overflow: auto;
+  margin-right: 1rem;
 `;
 
 export const TabItem = styled.a`
+  border-bottom: 4px solid ${(props) => (props.selected ? colors.B1 : `rgba(0,0,0,0)`)};
+  color: ${(props) => (props.selected ? colors.B1 : `black`)};
   display: block;
   padding: 0.25rem 0;
   margin-right: 1.5rem;
-  border-bottom: 4px solid ${(props) => (props.selected ? colors.B1 : `rgba(0,0,0,0)`)};
-  color: ${(props) => (props.selected ? colors.B1 : `black`)};
+  white-space: nowrap;
 `;

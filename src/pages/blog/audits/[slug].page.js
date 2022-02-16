a8c23903514b653609bece9ef362b6c641691a15
cw1997 <867597730@qq.com>
2022-02-16 05:23:13 -0800

    CP-249 fix: fix permission of pending blog's detail page

--- src/pages/blog/audits/[slug].page.js
+++ src/pages/blog/audits/[slug].page.js
@@ -1,17 +1,15 @@
 import BlogPage from '~/pages/blog/[slug]/index.page';
 import { getI18nProps } from '~/utils/i18n.utils';
-import { getPageQuery } from '~/utils/pagination.utils';
 
 export const getServerSideProps = async (ctx) => {
   const i18nProps = await getI18nProps(['common'])(ctx);
-  const { page, size } = getPageQuery(ctx.query);
   return {
     props: {
       ...i18nProps,
-      page,
-      size,
     },
   };
 };
 
-export default BlogPage
+export default function AuditsDetailPage() {
+  return <BlogPage />;
+}

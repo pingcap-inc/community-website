b190ae8751ab682d4176e5edd4107a93b214d1cb
cw1997 <867597730@qq.com>
2022-02-18 00:32:03 -0800

    CP-249: fix blog user page

--- src/pages/blog/user/_component/PostItem/index.tsx
+++ src/pages/blog/user/_component/PostItem/index.tsx
@@ -2,17 +2,22 @@ import * as React from 'react';
 import * as Styled from './index.styled';
 //import Anchor from '~/components/Anchor';
 
-export default function PostItem({ blogInfo, actionText, statusBadge }) {
+export default function PostItem({
+  blogInfo,
+  actionText,
+  statusBadge,
+  getUrlBySlugCallback = (slug) => `/blog/${slug}`,
+}) {
   return (
     <Styled.Container>
       {actionText && actionText.length !== 0 && <Styled.Header>{actionText}</Styled.Header>}
       <Styled.Title>
-        <Styled.TitleAnchor href={`/blog/${blogInfo.slug}`}>{blogInfo.title}</Styled.TitleAnchor>
+        <Styled.TitleAnchor href={getUrlBySlugCallback(blogInfo.slug)}>{blogInfo.title}</Styled.TitleAnchor>
         {/*<Styled.StatusBadge>*/}
         {statusBadge}
         {/*</Styled.StatusBadge>*/}
       </Styled.Title>
-      <Styled.Author href={`/blog/${blogInfo.slug}`}>
+      <Styled.Author href={`/blog/user/${blogInfo.author.id}`}>
         <Styled.AuthorAvatar>
           <img src={blogInfo.author.avatarURL} alt={blogInfo.author.username} />
         </Styled.AuthorAvatar>

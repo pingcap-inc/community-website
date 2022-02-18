f4dea940d186a993bb3131ee497e6035aab6fb2f
cw1997 <867597730@qq.com>
2022-02-18 00:54:18 -0800

    CP-249: update blog homepage, use SWR instead of native datasource API

--- src/pages/blog/index.page.js
+++ src/pages/blog/index.page.js
@@ -6,14 +6,16 @@ import { getI18nProps } from '~/utils/i18n.utils';
 
 import BlogHomepage from './home/BlogHomepage';
 import { getPageQuery } from '~/utils/pagination.utils';
-import { CommunityHead } from '~/components';
+import { CommunityHead, ErrorPage } from '~/components';
 import { PageDataContext } from '~/context';
+import useSWR from 'swr';
+import { useRouter } from 'next/router';
+import { Skeleton } from 'antd';
 
 const { getCategories, getRecommend, getHotTags } = api.blog;
 
 export const getServerSideProps = async (ctx) => {
   const i18nProps = await getI18nProps(['common'])(ctx);
-
   const { page, size } = getPageQuery(ctx.query);
   const [categories, blogs, hotTags] = await Promise.all([getCategories(), getRecommend({ page, size }), getHotTags()]);
   return {
@@ -26,7 +28,30 @@ export const getServerSideProps = async (ctx) => {
   };
 };
 
-export default function BlogHomePage(props) {
+export default function BlogHomePage({ categories: categoriesFromSSR, blogs: blogsFromSSR, hotTags: hotTagsFromSSR }) {
+  const router = useRouter();
+  const { page, size } = getPageQuery(router.query);
+
+  const { data: categories, error: categoriesError } = useSWR(['blog.getCategories', JSON.stringify({})], {
+    fallbackData: categoriesFromSSR,
+    revalidateOnMount: true,
+  });
+  const { data: blogs, error: blogsError } = useSWR(['blog.getRecommend', JSON.stringify({ page, size })], {
+    fallbackData: blogsFromSSR,
+    revalidateOnMount: true,
+  });
+  const { data: hotTags, error: hotTagsError } = useSWR(['blog.getHotTags', JSON.stringify({})], {
+    fallbackData: hotTagsFromSSR,
+    revalidateOnMount: true,
+  });
+
+  const props = { categories, blogs, hotTags };
+
+  const loading = categories === undefined || blogs === undefined || hotTags === undefined;
+  const error = categoriesError !== undefined || blogsError !== undefined || hotTagsError !== undefined;
+  if (loading) return <Skeleton active />;
+  if (error) return <ErrorPage />;
+
   return (
     <PageDataContext.Provider value={{ showRecommendedIcon: true }}>
       <CommunityHead

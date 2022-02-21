aacc351d764ef03744f5c44b2170666151332b5c
cw1997 <867597730@qq.com>
2022-02-21 05:43:58 -0800

    CP-249: reduce activity banner icon's height size and fix the relative path of tidb.io blog

--- packages/uiScripts/src/scripts/asktugHeaderFooter/index.js
+++ packages/uiScripts/src/scripts/asktugHeaderFooter/index.js
@@ -81,8 +81,8 @@ const ActivityBannerComponent = () => {
             version="1.1"
             xmlns="http://www.w3.org/2000/svg"
             p-id="8486"
-            width="24"
-            height="24"
+            width="20"
+            height="20"
           >
             <path
               d="M848 359.3H627.7L825.8 109c4.1-5.3 0.4-13-6.3-13H436c-2.8 0-5.5 1.5-6.9 4L170 547.5c-3.1 5.3 0.7 12 6.9 12h174.4l-89.4 357.6c-1.9 7.8 7.5 13.3 13.3 7.7L853.5 373c5.2-4.9 1.7-13.7-5.5-13.7zM378.2 732.5l60.3-241H281.1l189.6-327.4h224.6L487 427.4h211L378.2 732.5z"
@@ -94,7 +94,7 @@ const ActivityBannerComponent = () => {
       }
       // backgroundImage={'https://tidb.io/images/activity/banner.svg'}
       // buttonImage={'https://tidb.io/images/activity/button.svg'}
-      link={'https://tidb.io/blog'}
+      link={'/blog'}
       // onClick={() => onNavClick({ link: 'https://tidb.io/blog', target: '_blank' })}
     />
   );

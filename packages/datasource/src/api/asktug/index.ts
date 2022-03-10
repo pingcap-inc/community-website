c370411901f61e63b4cf0a8a1d9abdafad4e509d
Chang Wei <changwei1006@gmail.com>
2022-03-10 18:17:57 +0800

    CP-307 fix: fix type definition

--- packages/datasource/src/api/asktug/index.ts
+++ packages/datasource/src/api/asktug/index.ts
@@ -2,16 +2,16 @@ import { ApiRequestFunction } from '../index';
 
 import client from '../client';
 
-//interface Badges {
-//  data?: DataEntity[] | null;
-//}
+export interface Badges {
+  data?: BadgeEntity[] | null;
+}
 
-//interface BadgeEntity {
-//  id: number;
-//  name: string;
-//  image: string;
-//  icon: string;
-//  has_badge: boolean;
-//}
+export interface BadgeEntity {
+  id: number;
+  name: string;
+  image: string;
+  icon: string;
+  has_badge: boolean;
+}
 
 export const getBadgesList: ApiRequestFunction<void, any> = () => client.get('/api/asktug/badges');

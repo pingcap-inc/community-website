78d2a23d448078cadaf835854210152eb8102d55
Chang Wei <changwei1006@gmail.com>
2022-03-17 16:14:48 +0800

    add interceptors for debug in initStrapiClient module

--- packages/datasource/src/api/initStrapiClient.js
+++ packages/datasource/src/api/initStrapiClient.js
@@ -21,6 +21,20 @@ const initStrapiClient = async ({ baseUrl, email, password } = {}) => {
     return config;
   });
 
+  /*axios.interceptors.request.use(function (config) {
+    console.info('[strapi] request config: ', config);
+    return config;
+  }, error => {
+    return Promise.reject(error);
+  });
+  axios.interceptors.response.use(function (response) {
+    console.info('[strapi] response config: ', response);
+    return response;
+  }, error => {
+    console.error('[strapi] error config: ', error);
+    return Promise.reject(error);
+  });*/
+
   return client;
 };
 

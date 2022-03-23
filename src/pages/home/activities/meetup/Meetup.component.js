a7656ee7ac49a3aa468be48dbd60c735e1ee77b1
Chang Wei <changwei1006@gmail.com>
2022-03-24 00:44:46 +0800

    fix homepage: use Anchor component instead of router.push() on homepage.Activities.Meetup component

--- src/pages/home/activities/meetup/Meetup.component.js
+++ src/pages/home/activities/meetup/Meetup.component.js
@@ -4,37 +4,33 @@ import { Badge } from 'antd';
 import { colors } from '@tidb-community/ui';
 
 import * as Styled from './meetup.styled';
-import { useRouter } from 'next/router';
-import { link as linkUtils } from '~/utils';
+import Anchor from '~/components/Anchor';
 
-const Meetup = ({ title, link, status, type, date }) => {
-  const router = useRouter();
+const Meetup = ({ title, status, type, date, link }) => {
   const dayObj = dayjs(date);
   const month = dayObj.format('MMM');
   const day = dayObj.format('D');
-  const onClick = (link) => (e) => {
-    e.preventDefault();
-    linkUtils.handleRedirect(router, link);
-  };
 
   return (
-    <Styled.Container onClick={onClick(link)}>
-      <Styled.Calendar>
-        <Styled.CalendarHead>{month}</Styled.CalendarHead>
-        <Styled.CalendarBody>
-          {day}
-          <Styled.Arrow />
-          <Styled.OppsiteArrow />
-        </Styled.CalendarBody>
-      </Styled.Calendar>
-      <Styled.Content>
-        <h3>{title}</h3>
-        <div>
-          {status && <Badge color={colors.T3} text={status} />}
-          {type && <Badge color={colors.T1} text={type} />}
-        </div>
-      </Styled.Content>
-    </Styled.Container>
+    <Anchor href={link}>
+      <Styled.Container>
+        <Styled.Calendar>
+          <Styled.CalendarHead>{month}</Styled.CalendarHead>
+          <Styled.CalendarBody>
+            {day}
+            <Styled.Arrow />
+            <Styled.OppsiteArrow />
+          </Styled.CalendarBody>
+        </Styled.Calendar>
+        <Styled.Content>
+          <h3>{title}</h3>
+          <div>
+            {status && <Badge color={colors.T3} text={status} />}
+            {type && <Badge color={colors.T1} text={type} />}
+          </div>
+        </Styled.Content>
+      </Styled.Container>
+    </Anchor>
   );
 };
 

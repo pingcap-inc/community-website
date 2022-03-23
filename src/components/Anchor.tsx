23b358d2535643c7b67b04bffd57682e46ba9014
Chang Wei <changwei1006@gmail.com>
2022-03-24 00:28:25 +0800

    update Anchor component: don't apply target and rel attrs when href includes hash mark '#'

--- src/components/Anchor.tsx
+++ src/components/Anchor.tsx
@@ -4,8 +4,9 @@ import * as React from 'react';
 export interface IProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {}
 
 export default function Anchor({ target, rel, children, ...rest }: IProps) {
-  const props: IProps = { ...rest };
-  props.target = target ?? '_blank';
-  props.rel = rel ?? 'noopener noreferrer';
-  return <a {...props}>{children}</a>;
+  const props: IProps = { ...rest }
+  const hasHash = props.href.includes('#')
+  props.target = hasHash ? '' : (target ?? '_blank')
+  props.rel = hasHash ? '' : (rel ?? 'noopener noreferrer')
+  return <a {...props}>{children}</a>
 }

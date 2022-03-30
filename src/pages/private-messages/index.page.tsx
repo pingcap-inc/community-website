340d21009aeebe9085998751b515f351d77d0992
Chang Wei <changwei1006@gmail.com>
2022-03-30 18:02:07 +0800

    add seo metadata to private-message page

--- src/pages/private-messages/index.page.tsx
+++ src/pages/private-messages/index.page.tsx
@@ -2,10 +2,20 @@
 import Layout from './layout';
 import PrivateMessages from './PrivateMessages.component';
 import { usePrivateMessages } from '~/pages/private-messages/hooks';
+import React from 'react';
+import Head from '~/components/head/Head.component';
 
 export default function Page() {
   const messages = usePrivateMessages(false);
-  return <PrivateMessages messages={messages} />;
+  const seoMetadata = {
+    title: '私信',
+  };
+  return (
+    <>
+      <Head {...seoMetadata} />
+      <PrivateMessages messages={messages} />
+    </>
+  );
 }
 
 Page.Layout = Layout;

3e2d6c49df4db20914bb43ebb0b565137ad67d70
Chang Wei <changwei1006@gmail.com>
2022-03-30 18:03:10 +0800

    add seo metadata to notification page

--- src/pages/notifications/index.page.tsx
+++ src/pages/notifications/index.page.tsx
@@ -1,19 +1,26 @@
 // @ts-ignore
 import Layout from './layout';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Filter } from './layout/menu';
 import Asktug from './Asktug.component';
 import Blog from './Blog.component';
+import Head from '~/components/head/Head.component';
 
 const Page = ({ initIdx }) => {
   const [filter, setFilter] = useState<Filter>();
+  const seoMetadata = {
+    title: '通知',
+  };
   return (
-    <Layout filter={filter} onFilterChange={setFilter} initIdx={initIdx}>
-      {filter?.from === 'asktug' ? <Asktug filter={filter} /> : undefined}
-      {/* prevent react cache */}
-      <div />
-      {filter?.from === 'blog' ? <Blog filter={filter} /> : undefined}
-    </Layout>
+    <>
+      <Head {...seoMetadata} />
+      <Layout filter={filter} onFilterChange={setFilter} initIdx={initIdx}>
+        {filter?.from === 'asktug' ? <Asktug filter={filter} /> : undefined}
+        {/* prevent react cache */}
+        <div />
+        {filter?.from === 'blog' ? <Blog filter={filter} /> : undefined}
+      </Layout>
+    </>
   );
 };
 

d700b16998b3ae007566da6f18c16b13fa85e97c
Jagger <634750802@qq.com>
2022-03-30 14:32:05 +0800

    fix: private message page html meta

--- src/pages/private-messages/index.page.tsx
+++ src/pages/private-messages/index.page.tsx
@@ -2,6 +2,17 @@
 import Layout from './layout';
 import PrivateMessages from './PrivateMessages.component';
 import { usePrivateMessages } from '~/pages/private-messages/hooks';
+import { getI18nProps } from '~/utils/i18n.utils';
+
+export const getStaticProps = async () => {
+  const i18nProps = await getI18nProps(['common'])();
+
+  return {
+    props: {
+      ...i18nProps,
+    },
+  };
+};
 
 export default function Page() {
   const messages = usePrivateMessages(false);

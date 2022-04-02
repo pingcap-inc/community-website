1b232ce8d1d8ee4e3045220b6dc0be92587d15fb
Jagger <634750802@qq.com>
2022-04-02 15:24:53 +0800

    fix

--- packages/ui/src/utils/index.js
+++ packages/ui/src/utils/index.js
@@ -1,5 +1,4 @@
 import * as errors from './errors.utils';
-import * as header from '../components/header/header.utils';
 import * as menu from './menu.utils';
 
-export { errors, header, menu };
+export { errors, menu };

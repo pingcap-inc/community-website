2c75da7a7a6e332405a57f83721a005457a7041c
Chang Wei <changwei1006@gmail.com>
2022-04-07 23:53:50 +0800

    fix deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,8 +1,8 @@
-export $ENVIRONMENT=$1
-export $DOCKER_CONTAINER_NAME=$2
-export $DOCKER_REGISTRY_ADDRESS=$3
-export $DOCKER_REGISTRY_NAMESPACE=$4
-export $DOCKER_IMAGE_NAME=$5
-export $DOCKER_IMAGE_TAG=$6
+export ENVIRONMENT=$1
+export DOCKER_CONTAINER_NAME=$2
+export DOCKER_REGISTRY_ADDRESS=$3
+export DOCKER_REGISTRY_NAMESPACE=$4
+export DOCKER_IMAGE_NAME=$5
+export DOCKER_IMAGE_TAG=$6
 
 cd ${ENVIRONMENT} && docker-compose pull && docker-compose down && docker-compose up -d

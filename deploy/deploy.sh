f3379d31c625fcc095d70b67a2b05f53f1489f61
Chang Wei <changwei1006@gmail.com>
2022-04-07 22:04:08 +0800

    fix $DOCKER_COMPOSE_FILEPATH

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,7 +6,7 @@ export $DOCKER_REGISTRY_NAMESPACE=$5
 export $DOCKER_IMAGE_NAME=$6
 export $DOCKER_IMAGE_TAG=$7
 
-$DOCKER_COMPOSE_FILEPATH="${ENVIRONMENT}/docker-compose.yaml"
+$DOCKER_COMPOSE_FILEPATH="./docker-compose.yaml"
 
 cd ${DOCKER_COMPOSE_FILEPATH} && docker-compose pull && docker-compose down && docker-compose up -d
 

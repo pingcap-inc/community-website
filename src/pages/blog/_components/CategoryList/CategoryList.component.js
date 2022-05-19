109b96b037739dac54ed5888494b267cfbcc0fb8
cw1997 <867597730@qq.com>
2022-05-19 02:52:27 -0700

    add my post link to blog index page

--- src/pages/blog/_components/CategoryList/CategoryList.component.js
+++ src/pages/blog/_components/CategoryList/CategoryList.component.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import * as Styled from './index.styled';
 import { Divider } from 'antd';
 import { usePrincipal } from '../../blog.hooks';
 import Link from 'next/link';
+import { MeContext } from '~/context';
 
 const CategoryList = ({ categories: { content }, shallow = false, current, type }) => {
   const { hasAuthority } = usePrincipal();
   const showAudits = hasAuthority('REVIEW_POST');
+
+  const { meData, isMeValidating } = useContext(MeContext);
 
   return (
     <Styled.Container>
@@ -19,6 +22,7 @@ const CategoryList = ({ categories: { content }, shallow = false, current, type 
       <Divider />
       {showAudits && <NextLink href={`/blog/audits`}>待审核</NextLink>}
       <div>
+        {meData?.username && !isMeValidating && <FixedLink href={`/u/${meData?.username}`}>我的专栏</FixedLink>}
         <FixedLink href={'https://tidb.net/blog/66c5e81b'}>专栏发布指南</FixedLink>
         <FixedLink href={'https://pingcap.com/zh/privacy-policy/'}>隐私协议</FixedLink>
         <FixedLink href={'mailto:community@tidb.io'}>联系我们</FixedLink>

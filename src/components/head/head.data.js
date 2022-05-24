eee6656497d116ec59d8acce9cf80dec1b7bd82d
Jagger <634750802@qq.com>
2022-05-24 10:56:48 +0800

    chore: update ga measurement id

--- src/components/head/head.data.js
+++ src/components/head/head.data.js
@@ -2,11 +2,11 @@ export const tug = {
   description:
     'TUG（TiDB User Group） 是由 TiDB 技术爱好者、使用者自发组织、管理的独立组织。TUG 社区的理念是“连接用户，共建社区”；愿景是打造一个前沿的、活跃的、自治的、汇聚全球数据库、大数据从业者的技术社区',
   faviconPathname: 'tug',
-  googleAnalyticsId: 'G-6W0S4GLFZ1',
+  googleAnalyticsId: 'G-5FQSB5GH7F',
   titleSuffix: 'TUG',
 };
 
 export const community = {
   faviconPathname: 'community',
-  googleAnalyticsId: 'G-BJZ86GFQMT',
+  googleAnalyticsId: 'G-5FQSB5GH7F',
 };

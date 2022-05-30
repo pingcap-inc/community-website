61de9d3406a0789514860af3d6d3186ecd2300c1
cw1997 <867597730@qq.com>
2022-05-29 22:40:48 -0700

    feature/update-page-contact-us-cooperation: update color

--- src/pages/contact-us/cooperation/index.page.js
+++ src/pages/contact-us/cooperation/index.page.js
@@ -7,7 +7,7 @@ import * as Styled from './index.styled';
 import { CommunityHead } from '~/components';
 import { Row, Col } from 'antd';
 import Anchor from '~/components/Anchor';
-import {CoreLayout} from "~/layouts";
+import { CoreLayout } from '~/layouts';
 
 // export { getServerSideProps } from '../utils';
 
@@ -27,7 +27,7 @@ const Page = () => {
           <Styled.Title>Hello! 欢迎来到 TiDB 社区，有什么能帮到你？</Styled.Title>
           <Row gutter={[32, 32]}>
             <Col sm={24} md={8}>
-              <Styled.Card color={'yellow'}>
+              <Styled.Card color={'#F8C200'}>
                 <Styled.CardTitle>遇到技术问题</Styled.CardTitle>
                 <Styled.CardParagraph>你可以在论坛和 2w+ 社区⼩伙伴互相交流问题</Styled.CardParagraph>
                 <Styled.CardParagraph>
@@ -40,7 +40,7 @@ const Page = () => {
               </Styled.Card>
             </Col>
             <Col sm={24} md={8}>
-              <Styled.Card color={'orange'}>
+              <Styled.Card color={'#F15A24'}>
                 <Styled.CardTitle>商业⽀持</Styled.CardTitle>
                 <Styled.CardParagraph>想要获得商业专家产品最⾼响应级别 7*24 ⽀持服务</Styled.CardParagraph>
                 <Styled.CardComment>规划/实施/主动式巡检/故障协查/知识转移/重要时期保障</Styled.CardComment>
@@ -50,7 +50,7 @@ const Page = () => {
               </Styled.Card>
             </Col>
             <Col sm={24} md={8}>
-              <Styled.Card color={'red'}>
+              <Styled.Card color={'#E30C34'}>
                 <Styled.CardTitle>社区合作</Styled.CardTitle>
                 <Styled.CardParagraph>
                   TiDB 社区已经和多家社区建⽴了合作关系，如：思否、开源中国、开源社、infoQ 等。

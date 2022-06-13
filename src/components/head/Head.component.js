8636d186686a841ff854064eb10fb8a82de3f829
Jagger <634750802@qq.com>
2022-06-13 11:38:33 +0800

    chore: tagmanager

--- src/components/head/Head.component.js
+++ src/components/head/Head.component.js
@@ -46,8 +46,22 @@ const Head = ({
       <link rel="shortcut icon" href={getFaviconsPath('/favicon.ico')} />
       <meta name="theme-color" content="#ffffff" />
 
-      {/* GA setup: https://stackoverflow.com/a/62552263/14257627 */}
+      {/* GA tagmanager setup */}
+      <script
+        id="tagmanager-init-script"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${googleAnalyticsId}');
+          `,
+        }}
+      />
       <script async src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`} />
+
+      {/* GA setup: https://stackoverflow.com/a/62552263/14257627 */}
       <script
         id="gtag-init-script"
         // strategy="beforeInteractive"

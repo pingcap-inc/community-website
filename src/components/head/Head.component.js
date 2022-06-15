f1934012ec7e275e4cd75936f64779da5e743035
Jagger <634750802@qq.com>
2022-06-15 13:19:12 +0800

    feat: use tagmanager (#420)
    * chore: tagmanager
    
    * fix

--- src/components/head/Head.component.js
+++ src/components/head/Head.component.js
@@ -46,18 +46,17 @@ const Head = ({
       <link rel="shortcut icon" href={getFaviconsPath('/favicon.ico')} />
       <meta name="theme-color" content="#ffffff" />
 
-      {/* GA setup: https://stackoverflow.com/a/62552263/14257627 */}
-      <script async src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`} />
+      {/* GA tagmanager setup */}
       <script
-        id="gtag-init-script"
-        // strategy="beforeInteractive"
+        id="tagmanager-init-script"
         dangerouslySetInnerHTML={{
           __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${googleAnalyticsId}');
-        `,
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${googleAnalyticsId}');
+          `,
         }}
       />
 

57dded4677ae2b9dea449441368b787f6e3a5698
Jagger <634750802@qq.com>
2022-06-27 13:58:43 +0800

    remove codes

--- packages/ui/src/components/index.js
+++ packages/ui/src/components/index.js
@@ -1,6 +1,5 @@
 export { default as ActivityBanner } from './activityBanner';
 export { default as ActivityCards } from './activityCards';
-export { default as BenefitCards } from './benefitCards';
 export { default as CountDown } from './countDown';
 export { default as Footer } from './footer';
 export { default as Header } from './header';
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -8,7 +8,6 @@ export { default as createAppGlobalStyle } from './globalStyle';
 export {
   ActivityBanner,
   ActivityCards,
-  BenefitCards,
   CountDown,
   Footer,
   Header,

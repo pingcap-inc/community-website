7cff51c9a43e09dc1d700eb62104634758f345a7
Chang Wei <changwei1006@gmail.com>
2022-07-01 11:46:46 +0800

    fix: fix character on tug/apply page, fix content 'whyJoinItems'

--- src/pages/tug/apply/ApplyStart/applyStart.constants.js
+++ src/pages/tug/apply/ApplyStart/applyStart.constants.js
@@ -13,7 +13,7 @@ export const answer = 'TUG 是汇聚全球数据库、⼤数据技术从业者�
 export const whyJoinTitle = '加⼊TUG ，你能获得什么？';
 
 export const whyJoinItems = [
-  { iconUrl: getImage('why-join-1.svg'), text: '让数据库、⼤数据从业者找到⾃⼰的圈⼦' },
-  { iconUrl: getImage('why-join-2.svg'), text: '探索技术问题，随时随地交流成⻓，解决问题' },
-  { iconUrl: getImage('why-join-3.svg'), text: '发表技术⻅解，收获前沿知识，提升个⼈影响⼒' },
+  { iconUrl: getImage('why-join-1.svg'), text: '让数据库、大数据从业者找到自己的圈子' },
+  { iconUrl: getImage('why-join-2.svg'), text: '探索技术问题，随时随地交流成长，解决问题' },
+  { iconUrl: getImage('why-join-3.svg'), text: '发表技术见解，收获前沿知识，提升个人影响力' },
 ];

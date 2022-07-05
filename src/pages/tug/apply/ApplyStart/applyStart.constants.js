6bd0663e334a06d2e8097e2f523dafae2daff4b5
Chang Wei <changwei1006@gmail.com>
2022-07-04 23:41:03 -0700

    Fix/fix character (#429)
    * fix: fix character on tug/apply page, fix content 'whyJoinItems'
    
    * fix: fix character on tug/apply page, fix content 'answer'

--- src/pages/tug/apply/ApplyStart/applyStart.constants.js
+++ src/pages/tug/apply/ApplyStart/applyStart.constants.js
@@ -8,12 +8,12 @@ export const postImageUrl = getImage('post.png');
 
 export const question = 'TUG 是什么';
 
-export const answer = 'TUG 是汇聚全球数据库、⼤数据技术从业者的社区，是⼀个独⽴、⾃发、不以盈利为⽬的的组织。';
+export const answer = 'TUG 是汇聚全球数据库、大数据技术从业者的社区，是一个独立、自发、不以盈利为目的的组织。';
 
 export const whyJoinTitle = '加⼊TUG ，你能获得什么？';
 
 export const whyJoinItems = [
-  { iconUrl: getImage('why-join-1.svg'), text: '让数据库、⼤数据从业者找到⾃⼰的圈⼦' },
-  { iconUrl: getImage('why-join-2.svg'), text: '探索技术问题，随时随地交流成⻓，解决问题' },
-  { iconUrl: getImage('why-join-3.svg'), text: '发表技术⻅解，收获前沿知识，提升个⼈影响⼒' },
+  { iconUrl: getImage('why-join-1.svg'), text: '让数据库、大数据从业者找到自己的圈子' },
+  { iconUrl: getImage('why-join-2.svg'), text: '探索技术问题，随时随地交流成长，解决问题' },
+  { iconUrl: getImage('why-join-3.svg'), text: '发表技术见解，收获前沿知识，提升个人影响力' },
 ];

28ca5ae628f605d49fce6b75da3ff5bc73fef170
Chang Wei <changwei1006@gmail.com>
2022-09-02 14:57:03 +0800

    feature/events/hackathon2022: optimize import
    Signed-off-by: Chang Wei <changwei1006@gmail.com>

--- src/pages/events/hackathon2022/index.styled.ts
+++ src/pages/events/hackathon2022/index.styled.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import Container from 'src/components/Container';
+import Container from '~/components/Container';
 
 export const MyContainer = styled.div`
   background: #20222b;

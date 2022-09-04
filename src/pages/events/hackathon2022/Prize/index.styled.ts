b157d6db040afb099053e9d954fb262dced793b5
Chang Wei <changwei1006@gmail.com>
2022-09-05 02:08:38 +0800

    feature/events/hackathon2022: update Prize, add normal prize list
    Signed-off-by: Chang Wei <changwei1006@gmail.com>

--- src/pages/events/hackathon2022/Prize/index.styled.ts
+++ src/pages/events/hackathon2022/Prize/index.styled.ts
@@ -1,10 +1,9 @@
 import styled from 'styled-components';
-import { Col, Row, Space } from 'antd';
+import { Col, Row } from 'antd';
 
-export const Container = styled(Space).attrs({
-  direction: 'vertical',
-  size: 74,
-})``;
+import normalImage from './normal.png'
+
+export const Container = styled.div``;
 
 export const Description = styled.div`
   margin-top: 8px;
@@ -19,6 +18,7 @@ export const Description = styled.div`
 `;
 
 export const SpecialPrizeTitle = styled.div`
+  margin-top: 78px;
   font-style: normal;
   font-weight: 400;
   font-size: 24px;
@@ -27,6 +27,99 @@ export const SpecialPrizeTitle = styled.div`
   text-align: center;
   letter-spacing: 0.2em;
   color: #ffffff;
+`;
+
+export const SpecialPrizeTitleFirst = styled.div`
+  margin-top: 24px;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 28px;
+  /* identical to box height, or 175% */
+  text-align: center;
+  /* F3 */
+  color: #999999;
+`;
+
+export const SpecialPrizeTitleSecond = styled.div`
+  margin-top: 74px;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 28px;
+  /* identical to box height, or 175% */
+  text-align: center;
+  /* F3 */
+  color: #999999;
+`;
+
+export const SpecialPrizeTopList = styled(Row).attrs({
+  gutter: [118, 118],
+  align: 'center',
+})`
+  margin-top: 56px;
+`;
+
+export const SpecialPrizeTopListColumn = styled(Col).attrs({
+  lg: 6,
+  md: 12,
+  sm: 12,
+  xs: 24,
+})`
+  
+`;
+
+export const SpecialPrizeTopListColumnItem = styled.div`
+  background-image: url(${normalImage.src});
+  background-size: contain;
+  background-repeat: no-repeat;
+  width: 252px;
+  height: 272px;
+  text-align: center;
+  padding-top: 44px;
+  padding-left: 24px;
+  padding-right: 24px;
+`;
+
+export const SpecialPrizeTopListColumnItemName = styled.div`
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 22px;
+  height: 22px;
+  text-align: center;
+  letter-spacing: 0.2em;
+  color: #FFFFFF;
+`;
+
+export const SpecialPrizeTopListColumnItemThing = styled.div`
+  margin-top: 48px;
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 34px;
+  /* identical to box height */
+  text-align: center;
+  color: #FFFFFF;
+`;
+
+export const SpecialPrizeTopListColumnItemTarget = styled.div`
+  margin-top: 32px;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 22px;
+  color: #FFFFFF;
+`;
+
+export const SpecialPrizeTopListItemGold = styled.div`
+  background-image: url("./gold.png");
+  
+`;
+
+export const SpecialPrizeTopListItemSliver = styled.div`
+  background-image: url("./sliver.png");
+  
+`;
+
+export const SpecialPrizeTopListItemBronze = styled.div`
+  background-image: url("./bronze.png");
+  
 `;
 
 export const SpecialPrizeDescription = styled.div`
@@ -69,4 +162,19 @@ export const Item = styled.div`
   width: 160px;
   height: 80px;
   background-color: #fff;
+`;
+
+export const Footer = styled.div`
+  margin-top: 74px;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 28px;
+  /* identical to box height, or 175% */
+  text-align: center;
+  /* F3 */
+  color: #999999;
+  a, a:hover {
+    color: #888DFF;
+    text-decoration: underline;
+  }
 `;
--- src/pages/events/hackathon2022/Prize/index.tsx
+++ src/pages/events/hackathon2022/Prize/index.tsx
@@ -1,8 +1,23 @@
 import * as React from 'react';
 
 import * as Styled from './index.styled';
+import Anchor from "~/components/Anchor";
 
 export interface IProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+const data = {
+  onlyApplicationGroup: [
+    {name: '最佳创意奖', thing: '¥ 5,000', target: '1 支队伍'},
+    {name: '公益贡献奖', thing: '¥ 5,000', target: '1 支队伍'},
+    {name: '技术趋势奖', thing: '¥ 5,000', target: '1 支队伍'},
+    {name: 'Cloud 应用生态奖（API）', thing: '¥ 5,000', target: '1 支队伍'},
+  ],
+  applicationAndTiDBProductGroup: [
+    {name: '最佳人气奖', thing: '键鼠套装 / 人 ', target: '1 支队伍 / 赛道'},
+    {name: '最佳校园奖', thing: '¥ 5,000', target: '1 支队伍 / 赛道'},
+    {name: '用户之选奖', thing: '¥ 5,000', target: '1 支队伍 / 赛道'},
+  ],
+}
 
 const Prize: React.FC<IProps> = (props) => {
   //function SectionTitle(props: IProps) {
@@ -12,8 +27,55 @@ const Prize: React.FC<IProps> = (props) => {
       <Styled.Description>
         各赛道 <span style={{ color: '#00CF71' }}>Top 6</span> 均可获得现金奖项
       </Styled.Description>
+      {/*TODO: gold sliver bronze*/}
       <Styled.SpecialPrizeTitle>特别奖项</Styled.SpecialPrizeTitle>
-      <Styled.SpecialPrizeDescription>仅应用组</Styled.SpecialPrizeDescription>
+      <Styled.SpecialPrizeTitleFirst>仅应用组</Styled.SpecialPrizeTitleFirst>
+      <Styled.SpecialPrizeTopList>
+        {data.onlyApplicationGroup.map((value) => (
+          <Styled.SpecialPrizeTopListColumn>
+            <Styled.SpecialPrizeTopListColumnItem>
+              <Styled.SpecialPrizeTopListColumnItemName>{value.name}</Styled.SpecialPrizeTopListColumnItemName>
+              <Styled.SpecialPrizeTopListColumnItemThing>{value.thing}</Styled.SpecialPrizeTopListColumnItemThing>
+              <Styled.SpecialPrizeTopListColumnItemTarget>{value.target}</Styled.SpecialPrizeTopListColumnItemTarget>
+            </Styled.SpecialPrizeTopListColumnItem>
+          </Styled.SpecialPrizeTopListColumn>
+        ))}
+      </Styled.SpecialPrizeTopList>
+
+      <Styled.SpecialPrizeTitleSecond>应用组 & TiDB 产品组</Styled.SpecialPrizeTitleSecond>
+      <Styled.SpecialPrizeTopList>
+        {data.applicationAndTiDBProductGroup.map((value) => (
+          <Styled.SpecialPrizeTopListColumn>
+            <Styled.SpecialPrizeTopListColumnItem>
+              <Styled.SpecialPrizeTopListColumnItemName>{value.name}</Styled.SpecialPrizeTopListColumnItemName>
+              <Styled.SpecialPrizeTopListColumnItemThing>{value.thing}</Styled.SpecialPrizeTopListColumnItemThing>
+              <Styled.SpecialPrizeTopListColumnItemTarget>{value.target}</Styled.SpecialPrizeTopListColumnItemTarget>
+            </Styled.SpecialPrizeTopListColumnItem>
+          </Styled.SpecialPrizeTopListColumn>
+        ))}
+      </Styled.SpecialPrizeTopList>
+
+      <Styled.Footer>
+        注：所有奖项奖金均为税前金额，奖项评选规则可参考<Anchor href={'#'}>评分规则</Anchor>。
+      </Styled.Footer>
+
+      {/*<Styled.SpecialPrizeTopList>*/}
+      {/*  <Styled.SpecialPrizeTopListItem>*/}
+      {/*    <Styled.SpecialPrizeTopListItemGold>*/}
+
+      {/*    </Styled.SpecialPrizeTopListItemGold>*/}
+      {/*  </Styled.SpecialPrizeTopListItem>*/}
+      {/*  <Styled.SpecialPrizeTopListItem>*/}
+      {/*    <Styled.SpecialPrizeTopListItemSliver>*/}
+
+      {/*    </Styled.SpecialPrizeTopListItemSliver>*/}
+      {/*  </Styled.SpecialPrizeTopListItem>*/}
+      {/*  <Styled.SpecialPrizeTopListItem>*/}
+      {/*    <Styled.SpecialPrizeTopListItemBronze>*/}
+
+      {/*    </Styled.SpecialPrizeTopListItemBronze>*/}
+      {/*  </Styled.SpecialPrizeTopListItem>*/}
+      {/*</Styled.SpecialPrizeTopList>*/}
     </Styled.Container>
   );
 };

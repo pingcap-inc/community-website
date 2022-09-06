f7c9979de50e15d4fa2476ea2e59518e30ae1372
Chang Wei <changwei1006@gmail.com>
2022-09-07 06:50:34 +0800

    refine: update components/Anchor, set children as same as href when the children node is null
    Signed-off-by: Chang Wei <changwei1006@gmail.com>

--- src/components/Anchor.tsx
+++ src/components/Anchor.tsx
@@ -4,11 +4,12 @@ export interface IProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
   isHash?: boolean;
 }
 
-export default function Anchor({ target, rel, children, isHash, ...rest }: IProps) {
+export default function Anchor({ target, rel, isHash, ...rest }: IProps) {
   const props: IProps = { ...rest };
   //const isInternalLink = rest.href.charAt(0) === '/'
   //const hasHash = props.href.includes('#') && isInternalLink
   props.target = isHash ? '' : target ?? '_blank';
   props.rel = isHash ? '' : rel ?? 'noopener noreferrer';
+  const children = props.children ?? props.href;
   return <a {...props}>{children}</a>;
 }

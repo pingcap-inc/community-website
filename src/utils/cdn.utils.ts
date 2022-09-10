63c72f67af4c9b55a93e40ad0a6d209f7b83fc98
Chang Wei <changwei1006@gmail.com>
2022-09-11 06:57:06 +0800

    fix: fix getImageUrl function, use CDN path when the runtime environment is not 'local'
    Signed-off-by: Chang Wei <changwei1006@gmail.com>

--- src/utils/cdn.utils.ts
+++ src/utils/cdn.utils.ts
@@ -1,3 +1,5 @@
 export function getImageUrl(relativePath: string): string {
-  return `${process.env.NEXT_PUBLIC_CDN_URL}/${relativePath}`;
+  return process.env.NEXT_PUBLIC_RUNTIME_ENV !== 'local'
+    ? `${process.env.NEXT_PUBLIC_CDN_URL}/${relativePath}`
+    : relativePath;
 }

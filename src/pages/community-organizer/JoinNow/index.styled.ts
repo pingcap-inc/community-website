02d43843efb3a43a01e2db85e4ce40238dbcddc4
Chang Wei <changwei1006@gmail.com>
2022-09-18 06:03:18 +0800

    feature/community-organizer: add component 'JoinNow'
    Signed-off-by: Chang Wei <changwei1006@gmail.com>

---
+++ src/pages/community-organizer/JoinNow/index.styled.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding: 48px 0;
+  background: #fff7f0;
+`;
+
+export const Title = styled.div`
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 34px;
+  text-align: center;
+  color: #2c2c2c;
+`;
+
+export const Action = styled.div`
+  margin-top: 32px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;

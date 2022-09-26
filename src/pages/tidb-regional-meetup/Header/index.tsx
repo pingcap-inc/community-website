f208e4b11c9babedcdaa02dc7eb6e20f46cafe3f
Chang Wei <changwei1006@gmail.com>
2022-09-26 14:04:41 +0800

    feature/community-organizer: update Header section
    Signed-off-by: Chang Wei <changwei1006@gmail.com>

--- src/pages/tidb-regional-meetup/Header/index.tsx
+++ src/pages/tidb-regional-meetup/Header/index.tsx
@@ -1,15 +1,16 @@
 import * as React from 'react';
-import { Carousel } from 'antd';
-import Image, { StaticImageData } from 'next/image';
+import { Carousel, Col, Row } from 'antd';
+import Image from 'next/image';
+import type { StaticImageData } from 'next/image';
+
+import Container from '~/components/Container';
 
 import * as Styled from './index.styled';
-
 import photo广州Image from './广州.jpg';
 import photo天津Image from './天津.jpg';
 import photo武汉Image from './武汉.jpg';
 import photo济南Image from './济南.jpg';
 import photo石家庄Image from './石家庄.jpg';
-import MyContainer from '~/components/Container';
 
 export interface IProps extends React.HTMLAttributes<HTMLDivElement> {
   data: {
@@ -31,26 +32,29 @@ const Header: React.FC<IProps> = (props) => {
   const { data, ...rest } = props;
   return (
     <Styled.Container {...rest}>
-      <Carousel autoplay>
-        {images.map((value) => (
-          <div key={value.image.src}>
-            <Image {...value.image} layout={'responsive'} />
-          </div>
-        ))}
-      </Carousel>
-
-      <Styled.CardContainer>
-        <MyContainer>
-          <Styled.Card>
-            <Styled.Title>{data.title}</Styled.Title>
-            <Styled.Description>{data.description}</Styled.Description>
-            <Styled.Actions>
-              <Styled.JoinButton>🙋‍ 我要成为地区组织者</Styled.JoinButton>
-              <Styled.NominateButton>我要提名 TA</Styled.NominateButton>
-            </Styled.Actions>
-          </Styled.Card>
-        </MyContainer>
-      </Styled.CardContainer>
+      <Container>
+        <Row gutter={[32, 64]} wrap justify={'space-between'} align={'middle'}>
+          <Col sm={24} md={12}>
+            <Styled.Card>
+              <Styled.Title>{data.title}</Styled.Title>
+              <Styled.Description>{data.description}</Styled.Description>
+              <Styled.Actions>
+                <Styled.JoinButton>🙋‍ 我要成为地区组织者</Styled.JoinButton>
+                <Styled.NominateButton>我要提名 TA</Styled.NominateButton>
+              </Styled.Actions>
+            </Styled.Card>
+          </Col>
+          <Col sm={24} md={12}>
+            <Carousel autoplay dotPosition={'right'}>
+              {images.map((value) => (
+                <div key={value.image.src}>
+                  <Image {...value.image} layout={'responsive'} />
+                </div>
+              ))}
+            </Carousel>
+          </Col>
+        </Row>
+      </Container>
     </Styled.Container>
   );
 };

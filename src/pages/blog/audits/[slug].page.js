458519b9af81505ee01da64294f3178146d0ff51
Chang Wei <changwei1006@gmail.com>
2022-10-16 14:28:29 +0800

    optimize/remove-unused-i18n-props: optimize import on page 'blog/audits/[slug]'
    Signed-off-by: Chang Wei <changwei1006@gmail.com>

--- src/pages/blog/audits/[slug].page.js
+++ src/pages/blog/audits/[slug].page.js
@@ -1,14 +1,4 @@
 import BlogPage from '~/pages/blog/[slug]/index.page';
-import { getI18nProps } from '~/utils/i18n.utils';
-
-export const getServerSideProps = async (ctx) => {
-  const i18nProps = await getI18nProps(['common'])(ctx);
-  return {
-    props: {
-      ...i18nProps,
-    },
-  };
-};
 
 export default function AuditsDetailPage() {
   return <BlogPage />;

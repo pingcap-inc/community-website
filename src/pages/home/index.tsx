d6e63d98d1da4d8df2f712066b367cca918d9417
Chang Wei <changwei1006@gmail.com>
2022-10-16 14:52:24 +0800

    optimize/remove-unused-i18n-props: ad seo title prop on page 'home'
    Signed-off-by: Chang Wei <changwei1006@gmail.com>

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -65,7 +65,7 @@ export const getStaticProps: GetStaticProps = async () => {
 
 const Page: NextPage<{ data: any }> = ({ data }) => (
   <PageDataContext.Provider value={{ data }}>
-    <CommunityHead />
+    <CommunityHead title={'首页'} />
 
     <CoreLayout>
       <Banner />

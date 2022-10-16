dd8c66482bbaf6cdb9dfa53b96c2040ac70c6576
Chang Wei <changwei1006@gmail.com>
2022-10-16 14:30:01 +0800

    optimize/remove-unused-i18n-props: optimize import on page 'blog/[slug]'
    Signed-off-by: Chang Wei <changwei1006@gmail.com>

--- src/pages/blog/[slug]/index.page.js
+++ src/pages/blog/[slug]/index.page.js
@@ -4,24 +4,26 @@ import useSWR from 'swr';
 import { Breadcrumb, Skeleton } from 'antd';
 import NextHead from 'next/head';
 import Link from 'next/link';
+import { cloneDeep, throttle } from 'lodash';
 
-import * as Styled from './blog.styled';
-import { CoreLayout } from '~/layouts';
-import { OriginLabel, RepostLabel } from './components/labels';
-import BlogInfo from '../_components/BlogInfo';
+import { api } from '@tidb-community/datasource';
 import TiEditor, { createFactory } from '@pingcap-inc/tidb-community-editor';
 import '@pingcap-inc/tidb-community-editor/dist/style.css';
+
+import { CommunityHead } from '~/components';
+import { CoreLayout } from '~/layouts';
+import { MeContext } from '~/context';
+import Anchor from '~/components/Anchor';
+import ErrorPage from '~/components/errorPage';
+
+import * as Styled from './blog.styled';
+import { OriginLabel, RepostLabel } from './components/labels';
+import BlogInfo from '../_components/BlogInfo';
 import AuthorInfo from './components/AuthorInfo';
 import Interactions from './components/Interactions';
 import Comments from './components/Comments';
 import StatusAlert from './components/StatusAlert';
-import { api } from '@tidb-community/datasource';
-import { CommunityHead } from '~/components';
 import { usePrincipal } from '../blog.hooks';
-import ErrorPage from '../../../components/errorPage';
-import { MeContext } from '~/context';
-import Anchor from '~/components/Anchor';
-import { cloneDeep, throttle } from 'lodash';
 
 export const getServerSideProps = async (ctx) => {
   const { req } = ctx;

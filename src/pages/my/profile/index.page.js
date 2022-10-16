877660bb1ce9a28f706c0b6f172ed7bc20b6e918
Chang Wei <changwei1006@gmail.com>
2022-10-16 14:16:09 +0800

    optimize/remove-unused-i18n-props: remove i18n props from page 'my/profile'
    Signed-off-by: Chang Wei <changwei1006@gmail.com>

--- src/pages/my/profile/index.page.js
+++ src/pages/my/profile/index.page.js
@@ -1,21 +1,10 @@
 import React, { useContext } from 'react';
 
+import { CommunityHead, PageLoader } from '~/components';
+import { AuthContext, MeContext } from '~/context';
+
 import Form from './form';
 import Layout from '~/pages/my/layout';
-import { AuthContext, MeContext } from '~/context';
-import { CommunityHead } from '~/components';
-import { PageLoader } from '~/components';
-import { getI18nProps } from '~/utils/i18n.utils';
-
-export const getServerSideProps = async (ctx) => {
-  const i18nProps = await getI18nProps(['common'])(ctx);
-
-  return {
-    props: {
-      ...i18nProps,
-    },
-  };
-};
 
 const PageContent = ({ title }) => {
   const { login, isAnonymous } = useContext(AuthContext);

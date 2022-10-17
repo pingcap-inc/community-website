1b8a440a211bb774569e94abf3bd532f2abb3f62
Chang Wei <changwei1006@gmail.com>
2022-10-18 01:15:00 +0800

    fix/account: use account hook to refactor component 'my/Layout'
    Signed-off-by: Chang Wei <changwei1006@gmail.com>

--- src/pages/my/layout/Layout.component.js
+++ src/pages/my/layout/Layout.component.js
@@ -1,34 +1,34 @@
-import React, { useEffect, useContext } from 'react';
-import { Col, Row } from 'antd';
+import React from 'react';
+import {Col, Row} from 'antd';
 
 import * as Styled from './layout.styled';
 import Menu from './menu';
-import { AuthContext } from '~/context';
-import { CoreLayout } from '~/layouts';
+import {CoreLayout} from '~/layouts';
+import {useAccount} from "~/hooks/account";
+import {PageLoader} from "~/components";
 
 const Layout = ({ children, title }) => {
-  const { setIsAuthRequired } = useContext(AuthContext);
-
-  useEffect(() => {
-    setIsAuthRequired(true);
-    return () => setIsAuthRequired(false);
-  }, [setIsAuthRequired]);
-
+  const account = useAccount()
+  
   return (
     <>
       <CoreLayout hasMargin>
         <Styled.Container>
-          <Row gutter={[32, 32]}>
-            <Col xs={24} sm={8} md={6}>
-              <Menu />
-            </Col>
-            <Col xs={24} sm={16} md={18}>
-              <Styled.Main>
-                <Styled.Title>{title}</Styled.Title>
-                <Styled.Content>{children}</Styled.Content>
-              </Styled.Main>
-            </Col>
-          </Row>
+          {account.isValidating ? (
+            <PageLoader />
+          ): (
+            <Row gutter={[32, 32]}>
+              <Col xs={24} sm={8} md={6}>
+                <Menu />
+              </Col>
+              <Col xs={24} sm={16} md={18}>
+                <Styled.Main>
+                  <Styled.Title>{title}</Styled.Title>
+                  <Styled.Content>{children}</Styled.Content>
+                </Styled.Main>
+              </Col>
+            </Row>
+          )}
         </Styled.Container>
       </CoreLayout>
     </>

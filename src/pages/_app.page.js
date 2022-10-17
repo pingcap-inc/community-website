856e1b2fd3bf6c9b9ccb5b77b27f871f7c695559
Chang Wei <changwei1006@gmail.com>
2022-10-18 00:41:35 +0800

    fix/account: optimize _app.page.js
    Signed-off-by: Chang Wei <changwei1006@gmail.com>

--- src/pages/_app.page.js
+++ src/pages/_app.page.js
@@ -1,42 +1,37 @@
-import '@formatjs/intl-numberformat/polyfill';
-import '@formatjs/intl-numberformat/locale-data/en';
-import '@pingcap-inc/tidb-community-ui/dist/antd.css';
-import 'dayjs/locale/zh';
 import * as R from 'ramda';
-import React, { useEffect, useState } from 'react';
+import React, {useEffect} from 'react';
+import {SWRConfig} from 'swr';
+// import {Provider} from 'react-redux';
+import Link from 'next/link';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
-import useSWR, { SWRConfig } from 'swr';
-import { Provider } from 'react-redux';
-import { api, useApiErrorListener } from '@tidb-community/datasource';
-import { appWithTranslation } from 'next-i18next';
-import { constants, createAppGlobalStyle, utils } from '@tidb-community/ui';
-import { logPageView } from '@tidb-community/tracking-script';
-import { message } from 'antd';
-import { withLayout } from '@tidb-community/common';
+import 'dayjs/locale/zh';
 
-import '~/components/Button/Button.scss';
-import '~/components/Container/Container.scss';
-import '~/styles/globals.css';
-import ErrorPage from './_error.page';
-import nextI18NextConfig from '@/next-i18next.config';
-import { authContext, AuthContext, MeContext } from '~/context';
-import { isEmptyOrNil } from '~/utils/common.utils';
-import { store } from '~/redux';
+import {api} from '@tidb-community/datasource';
+import {constants, createAppGlobalStyle} from '@tidb-community/ui';
+import {logPageView} from '@tidb-community/tracking-script';
+import {withLayout} from '@tidb-community/common';
+import {
+  defineSiteComponentsConfig,
+  Env,
+  Site,
+  // SiteComponentsContext,
+} from '@pingcap-inc/tidb-community-site-components';
 
 import '@fullcalendar/common/main.css';
 import '@fullcalendar/daygrid/main.css';
 
-import {
-  SiteComponentsContext,
-  defineSiteComponentsConfig,
-  Site,
-  Env,
-} from '@pingcap-inc/tidb-community-site-components';
-import '@pingcap-inc/tidb-community-site-components/dist/index.css';
-import Link from 'next/link';
+import '@formatjs/intl-numberformat/polyfill';
+import '@formatjs/intl-numberformat/locale-data/en';
 
-import { fetcher as newFetcher } from '~/api';
+import '@pingcap-inc/tidb-community-site-components/dist/index.css';
+import '@pingcap-inc/tidb-community-ui/dist/antd.css';
+
+import '~/components/Button/Button.scss';
+import '~/components/Container/Container.scss';
+import '~/styles/globals.css';
+
+// import {fetcher as newFetcher} from '~/api';
 
 dayjs.extend(relativeTime);
 // TODO: Need to sync with NextJS locale value
@@ -79,35 +74,15 @@ defineSiteComponentsConfig({
   },
 });
 
-const siteFetchers = {
-  fetchers: {
-    accounts: newFetcher,
-    blog: newFetcher,
-    asktug: newFetcher,
-  },
-};
+// const siteFetchers = {
+//   fetchers: {
+//     accounts: newFetcher,
+//     blog: newFetcher,
+//     asktug: newFetcher,
+//   },
+// };
 
 const App = ({ Component, pageProps, router }) => {
-  const [errorStatus, setErrorStatus] = useState();
-  const [errorMsg, setErrorMsg] = useState();
-
-  useApiErrorListener((err) => {
-    if (!err.status) {
-      message.error(utils.errors.getErrorMessage(err));
-      return;
-    }
-
-    const { status, statusText, data } = err;
-    const errorMsg = data?.detail || statusText;
-
-    if ([403, 404].includes(status)) {
-      setErrorStatus(status);
-      setErrorMsg(errorMsg);
-    } else {
-      message.error(`${errorMsg}`, 5);
-    }
-  });
-
   useEffect(() => {
     const handleRouteChange = () => {
       if (process.env.NEXT_PUBLIC_RUNTIME_ENV === 'production') {
@@ -126,53 +101,22 @@ const App = ({ Component, pageProps, router }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  useEffect(() => {
-    setErrorStatus();
-    setErrorMsg();
-  }, [router.pathname]);
-
-  const {
-    data: meResp,
-    error: meError,
-    isValidating: isMeValidating,
-    mutate: mutateMe,
-  } = useSWR('me', fetcher, {
-    // Default configs could be found from
-    // https://github.com/vercel/swr/blob/master/src/config.ts
-    revalidateOnFocus: false,
-    shouldRetryOnError: false,
-  });
-
-  const meData = meResp?.data;
-
-  authContext.isAnonymous = !!meError;
-  authContext.isLoggedIn = !isEmptyOrNil(meData);
-
-  if (errorStatus) {
-    return <ErrorPage statusCode={errorStatus} errorMsg={errorMsg} />;
-  }
-
   const WrappedComponent = withLayout(Component);
 
   return (
     <SWRConfig
       value={{
         fetcher,
-        revalidateOnFocus: false,
       }}
     >
-      <Provider store={store}>
+      {/*<Provider store={store}>*/}
         <GlobalStyle />
-        <AuthContext.Provider value={authContext}>
-          <MeContext.Provider value={{ meData, mutateMe, isMeValidating }}>
-            <SiteComponentsContext.Provider value={siteFetchers}>
-              <WrappedComponent {...pageProps} />
-            </SiteComponentsContext.Provider>
-          </MeContext.Provider>
-        </AuthContext.Provider>
-      </Provider>
+        {/*<SiteComponentsContext.Provider value={siteFetchers}>*/}
+          <WrappedComponent {...pageProps} />
+        {/*</SiteComponentsContext.Provider>*/}
+      {/*</Provider>*/}
     </SWRConfig>
   );
 };
 
-export default appWithTranslation(App, nextI18NextConfig);
+export default App;

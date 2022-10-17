08223d7a26fe458850484327995e9dc17c5be9a3
Chang Wei <changwei1006@gmail.com>
2022-10-18 02:47:57 +0800

    feature/regional-meetup: fix data while user has not any badges
    Signed-off-by: Chang Wei <changwei1006@gmail.com>

--- src/pages/regional-meetup/index.page.tsx
+++ src/pages/regional-meetup/index.page.tsx
@@ -90,7 +90,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
           width: 64,
           height: 64,
         },
-        iconImages: user.badges.map((value) => ({ src: value.image, width: 32, height: 32 })),
+        iconImages: (user.badges ?? []).map((value) => ({ src: value.image, width: 32, height: 32 })),
       });
     } catch (e) {
       console.error('[Error] [/regional-meetup] [getServerSideProps] [sharedContentCards]', e);

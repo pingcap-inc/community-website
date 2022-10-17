add9ae0290e2a62c30681aa2a65d97bd0bae99d5
Chang Wei <changwei1006@gmail.com>
2022-10-18 00:09:06 +0800

    fix/account: refactor _document.page from js to tsx
    Signed-off-by: Chang Wei <changwei1006@gmail.com>

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -2,10 +2,13 @@
 // https://github.com/vercel/next.js/blob/master/examples/with-styled-components/pages/_document.js
 import React from 'react';
 // eslint-disable-next-line @next/next/no-document-import-in-page
-import Document from 'next/document';
+import Document from 'next/document'
+import type {DocumentInitialProps} from 'next/document'
+
 import { ServerStyleSheet } from 'styled-components';
+
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(ctx): Promise<DocumentInitialProps> {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 

ec561d37dcca9a46a9b5c7bce078c4c0a42621e1
Chang Wei <changwei1006@gmail.com>
2022-10-25 21:48:22 +0800

    chore/sitemap-2022-10-25: update next-sitemap.config.js, set generateIndexSitemap to false because baidu tongji doesn't accept the index-sitemap file
    Signed-off-by: Chang Wei <changwei1006@gmail.com>

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_HOME_URL || 'https://tidb.net',
   generateRobotsTxt: true, // (optional)
+  /**
+   * set generateIndexSitemap to false because baidu tongji doesn't accept the index-sitemap file
+   * limit of baidu tongji sitemap.xml at 2022-10: max count < 50,000 and file size < 10 MB
+   * comment by cw1997<changwei@pingcap.com>
+   */
+  generateIndexSitemap: false,
   exclude: [
     '/my',
     '/my/*',

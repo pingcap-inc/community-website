5581a0eab9c286716b4a6ecb3c44399f9b33c288
Chang Wei <changwei1006@gmail.com>
2022-10-25 21:43:38 +0800

    chore/sitemap-2022-10-25: reformat next-sitemap.config.js, move arrow function definition 'getAllBlogs' to the end of file
    Signed-off-by: Chang Wei <changwei1006@gmail.com>

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,4 @@
 /** @type {import('next-sitemap').IConfig} */
-
-const getAllBlogs = require('./scripts/sitemap');
-
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_HOME_URL || 'https://tidb.net',
   generateRobotsTxt: true, // (optional)
@@ -22,3 +19,5 @@ module.exports = {
     return await getAllBlogs();
   },
 };
+
+const getAllBlogs = require('./scripts/sitemap');

0091f95b007c7312435ead0eb8608273f87f3a14
Chang Wei <changwei1006@gmail.com>
2022-10-25 19:43:18 +0800

    chore: sitemap-2022-10-25 (#465)
    * chore/sitemap-2022-10-25: update package.json, execute build:sitemap after build:production
    
    Signed-off-by: Chang Wei <changwei1006@gmail.com>
    
    * chore/sitemap-2022-10-25: update next-sitemap.config.js, exclude some paths which are needed to authorization
    
    Signed-off-by: Chang Wei <changwei1006@gmail.com>
    
    Signed-off-by: Chang Wei <changwei1006@gmail.com>

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,6 +5,18 @@ const getAllBlogs = require('./scripts/sitemap');
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_HOME_URL || 'https://tidb.net',
   generateRobotsTxt: true, // (optional)
+  exclude: [
+    '/my',
+    '/my/*',
+    '/member',
+    '/member/*',
+    '/blog/audit',
+    '/blog/audit/*',
+    '/notification',
+    '/notification/*',
+    '/private-messages',
+    '/private-messages/*',
+  ],
   // ...other options
   additionalPaths: async (config) => {
     return await getAllBlogs();

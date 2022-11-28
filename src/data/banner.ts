c71bf4b077c5417ca055a1c529151f1db431e555
Yan Yan <102777826+Yan-yyCL@users.noreply.github.com>
2022-11-28 11:08:42 +0800

    Update banner.ts
    update ad banner

--- src/data/banner.ts
+++ src/data/banner.ts
@@ -1,2 +1,2 @@
-export const text = 'TiCDC 源码解读上线，让 TiDB 更好用的工具背后的原理是？';
-export const link = 'https://asktug.com/t/topic/995759';
+export const text = 'Serverless HTAP on TiDB Cloud | 12月10日 TiDB Workshop Day 上海站！';
+export const link = 'https://asktug.com/t/topic/996229';

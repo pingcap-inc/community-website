6c3cdbb809c8e97c4ab0856f69f3458248ddf8d3
Yan Yan <102777826+Yan-yyCL@users.noreply.github.com>
2022-12-12 16:12:11 +0800

    Update banner.ts (#478)
    update ad banner

--- src/data/banner.ts
+++ src/data/banner.ts
@@ -1,2 +1,2 @@
-export const text = 'Serverless HTAP on TiDB Cloud | 12月10日 TiDB Workshop Day 上海站！';
-export const link = 'https://asktug.com/t/topic/996229';
+export const text = '社区力量|这里集齐了 34 位社区小伙伴和 TiDB 的故事';
+export const link = 'https://asktug.com/t/topic/994906';

8415ebff3b1625a0e6ba071411e33f6f25705079
Chang Wei <changwei1006@gmail.com>
2022-12-14 23:43:08 +0800

    update 'pages/api/[...dataset].page': add 'middleware' cors
    Signed-off-by: Chang Wei <changwei1006@gmail.com>

--- src/pages/api/cms/[...dataset].page.js
+++ src/pages/api/cms/[...dataset].page.js
@@ -1,3 +1,5 @@
+import NextCors from 'nextjs-cors';
+
 import { api } from '@tidb-community/datasource';
 
 // This handler proxies request to
@@ -8,7 +10,7 @@ const handler = async (req, res) => {
     origin: '*',
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   });
-  
+
   const { dataset, ...params } = req.query;
 
   const client = await api.initStrapiClient();

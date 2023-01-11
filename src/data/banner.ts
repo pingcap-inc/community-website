4231f097ddfa7452336cd783e8e899ad411c16be
billmay <78589535+billmay-B@users.noreply.github.com>
2023-01-11 11:23:51 +0800

    Modify AD entry (#481)

--- src/data/banner.ts
+++ src/data/banner.ts
@@ -1,2 +1,2 @@
-export const text = '社区力量|这里集齐了 34 位社区小伙伴和 TiDB 的故事';
-export const link = 'https://asktug.com/t/topic/994906';
+export const text = 'TiDB 社区 2022 年度总结';
+export const link = 'https://asktug.com/t/topic/998896';

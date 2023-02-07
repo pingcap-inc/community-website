d4bfd97ce07d997a96e79f40251c071ea813bf81
billmay <78589535+billmay-B@users.noreply.github.com>
2023-02-07 11:10:33 +0800

    修改
    修改了最新的banner 广告位

--- src/data/banner.ts
+++ src/data/banner.ts
@@ -1,2 +1,2 @@
-export const text = 'TiDB 社区 2022 年度总结';
-export const link = 'https://asktug.com/t/topic/998896';
+export const text = '【上海站 Meetup 报名中】2月25日，一起来共同探讨数据库未来的发展趋势，听听 TiDB 的产品发展路线！';
+export const link = 'https://forms.pingcap.com/f/meetup-shanghai';

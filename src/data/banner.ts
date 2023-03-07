425bc9a908ee9689e49f0426a3299d9a67ee3e43
billmay <78589535+billmay-B@users.noreply.github.com>
2023-03-07 15:00:48 +0800

    更新 banner (#486)

--- src/data/banner.ts
+++ src/data/banner.ts
@@ -1,2 +1,2 @@
-export const text = '【社区智慧合集】TiDB 用户实践行业案例&解决方案';
-export const link = 'https://asktug.com/t/topic/1002009';
+export const text = '【征文大赛】TiDB 社区第二届征文大赛，一次性带走社区全部新周边，还有bose 降噪耳机、倍轻松按摩仪等你拿！';
+export const link = 'https://asktug.com/t/topic/1002476';

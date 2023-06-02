5ab3d932c581424b2f90c9a0279ac75bb9083397
Chang Wei <changwei1006@gmail.com>
2023-06-02 15:17:34 +0800

    Update banner.ts

--- src/data/banner.ts
+++ src/data/banner.ts
@@ -1,2 +1,2 @@
-export const text = '【【TiDB v7.1 荣誉体验官招募】索尼PS5、索尼无线降噪耳机、倍轻松颈部按摩器等你拿！';
+export const text = '【TiDB v7.1 荣誉体验官招募】索尼PS5、索尼无线降噪耳机、倍轻松颈部按摩器等你拿！';
 export const link = 'https://asktug.com/t/topic/1006975';

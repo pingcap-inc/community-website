522ff2f772d45a5d21f23c60afcf02d69da02b99
billmay <78589535+billmay-B@users.noreply.github.com>
2023-06-02 13:38:08 +0800

    Update banner.ts (#490)
    更新banner

--- src/data/banner.ts
+++ src/data/banner.ts
@@ -1,2 +1,2 @@
-export const text = '【征文大赛】TiDB 社区第二届征文大赛，一次性带走社区全部新周边，还有bose 降噪耳机、倍轻松按摩仪等你拿！';
-export const link = 'https://asktug.com/t/topic/1002476';
+export const text = '【【TiDB v7.1 荣誉体验官招募】索尼PS5、索尼无线降噪耳机、倍轻松颈部按摩器等你拿！';
+export const link = 'https://asktug.com/t/topic/1006975';

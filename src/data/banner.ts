d6abcb2584e5c3a7d252c9ea2ac4db4dfceae16c
billmay1023 <86404540+billmay1023@users.noreply.github.com>
2023-08-01 17:46:14 +0800

    Update banner.ts
    update

--- src/data/banner.ts
+++ src/data/banner.ts
@@ -1,2 +1,2 @@
-export const text = '【TiDB Future App Hackathon 2023 】TiDB 首届全球黑客马拉松，开发者的狂欢夏日盛会！快来一起 Coding 吧！';
-export const link = 'https://asktug.com/t/topic/1007076';
+export const text = '【七夕节为爱挑战】“爱有多深，TiDB 就有多稳”TiDB 社区为你准备好了给另一半的“七夕节”礼物，回复“我要挑战”参加吧！';
+export const link = 'https://asktug.com/t/topic/1008602';

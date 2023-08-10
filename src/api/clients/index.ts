46ca17aef52bc19e60618f45653f505c2c405135
Chang Wei <changwei1006@gmail.com>
2023-08-11 00:27:59 +0800

    Revert "fix: prevent api error breaking pages (#470)"
    This reverts commit 27680b63a2d1a0dcbf22929f6a1ad0e125f34e67.

--- src/api/clients/index.ts
+++ src/api/clients/index.ts
@@ -4,17 +4,10 @@ import _client from './client';
 import _nextClient from './nextClient';
 
 declare module 'axios' {
-  export interface AxiosRequestConfig<D> {
+  export interface AxiosRequestConfig {
     isDispatchApiError?(res: AxiosResponse): boolean;
     isReturnErrorResponse?: boolean;
-
-    /**
-     * Prevent breaking pages while some error occurred.
-     * If the fallbackResponse is used, the error would be sent to sentry.
-     *
-     * @see import('./interceptors/fallback').addFallbackDataInterceptors
-     */
-    fallbackResponse?: D;
+    fallbackResponse?: any;
   }
 }
 

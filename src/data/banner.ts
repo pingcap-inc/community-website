2b1a01b058ac8c8d0c916aa27cf2992e3df95e1b
billmay <78589535+billmay-B@users.noreply.github.com>
2023-10-08 13:21:50 +0800

    Update banner.ts (#498)
    update

--- src/data/banner.ts
+++ src/data/banner.ts
@@ -1,2 +1,2 @@
-export const text = '【Hackathon 获奖名单公布】TiDB Hackathon 2023 获奖名单新鲜出炉，来看一下都有哪些特别惊喜的项目吧！';
-export const link = 'https://asktug.com/t/topic/1011267';
+export const text = 'TiDB 社区第三届 1024 程序员心愿节｜你的心愿，我来实现，人均带着一个礼物的时刻到啦！';
+export const link = 'https://asktug.com/t/topic/1013747';

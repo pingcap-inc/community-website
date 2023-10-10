15aed73be2cafac7e582a60d62e194a27b72737c
Chang Wei <changwei1006@gmail.com>
2023-10-10 22:20:37 +0800

    feature/order-cms-data-by-create-datetime: use Axios response interceptor to re-order the data from CMS by 'created_at'
    Signed-off-by: Chang Wei <changwei1006@gmail.com>

--- packages/datasource/src/api/initStrapiClient.js
+++ packages/datasource/src/api/initStrapiClient.js
@@ -1,11 +1,19 @@
 import axios from 'axios';
 
+const sortCmsDataByCreatedDatetime = (data) => {
+  if (Array.isArray(data) && data.length > 0 && data.filter(({ created_at }) => !(created_at == null))) {
+    return data.sort((a, b) => (new Date(a.created_at) > new Date(b.created_at) ? -1 : 1));
+  } else {
+    return data;
+  }
+};
+
 const initStrapiClient = async ({ baseUrl, email, password } = {}) => {
   const client = axios.create({
     baseURL: baseUrl ?? process.env.NEXT_PUBLIC_STRAPI_BASE_URL,
   });
 
-  client.interceptors.response.use(({ data }) => data);
+  client.interceptors.response.use(({ data }) => sortCmsDataByCreatedDatetime(data));
 
   const authResp = await client.post('/admin/login', {
     email: email ?? process.env.NEXT_PUBLIC_STRAPI_EMAIL,

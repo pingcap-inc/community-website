aa276f713b9fdc3a1bf8320b9ea39be00d29b2e7
Joyz-zzz <109236786+Joyz-zzz@users.noreply.github.com>
2024-02-21 14:37:44 +0700

    Update banner.ts

--- src/data/banner.ts
+++ src/data/banner.ts
@@ -1,2 +1,2 @@
-export const text = '2023 年 TiDB 社区年终总结，喜迎 2024，祝大家新年胜旧年！抽 10 名送 PingCAP & TiDB 2024 年新年礼盒';
-export const link = 'https://asktug.com/t/topic/1020038';
+export const text = '【3月9日】TiDB 社区活动在深圳！一起聊聊分布式数据库的选型，听听零售行业老法师们选择从 MySQL 迁移到 TiDB 的思考和实践！还有 TiDB 7.5 LTS 高性能批处理方案!';
+export const link = 'https://asktug.com/t/topic/1021705';

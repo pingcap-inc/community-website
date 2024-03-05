e75bac56b0e4a842c73a3a699c79ee3e10e5ceab
Joy Zhou <109236786+Joyz-zzz@users.noreply.github.com>
2024-03-05 11:25:47 +0800

    Update banner.ts (#506)
    update event promotion information

--- src/data/banner.ts
+++ src/data/banner.ts
@@ -1,2 +1,2 @@
-export const text = '【3月9日】TiDB 社区活动在深圳！一起聊聊分布式数据库的选型，听听零售行业老法师们选择从 MySQL 迁移到 TiDB 的思考和实践！还有 TiDB 7.5 LTS 高性能批处理方案!';
-export const link = 'https://asktug.com/t/topic/1021705';
+export const text = '【3月9日 TiDB 社区活动在深圳】你是不是也想知道 TiDB 在零售、互联网+、Saas、游戏、出海企业的降本增效方案呢？本周六，深圳见！速来！ ';
+export const link = 'https://asktug.com/t/topic/1022388';

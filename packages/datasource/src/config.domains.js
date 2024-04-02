827eec41fe5eb6c7510528ad622ec80315cf7a13
WD <me@wangdi.ink>
2024-04-02 21:35:40 +0800

    migrate account preview domain (#509)
    * migrate account preview domain
    
    * update
    
    * update dep
    
    * update urls
    
    ---------
    
    Co-authored-by: Jagger <634750802@qq.com>

--- packages/datasource/src/config.domains.js
+++ packages/datasource/src/config.domains.js
@@ -9,12 +9,12 @@ export const defaultEnvDomains = {
     'tidb.net': 'community-preview.tidb.io',
     'contributor.tidb.io': 'community-preview-contributor.tidb.io',
     'asktug.com': 'community-preview.asktug.com',
-    'accounts.pingcap.com': 'accounts-preview.pingcap.com',
+    'accounts.pingcap.com': 'accounts-preview.pingcap.cn',
   },
   local: {
     'tidb.net': 'localhost:3000',
     'contributor.tidb.io': 'contributor.tidb-local.io',
     'asktug.com': 'community-preview.asktug.com',
-    'accounts.pingcap.com': 'accounts-preview.pingcap.com',
+    'accounts.pingcap.com': 'accounts-preview.pingcap.cn',
   },
 };

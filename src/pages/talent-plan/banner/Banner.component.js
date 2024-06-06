e7fb4341e1e5de5bc9541b01a41359476dce88ef
xuliwenwenwen <74528885+xuliwenwenwen@users.noreply.github.com>
2024-06-06 10:35:22 +0300

    feat: update talent plan ranking (#511)
    * feat: update talent plan ranking
    
    * fest: update banner button space style

--- src/pages/talent-plan/banner/Banner.component.js
+++ src/pages/talent-plan/banner/Banner.component.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Button, Space, Tooltip } from 'antd';
-import { GithubOutlined, MailOutlined } from '@ant-design/icons';
+import Icon, { GithubOutlined, MailOutlined } from '@ant-design/icons';
 
 import { useIsSmallScreen } from '~/hooks';
 import TwoColumnsSection from '~/layouts/twoColumnsSection';
 import { getImage } from '~/pages/talent-plan/talent-plan.utils';
 import Anchor from '~/components/Anchor';
+import AtomGitSvg from './AtomGitSvg';
 
 import * as Styled from './banner.styled';
+
+const AtomGitIcon = (props) => <Icon component={AtomGitSvg} {...props} />;
 
 const Banner = () => {
   const { isSmallScreen, breakpoint } = useIsSmallScreen();
@@ -23,10 +26,15 @@ const Banner = () => {
                 以 Talent Plan
                 开源数据库开发课程为依托，联合优秀高校和企业，建设成对全国各高校数据库开发人才培养的最佳实践平台。既能帮助学习者掌握数据库开发的理论知识，进行实际数据库开发锻炼，又能给与学习者使用开源资源，开发开源软件的培养。
               </Styled.Intro>
-              <Space size={24}>
+              <Space size={24} wrap>
                 <Anchor href="https://github.com/pingcap/talent-plan" style={{ fontSize: 24 }}>
                   <Button type={'text'} icon={<GithubOutlined />}>
-                    开始学习
+                    Github 入口
+                  </Button>
+                </Anchor>
+                <Anchor href="https://atomgit.com/pingcap/talent-plan#/" style={{ fontSize: 24 }}>
+                  <Button type={'text'} icon={<AtomGitIcon />}>
+                    AtomGit 入口
                   </Button>
                 </Anchor>
                 <Anchor href="mailto:talent-plan@tidb.io" style={{ fontSize: 24 }}>

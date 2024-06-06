150a04af05ce3d83fdec27708a8a70c825ecc3e8
xuliwenwenwen <liwen.xu@pingcap.com>
2024-06-06 12:30:07 +0800

    fest: update banner button space style

--- src/pages/talent-plan/banner/Banner.component.js
+++ src/pages/talent-plan/banner/Banner.component.js
@@ -26,7 +26,7 @@ const Banner = () => {
                 以 Talent Plan
                 开源数据库开发课程为依托，联合优秀高校和企业，建设成对全国各高校数据库开发人才培养的最佳实践平台。既能帮助学习者掌握数据库开发的理论知识，进行实际数据库开发锻炼，又能给与学习者使用开源资源，开发开源软件的培养。
               </Styled.Intro>
-              <Space size={24}>
+              <Space size={24} wrap>
                 <Anchor href="https://github.com/pingcap/talent-plan" style={{ fontSize: 24 }}>
                   <Button type={'text'} icon={<GithubOutlined />}>
                     Github 入口

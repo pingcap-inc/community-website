d61f3a1a6bf71bd729f96a226615aefa0dc95a15
Jagger <634750802@qq.com>
2024-07-01 12:10:47 +0800

    Update banner.ts (#514)
    升级活动宣传
    
    Co-authored-by: Joy Zhou <109236786+Joyz-zzz@users.noreply.github.com>

--- src/data/banner.ts
+++ src/data/banner.ts
@@ -1,3 +1,3 @@
 export const text =
-  '【6月22日｜TiDB 地区交流活动（武汉站）开启报名！】和 TiDB 社区版主、TiCDC 研发负责人、TiDB 元老级用户一起聊聊“传统vs分布式｜分布式数据库 TiDB 的优势场景和未来趋势”';
-export const link = 'https://asktug.com/t/topic/1027063';
+  '【升级好机会】2024 TiDB 社区升级互助活动火热进行中！由 20 位社区版主、资深布道师及官方技术支持组成的升级导师天团为你升级保驾护航！';
+export const link = 'https://asktug.com/t/topic/1025499';

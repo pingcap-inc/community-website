c13021cacd5035458454525fc5f9abda8a9ae99d
Jagger <634750802@qq.com>
2024-07-15 14:47:47 +0800

    update

--- src/pages/home/others/Others.component.js
+++ src/pages/home/others/Others.component.js
@@ -8,24 +8,9 @@ import { PageDataContext } from '~/context';
 import { useIsSmallScreen } from '~/hooks';
 
 import * as Styled from './others.styled';
-import zhihuLogoImage from './others-zhihu-logo.png';
-import xiaomiLogoImage from './others-xiaomi-logo.png';
-import iqiyiLogoImage from './others-iqiyi-logo.png';
-import ucloudLogoImage from './others-ucloud-logo.png';
-import bilibiliLogoImage from './others-bilibili-logo.png';
-import pingcapLogoImage from './others-pingcap-logo.png';
 import PingcapLogoSvg from './others-pingcap-logo.svg';
 import flinkLogoImage from './others-flink-logo.png';
 import actionLogoImage from './others-action-logo.png';
-
-const jobLogos = [
-  { name: 'zhihu', image: zhihuLogoImage },
-  { name: 'xiaomi', image: xiaomiLogoImage },
-  { name: 'iqiyi', image: iqiyiLogoImage },
-  { name: 'ucloud', image: ucloudLogoImage },
-  { name: 'bilibili', image: bilibiliLogoImage },
-  { name: 'pingcap', image: pingcapLogoImage },
-];
 
 const Others = () => {
   const { data } = useContext(PageDataContext);
@@ -48,22 +33,6 @@ const Others = () => {
                 </Col>
               ))}
             </Row>
-            <Styled.Divider />
-            <Styled.ModuleTitle>
-              <Styled.Subtitle> 工作机会 </Styled.Subtitle>
-              <Styled.Link href="https://tidb-jobs.pingcap.com/">查看更多</Styled.Link>
-            </Styled.ModuleTitle>
-            <Row>
-              <Styled.Text>
-                如果你喜欢新技术，希望在分布式数据库领域实现自己的价值，TiDB
-                社区联合了相关生态企业为你提供了就业的机会，帮助你在工作中实践 TiDB 知识，获得职业上的成长。
-              </Styled.Text>
-            </Row>
-            <Styled.LogosBox size={36} justify="left" wrap $isSmallScreen={isSmallScreen}>
-              {jobLogos.map(({ name, image }) => (
-                <img key={name} {...image} alt={`${name} logo`} />
-              ))}
-            </Styled.LogosBox>
           </>
         }
         rightPanel={
@@ -94,12 +63,6 @@ const Others = () => {
                 <Image {...actionLogoImage} width={133} height={36} alt={'action logo'} />
               </div>
             </Styled.LogoWrapper>
-            <Styled.SmallerText>
-              <Styled.SmallerLink href={'https://opensource.actionsky.com'}>爱可生社区</Styled.SmallerLink>
-              以开源高质量的运维工具、日常分享技术干货内容、持续的全国性的社区活动为社区己任；立志于每年 10.24
-              日开源一款高质量的数据库相关的开源产品；目前开源的产品有：分布式中间件 DBLE、数据传输组件
-              DTLE、全局事务框架 TXLE、SQL 审核组件 SQLE。
-            </Styled.SmallerText>
             {/*<Styled.Divider />*/}
             {/*<Styled.SmallerText>*/}
             {/*  {contactLang.label}*/}

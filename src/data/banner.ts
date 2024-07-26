abd51b09e167887ec6e163630d19fa3cacdcf858
Jagger <634750802@qq.com>
2024-07-26 11:04:25 +0800

    update

--- src/data/banner.ts
+++ src/data/banner.ts
@@ -1,3 +1,3 @@
 export const text =
-  '2024 TiDB AI 创新应用黑客马拉松火热进行中！一起来用 TiDB 构建未来的 AI 创新应用，瓜分超￥210,000 奖金池！';
+  '2024 TiDB AI 创新应用黑客马拉松火热进行中！一起来用 TiDB 构建未来的 AI 创新应用，瓜分超￥220,000 奖金池！';
 export const link = 'https://asktug.com/t/topic/1029569';

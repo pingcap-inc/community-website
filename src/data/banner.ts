158fa971cbcdb90b78ba6758e869b17e96ea04dd
Mini256 <minianter@foxmail.com>
2024-11-15 15:28:17 +0800

    Update banner.ts (#522)
    promote Dalian Meetup
    
    Co-authored-by: Joy Zhou <109236786+Joyz-zzz@users.noreply.github.com>

--- src/data/banner.ts
+++ src/data/banner.ts
@@ -1,3 +1,3 @@
 export const text =
-  '10 月 26 号！TiDB 地区交流活动（杭州站）-走进电魂网络｜一起聊聊核心系统上 TiDB 的收益和体验 & 多业务场景怎么在 TiDB 一站式实现！';
-export const link = 'https://asktug.com/t/topic/1032923';
+  '【11月30日】TiDB 社区活动大连站——走进乐天 Rakuten：了解从数据库选型到运维 TiDB 为你带来的全新体验！解锁新城市，下一站，遇见你！';
+export const link = 'https://asktug.com/t/topic/1035337';

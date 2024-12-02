c81ee7097ac120b1c6b7981e2443beb7e932896f
Jagger <634750802@qq.com>
2024-12-02 14:15:51 +0800

    Update banner.ts (#524)
    TiDB 大连站活动回顾
    
    Co-authored-by: Joy Zhou <109236786+Joyz-zzz@users.noreply.github.com>

--- src/data/banner.ts
+++ src/data/banner.ts
@@ -1,3 +1,3 @@
 export const text =
-  '【11月30日】TiDB 社区活动大连站——走进乐天 Rakuten：了解从数据库选型到运维 TiDB 为你带来的全新体验！解锁新城市，下一站，遇见你！';
-export const link = 'https://asktug.com/t/topic/1035337';
+  '【活动回顾 & PPT 下载】TiDB 社区活动大连站——走进乐天 Rakuten：了解从数据库选型到运维，TiDB 为你带来的全新体验！';
+export const link = 'https://asktug.com/t/topic/1036250';

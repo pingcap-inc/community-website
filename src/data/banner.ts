ee67fecc074da8bce3fe0a15187c51b774e29b4e
Joy Zhou <109236786+Joyz-zzz@users.noreply.github.com>
2024-12-11 16:25:52 +0800

    Update banner.ts
    promote community event

--- src/data/banner.ts
+++ src/data/banner.ts
@@ -1,3 +1,3 @@
 export const text =
-  '【活动回顾 & PPT 下载】TiDB 社区活动大连站——走进乐天 Rakuten：了解从数据库选型到运维，TiDB 为你带来的全新体验！';
-export const link = 'https://asktug.com/t/topic/1036250';
+  '【12 月 28 日·上海站】TiDB 社区活动走进哔哩哔哩：一起聊聊国产数据库替换下新的使用方式，一套 TiDB 简化技术栈 & 承载企业多业务架构难题！';
+export const link = 'https://asktug.com/t/topic/1036631';

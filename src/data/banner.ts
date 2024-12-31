95c202a3fbf78a9734e0be52da3afd2ba4aee085
Joy Zhou <109236786+Joyz-zzz@users.noreply.github.com>
2024-12-31 14:37:33 +0800

    Update banner.ts
    更新活动回顾

--- src/data/banner.ts
+++ src/data/banner.ts
@@ -1,3 +1,3 @@
 export const text =
-  '【12 月 28 日·上海站】TiDB 社区活动走进哔哩哔哩：一起聊聊国产数据库替换下新的使用方式，一套 TiDB 简化技术栈 & 承载企业多业务架构难题！';
-export const link = 'https://asktug.com/t/topic/1036631';
+  '【PPT 下载】TiDB 在小红书、爱奇艺、咪咕、华安基金、数禾科技的核心场景/简化技术栈/降本增效实践，一起探索国产数据库替换下简化技术栈的收益和实践!';
+export const link = 'https://asktug.com/t/topic/1037542'

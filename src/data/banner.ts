aa2b2286af119fce9ab7307a71f4e4042ac2f742
Joy Zhou <109236786+Joyz-zzz@users.noreply.github.com>
2025-01-24 12:30:28 +0800

    Update banner.ts
    宣传 2024 年度用户白皮书

--- src/data/banner.ts
+++ src/data/banner.ts
@@ -1,3 +1,3 @@
 export const text =
-  '【PPT 下载】TiDB 在小红书、爱奇艺、咪咕、华安基金、数禾科技的核心场景/简化技术栈/降本增效实践，一起探索国产数据库替换下简化技术栈的收益和实践!';
-export const link = 'https://asktug.com/t/topic/1037542'
+  '【2024 TiDB 社区用户案例合集白皮书】集结 40+ 金融、互联网、零售、制造、物流、游戏行业用户全场景实践！';
+export const link = 'https://asktug.com/t/topic/1038427'

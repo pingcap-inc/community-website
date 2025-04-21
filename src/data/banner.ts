6dfc1a4361491b605738a7e3f7a4ab0f9a9fe03b
Jagger <634750802@qq.com>
2025-04-21 18:31:53 +0800

    Update banner.ts (#532)
    Co-authored-by: Elliotpp <164619510+Elliotpp@users.noreply.github.com>

--- src/data/banner.ts
+++ src/data/banner.ts
@@ -1,3 +1,3 @@
 export const text =
-  '【PPT 下载】TiDB 社区交流南京站，面向未来的国产数据库怎么选择？一键 get 从 MySQL/Oracle/SQL Server/Hadoop 到 HTAP 数据库 TiDB 的转型实战！';
-export const link = 'https://asktug.com/t/topic/1041158'
+  'TiDB 社区第四届专栏征文大赛联合墨天轮火热开启，TiDB 业务场景实战、运维开发攻略两大赛道，BOSE 降噪耳机、大疆手持云台、投影仪、运动手环、礼品卡等重磅礼品等你来拿！';
+export const link = 'https://asktug.com/t/topic/1041264'

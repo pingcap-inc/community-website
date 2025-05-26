926041f1c592bd1c3c931c6dd5652f2908c271da
Elliotpp <164619510+Elliotpp@users.noreply.github.com>
2025-05-26 14:24:05 +0800

    Update banner.ts

--- src/data/banner.ts
+++ src/data/banner.ts
@@ -1,3 +1,3 @@
 export const text =
-  '【开启报名啦】5 月 24 日 TiDB 社区活动在重庆！一起聊聊国产化+AI 时代：从 MySQL/RDS/PostgreSQL 跃迁到 TiDB，高成长企业的数据库选型和降本增效实践！';
-export const link = 'https://asktug.com/t/topic/1041855'
+  'TiDB 社区✖️墨天轮 第四届专栏征文大赛等你来投稿！TiDB 业务场景实战、运维开发攻略两大赛道，15席大奖虚位以待！';
+export const link = 'https://asktug.com/t/topic/1041264'

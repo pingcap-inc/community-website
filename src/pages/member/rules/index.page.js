3495dab674bdf7ed1faf57bfdbbed806a06517a0
Jagger <634750802@qq.com>
2025-06-25 14:48:34 +0800

    fix

--- src/pages/member/rules/index.page.js
+++ src/pages/member/rules/index.page.js
@@ -103,6 +103,7 @@ const Page = () => (
             </td>
             <td>50</td>
             <td>50</td>
+            <td />
             <td>仅第一次完善信息奖励</td>
             <td />
           </tr>
@@ -114,6 +115,7 @@ const Page = () => (
             <td>20</td>
             <td>仅第一次完善信息奖励</td>
             <td />
+            <td />
           </tr>
           <tr>
             <td>
@@ -122,6 +124,7 @@ const Page = () => (
             <td>10</td>
             <td>20</td>
             <td>仅第一次绑定账号奖励</td>
+            <td />
             <td>
               Contributor 在绑定成功后将自动授予：
               <Anchor href="https://asktug.com/badges/111/-">Contributor 专属徽章</Anchor>
@@ -134,6 +137,7 @@ const Page = () => (
             <td>10</td>
             <td>20</td>
             <td>仅第一次绑定邮箱奖励</td>
+            <td />
             <td>
               绑定邮箱之后将可解锁 TiDB
               社区每周精选邮件推送功能，第一时间获得社区精选资讯(热门问答+干货技术文章+活动推荐)。
@@ -146,6 +150,7 @@ const Page = () => (
             <td>10</td>
             <td>20</td>
             <td>仅第一次绑定手机号码奖励</td>
+            <td />
             <td>绑定手机号码是为了方便紧急情况下，社区技术老师可以第一时间与你取得联系，协助解决技术问题。</td>
           </tr>
           <tr>
@@ -155,6 +160,7 @@ const Page = () => (
             <td>2 ~ 5</td>
             <td>2 ~ 5</td>
             <td>1</td>
+            <td />
             <td>
               <Anchor href="/member#/summary">点击『签到』</Anchor>领取奖励, 签到 1 天奖励 2 分；连续签到 2 天奖励 3
               分；连续签到 3 天及以上奖励 5 分，断签重新开始。

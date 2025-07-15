84ac60ca03dc741aa7ba32df29b29b595504d310
Jagger <634750802@qq.com>
2025-07-15 16:23:07 +0800

    Update banner.ts (#537)
    Co-authored-by: Elliotpp <164619510+Elliotpp@users.noreply.github.com>

--- src/data/banner.ts
+++ src/data/banner.ts
@@ -1,3 +1,3 @@
 export const text =
-  '【活动回顾 & PPT 下载】TiDB 社区活动沈阳站，聚焦民生，TiDB 加速政企数字化转型！活动帖评论区分享活动收获，参与社区新款周边抽奖！ ';
-export const link = 'https://asktug.com/t/topic/1043168'
+  '【开启报名啦】8 月 9 日，TiDB 社区活动在厦门！瑞幸咖啡、美柚、美图、飞连、福建四大机场、厦门三甲医院的 TiDB 实践分享 & 数据库选型趋势！';
+export const link = 'https://asktug.com/t/topic/1044167'

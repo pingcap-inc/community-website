3e554629665d9e8707d96919a1c77a9fde8f1ff8
Jagger <634750802@qq.com>
2025-08-25 15:05:09 +0800

    Update banner.ts (#540)
    Co-authored-by: Elliotpp <164619510+Elliotpp@users.noreply.github.com>

--- src/data/banner.ts
+++ src/data/banner.ts
@@ -1,3 +1,3 @@
 export const text =
-  '平凯数据库（TiDB 企业版）敏捷模式内测开启！报名即可获得 6 个月的企业版使用权限 & 领取考试课程券和积分奖励，更有社区定制款行李箱！';
-export const link = 'https://asktug.com/t/topic/1045496'
+  'TiDB vs MySQL 最全产品介绍、对比参考材料、工程化迁移方案、用户案例等，点击一键查看！';
+export const link = 'https://asktug.com/t/topic/1038940'

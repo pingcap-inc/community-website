df3d81320999611f0a859b6c813232030468e227
Jagger <634750802@qq.com>
2026-01-19 13:05:50 +0800

    update

--- src/components/Anchor.tsx
+++ src/components/Anchor.tsx
@@ -4,12 +4,19 @@ export interface IProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
   isHash?: boolean;
 }
 
-export default function Anchor({ target, rel, isHash, ...rest }: IProps) {
+export default function Anchor({ target, rel, isHash, href, ...rest }: IProps) {
   const props: IProps = { ...rest };
   //const isInternalLink = rest.href.charAt(0) === '/'
   //const hasHash = props.href.includes('#') && isInternalLink
   props.target = isHash ? '' : target ?? '_blank';
   props.rel = isHash ? '' : rel ?? 'noopener noreferrer';
+
+  // Add path prefix
+  if (href && href.startsWith('/') && !href.startsWith('//')) {
+    href = '/tidbcommunity' + href;
+  }
+  props.href = href;
+  console.log(props.href, href);
   const children = props.children ?? props.href;
   return <a {...props}>{children}</a>;
 }

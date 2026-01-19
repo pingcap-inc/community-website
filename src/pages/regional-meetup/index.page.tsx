7b0e38086795b6684548258f6516ba3b053c929e
Jagger <634750802@qq.com>
2026-01-19 12:25:58 +0800

    update

--- src/pages/regional-meetup/index.page.tsx
+++ src/pages/regional-meetup/index.page.tsx
@@ -102,25 +102,25 @@ export default RegionalMeetupPage;
 
 export const getStaticProps: GetStaticProps<TProps> = async () => {
   const sharedContentFromServer: TSharedContentFromServer = {};
-  for (const username of Object.keys(sharedContents)) {
-    try {
-      const user = await getUserByUsername({ username });
-      sharedContentFromServer[username] = {
-        iconImages: (user.badges.filter((value) => value.image_url !== null).slice(0, 5) ?? []).map((value) => ({
-          alt: value.name,
-          src: `${NEXT_PUBLIC_ASKTUG_WEBSITE_BASE_URL}/${value.image_url}`,
-          width: 32,
-          height: 32,
-        })),
-      };
-    } catch (e) {
-      console.error('[Error] [/regional-meetup] [getServerSideProps] [sharedContentCards]', e);
-    }
-  }
-
   const videoRecordsFromServer: TVideoRecordFromServer = {};
-  await Promise.all(
-    Object.keys(videoRecords).map(async (bvid) => {
+
+  await Promise.all([
+    ...Object.keys(sharedContents).map(async (username) => {
+      try {
+        const user = await getUserByUsername({ username });
+        sharedContentFromServer[username] = {
+          iconImages: (user.badges.filter((value) => value.image_url !== null).slice(0, 5) ?? []).map((value) => ({
+            alt: value.name,
+            src: `${NEXT_PUBLIC_ASKTUG_WEBSITE_BASE_URL}/${value.image_url}`,
+            width: 32,
+            height: 32,
+          })),
+        };
+      } catch (e) {
+        console.error('[Error] [/regional-meetup] [getServerSideProps] [sharedContentCards]', e);
+      }
+    }),
+    ...Object.keys(videoRecords).map(async (bvid) => {
       try {
         const videoBasicInfo = await getVideoBasicInfo(bvid);
         if (videoBasicInfo.code === 0) {
@@ -138,8 +138,8 @@ export const getStaticProps: GetStaticProps<TProps> = async () => {
       } catch (e) {
         console.error('[Error] [/regional-meetup] [getServerSideProps] [videoRecordItems]', e);
       }
-    })
-  );
+    }),
+  ]);
 
   return {
     props: {
